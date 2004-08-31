0ca68692f01161ab9e72e9e0db9a88689b98b740
Thomas Friedrichsmeier <thomas.friedrichsmeier@ruhr-uni-bochum.de>
2004-08-31 12:37:33 +0000

    Adding more forgotten files
    svn path=/trunk/rkward/; revision=124

---
+++ rkward/misc/getfilenamewidget.cpp
@@ -0,0 +1,85 @@
+/***************************************************************************
+                          getfilenamewidget  -  description
+                             -------------------
+    begin                : Tue Aug 24 2004
+    copyright            : (C) 2004 by Thomas Friedrichsmeier
+    email                : tfry@users.sourceforge.net
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+#include "getfilenamewidget.h"
+
+#include <qlayout.h>
+#include <qlineedit.h>
+#include <qpushbutton.h>
+#include <qlabel.h>
+
+#include <klocale.h>
+#include <kfiledialog.h>
+
+#include "../debug.h"
+
+GetFileNameWidget::GetFileNameWidget (QWidget *parent, FileType mode, const QString &label, const QString &caption, const QString &initial) : QWidget (parent) {
+	RK_TRACE (MISC);
+	QVBoxLayout *vbox = new QVBoxLayout (this);
+	vbox->setResizeMode (QLayout::Minimum);
+
+	vbox->addWidget (new QLabel (label, this));
+
+	QHBoxLayout *hbox = new QHBoxLayout (vbox, 6);
+
+	location_edit = new QLineEdit (this);
+	location_edit->setText (initial);
+	connect (location_edit, SIGNAL (textChanged (const QString &)), this, SLOT (locationEditChanged (const QString &)));
+	hbox->addWidget (location_edit);
+	
+	browse_button = new QPushButton (i18n ("Browse"), this);
+	connect (browse_button, SIGNAL (clicked ()), this, SLOT (browseButtonClicked ()));
+	hbox->addWidget (browse_button);
+	
+	GetFileNameWidget::mode = mode;
+	
+	if (caption == "") {
+		GetFileNameWidget::caption = label;
+	} else {
+		GetFileNameWidget::caption = caption;
+	}
+}
+
+GetFileNameWidget::~GetFileNameWidget () {
+	RK_TRACE (MISC);
+}
+
+void GetFileNameWidget::locationEditChanged (const QString &) {
+	RK_TRACE (MISC);
+	emit (locationChanged ());
+}
+
+void GetFileNameWidget::browseButtonClicked () {
+	RK_TRACE (MISC);
+	QString temp;
+	if (mode == ExistingDirectory) {
+		temp = KFileDialog::getExistingDirectory (location_edit->text (), this, caption);
+	} else if (mode == ExistingFile) {
+		temp = KFileDialog::getOpenFileName (location_edit->text (), QString::null, this, caption);
+	} else {
+		RK_ASSERT (false);
+	}
+
+	if (temp != "") {
+		location_edit->setText (temp);
+	}
+}
+
+QString GetFileNameWidget::getLocation () {
+	return location_edit->text ();
+}
+
+#include "getfilenamewidget.moc"
---
+++ rkward/misc/getfilenamewidget.h
@@ -0,0 +1,54 @@
+/***************************************************************************
+                          getfilenamewidget  -  description
+                             -------------------
+    begin                : Tue Aug 24 2004
+    copyright            : (C) 2004 by Thomas Friedrichsmeier
+    email                : tfry@users.sourceforge.net
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+#ifndef GETFILENAMEWIDGET_H
+#define GETFILENAMEWIDGET_H
+
+#include <qwidget.h>
+#include <qstring.h>
+
+class QLineEdit;
+class QPushButton;
+
+/**
+Simple convenience class used to get a file/directoryname from the user. Basically provides a line-edit and a "browse"-button.
+
+@author Thomas Friedrichsmeier
+*/
+class GetFileNameWidget : public QWidget {
+	Q_OBJECT
+public:
+	enum FileType { ExistingFile=0, ExistingDirectory=1 };
+    
+	GetFileNameWidget (QWidget *parent, FileType mode, const QString &label, const QString &caption, const QString &initial);
+
+    ~GetFileNameWidget ();
+
+/// retrieves the current location
+	QString getLocation ();
+public slots:
+	void locationEditChanged (const QString &);
+	void browseButtonClicked ();
+signals:
+	void locationChanged ();
+private:
+	QLineEdit *location_edit;
+	QPushButton *browse_button;
+	QString caption;
+	FileType mode;
+};
+
+#endif
