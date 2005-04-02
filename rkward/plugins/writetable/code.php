f6a8d8db9ad9002c4461bbcbaeda0b11c790afde
Pierre Ecochard <ecoch@users.sf.net>
2005-04-02 08:56:49 +0000

    Adding two new plugins by Adrien to open and save tables in csv files.
    svn path=/trunk/rkward/; revision=247

---
+++ rkward/plugins/writetable/code.php
@@ -0,0 +1,26 @@
+<?
+	function preprocess () {
+	}
+	
+	function calculate () {
+?>
+
+write.table ( x =  <? getRK("data") ; ?> , file =  "<? getRK("file") ; ?>" , append =  <? getRK("append") ; ?>  ,quote = <? getRK("quote") ?> ,  sep = <? getRK("sep") ?> , eol = "<? getRK("eol")  ?>" , na = "<? getRK("na")  ?>" ,  dec = <? getRK("dec") ?> , row.names =  <? if (getRK_val("rows") == "custoRow") getRK("rownames") ; else  getRK("rows")  ; ?> ,  col.names =  <? if (getRK_val("columns") == "custoCol") getRK("colnames") ; else  getRK("columns")  ; ?> , qmethod=  <? getRK("qmethod") ; ?> ) 
+
+<?
+	}
+	
+	function printout () {
+	// produce the output
+?>
+
+<?
+	}
+	
+	function cleanup () {
+?>
+
+
+<?
+	}
+?>
