c156c5fa989955f0bdfe1511f254841f97909474
Adrien d'Hardemare <adhardemare@users.sf.net>
2005-04-05 09:50:04 +0000

    try to make skeleton plugin
    svn path=/trunk/rkward/; revision=263

---
+++ rkward/plugins/00saveload/save/skeleton/code.php
@@ -0,0 +1,27 @@
+<?
+	function preprocess () {
+	}
+	
+	function calculate () {
+$vars = str_replace ("\n", "','", trim (getRK_val ("data"))) ;
+?>
+
+package.skeleton(name =  "<? getRK("name") ; ?>", list = c('<? echo ($vars) ; ?>'),  path = "<? getRK("path") ; ?>", force =  "<? getRK("force") ; ?>")
+
+<?
+	}
+	
+	function printout () {
+	// produce the output
+?>
+
+<?
+	}
+	
+	function cleanup () {
+?>
+
+
+<?
+	}
+?>
