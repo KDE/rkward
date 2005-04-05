cb2aca786dad066182c082326ceeac98414de0c9
Adrien d'Hardemare <adhardemare@users.sf.net>
2005-04-05 09:48:29 +0000

    try to make write plugin
    svn path=/trunk/rkward/; revision=262

---
+++ rkward/plugins/00saveload/save/write/code.php
@@ -0,0 +1,26 @@
+<?
+	function preprocess () {
+	}
+	
+	function calculate () {
+?>
+
+write ( x =  <? getRK("data") ; ?> , file =  "<? getRK("file") ; ?>" , ncolumns =  <? getRK("ncolumns") ; ?> , append =  <? getRK("append") ; ?> )
+
+<?
+	}
+	
+	function printout () {
+	// produce the output
+?>
+
+<?
+	}
+	
+	function cleanup () {
+?>
+
+
+<?
+	}
+?>
