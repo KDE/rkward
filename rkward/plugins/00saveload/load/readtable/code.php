2a1655752ea3317811db0c7883b448a4dfb9faf3
Adrien d'Hardemare <adhardemare@users.sf.net>
2005-04-05 09:24:52 +0000

    this should do for load plugin
    svn path=/trunk/rkward/; revision=259

---
+++ rkward/plugins/00saveload/load/readtable/code.php
@@ -0,0 +1,30 @@
+<?
+	function preprocess () {
+	}
+	
+	function calculate () {
+?>
+<? getRK("name") ; ?> <- read.table(file =  "<? getRK("file") ; ?>", header = <? getRK("header") ?>, sep = <? getRK("sep") ?> , dec = <? getRK("dec") ?>, <? # doing row names (what a pity...) 
+if (getRK_val("isrow")=="true") echo( "row.names = " ) ; if (getRK_val("isrow")=="true" && getRK_val("rowname")=="NULL") echo (getRK_val("rowname") . ",") ; else if (getRK_val("isrow")=="true" && getRK_val("rowname")=="rowcol") echo (getRK("nomrow") . ",") ; else if  (getRK_val("isrow")=="true" && getRK_val("rowname")=="custoRow") echo (getRK_val("rownames") . ",") ; 
+# doing col names (what a pity...)
+if (getRK_val("colname") == "custoCol") echo( "col.names = ") ;  if (getRK_val("colname") == "custoCol") echo( getRK("colnames") ."," ); 
+# doing col class (what a pity...)
+if (getRK_val("colclass") == "custoClass") echo( "colClasses = ") ;  if (getRK_val("colclass") == "custoClass") echo( getRK("custoClasses") ."," );?>  <?#doing what is left?> , na.strings = "<? getRK("na")  ?>" ,  nrows = <? getRK("nrows") ; ?> , skip =  <? getRK("skip") ; ?> , check.names = <? getRK("checkname") ; ?> , fill = <? getRK("fill") ; ?> , strip.white =  <? getRK("stripwhite") ; ?>, blank.lines.skip = <? getRK("blanklinesskip") ; ?> ,comment.char = "<? getRK("commentchar") ; ?>" )
+
+<?
+	}
+	
+	function printout () {
+	// produce the output
+?>
+
+<?
+	}
+	
+	function cleanup () {
+?>
+
+
+<?
+	}
+?>
---
+++ rkward/plugins/00saveload/load/source/code.php
@@ -0,0 +1,26 @@
+<?
+	function preprocess () {
+	}
+	
+	function calculate () {
+?>
+
+source ( file =  "<? getRK("file") ; ?>" ,local =  <? getRK("local") ; ?> , echo =  <? getRK("echo") ; ?> , print.eval =  <? getRK("printeval") ; ?> , verbose =  <? getRK("verbose") ; ?> , prompt.echo =   <? getRK("promptecho") ; ?> , max.deparse.length =  <? getRK("maxdeparselength") ; ?> , chdir =  <? getRK("chdir") ; ?> )
+
+<?
+	}
+	
+	function printout () {
+	// produce the output
+?>
+
+<?
+	}
+	
+	function cleanup () {
+?>
+
+
+<?
+	}
+?>
