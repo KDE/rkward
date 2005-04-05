90c29bd15dac6316bb80a510eb92219fe5fd0ee2
Adrien d'Hardemare <adhardemare@users.sf.net>
2005-04-05 09:23:03 +0000

    third attempt (plugin load)
    svn path=/trunk/rkward/; revision=258

---
+++ rkward/plugins/00saveload/load/load/code.php
@@ -0,0 +1,26 @@
+<?
+	function preprocess () {
+	}
+	
+	function calculate () {
+?>
+
+load(file =  "<? getRK("file") ; ?>" )
+
+<?
+	}
+	
+	function printout () {
+	// produce the output
+?>
+
+<?
+	}
+	
+	function cleanup () {
+?>
+
+
+<?
+	}
+?>
