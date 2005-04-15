d8608d5929e661defe543e6571b93e2c17a72f43
Thomas Friedrichsmeier <thomas.friedrichsmeier@ruhr-uni-bochum.de>
2005-04-15 14:52:50 +0000

    Regenerated Makefile.am for plugins-dir and adding generation script to CVS
    svn path=/trunk/rkward/; revision=279

---
+++ rkward/plugins/makemakefileam.php
@@ -0,0 +1,50 @@
+#!/usr/bin/php
+<?
+
+# filthy script to generate a Makefile.am for the plugins-directory, installing all plugins as found.
+# usage: (in plugins dir)
+# ./makemakefileam.php > Makefile.am
+
+readsubs ("", "plugins");
+
+function readsubs ($dir, $prefix) {
+	if ($dir == "") {
+		$thisdir = opendir (".");
+		$ndir = "";
+	} else {
+		$thisdir = opendir ($dir);
+		$ndir = $dir . "/";
+	}
+	$subdirs = array ();
+	$files = array ();
+	
+	echo (strtr ($prefix, "/", "X") . "dir = $(kde_datadir)/rkward/" . $dir . "\n");
+	
+	while (false !== ($file = readdir($thisdir))) {
+		if (!is_dir ($ndir . $file)) {
+			if (($file == "description.xml") || ($file == "code.php")) {
+				array_push ($files, $file);
+			}
+		}  else {
+			if (($file != ".") && ($file != "..") && ($file != "CVS")) {
+				array_push ($subdirs, $file);
+			} 
+		}
+	}
+
+	if (count ($files)) {
+		echo ("dist_" . strtr ($prefix, "/", "X") . "_DATA =");
+		
+		foreach ($files as $item) {
+			echo (" \\\n\t" . $ndir . $item);
+		}
+		echo ("\n\n");
+	}
+	
+	foreach ($subdirs as $sub) {
+		readsubs ($ndir . $sub, $prefix . "/" . strtr ($sub, "_.", "UD"));
+	}
+	
+	closedir ($thisdir);
+}
+?>
