88609ef1e7db969a68c2d53162f275cf48f98861
Thomas Friedrichsmeier <thomas.friedrichsmeier@ruhr-uni-bochum.de>
2005-04-18 11:19:11 +0000

    Some enhancements and fixes to the API-documentation
    svn path=/trunk/rkward/; revision=286

--- rkward/rbackend/rcommandstack.h
+++ rkward/rbackend/rcommandstack.h
@@ -32,18 +32,28 @@ public:
 
 	~RCommandStack ();
 
+/// add a command to the given chain (static, as it does no matter, which stack the chain belongs to)
 	static void issueCommand (RCommand *command, RCommandChain *chain);
-	
+
+/// add a sub-chain to the given chain (static, as it does no matter, which stack the chain belongs to)
 	static RCommandChain *startChain (RCommandChain *parent);
+/** close the given chain, i.e. signal the chain may be deleted once its remaining commands are done
+ (static, as it does no matter, which stack the chain belongs to).  @Returns the parent of the closed chain. */
 	static RCommandChain *closeChain (RCommandChain *chain);
 
+/// @Returns true, if there are no commands or open chains waiting in this stack
 	bool isEmpty ();
+/// @Returns true, if the currently processed chain is not closed and not empty
 	bool isBlocked ();
+/// @Returns true, if there are commands to be processed in the current chain
 	bool isActive ();
-	
+
+/// removes the RCommand to be processed next from the stack and returns it. If there is no command to process right now, returns 0
 	RCommand *pop ();
-	
+
+/// the regular command stack, i.e. not a callback
 	static RCommandStack *regular_stack;
+/// return the parent RCommandStack of the given RCommandChain
 	static RCommandStack *chainStack (RCommandChain *child);
 private:
 	RCommandChain *current_chain;
