5d455206bf84be8ffa0379e2a007da1ae7caa7d0
Thomas Friedrichsmeier <thomas.friedrichsmeier@ruhr-uni-bochum.de>
2006-04-05 16:01:17 +0000

    More small fixes to the plugins
    svn path=/trunk/rkward/; revision=554

--- rkward/plugins/distributions/plot_poisson_distribution.php
+++ rkward/plugins/distributions/plot_poisson_distribution.php
@@ -9,7 +9,7 @@
 ?>rk.header ("Plot density <? getRK ("function"); ?>", list ("Minimum", "<? getRK ("min"); ?>", "Maximum", "<? getRK ("max"); ?>", "Mean", "<? getRK ("mean"); ?>", "Function", "<? getRK ("function"); ?>"));
 
 rk.graph.on ()
-plot (<? getRK ("function"); ?> (<? getRK ("min"); ?> : <? getRK ("max"); ?>) , lambda = <? getRK ("mean"); ?>)
+plot (<? getRK ("function"); ?> (<? getRK ("min"); ?> : <? getRK ("max"); ?>, lambda = <? getRK ("mean"); ?>))
 rk.graph.off ()
 <?
 	}
