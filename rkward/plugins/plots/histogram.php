253288035b4fd814c8fa50d7282d03e3132b944d
Thomas Friedrichsmeier <thomas.friedrichsmeier@ruhr-uni-bochum.de>
2006-04-11 00:16:42 +0000

    Default R method of producing histograms is too strange. Adding some more options
    svn path=/trunk/rkward/; revision=574

--- rkward/plugins/plots/histogram.php
+++ rkward/plugins/plots/histogram.php
@@ -6,11 +6,20 @@
 	}
 	
 	function printout () {
+	$breaks = getRK_val ("breaks");
+	$scale = getRK_val ("scale");
+	$x = getRK_val ("x");
+	if ($breaks == "int") {
+		$breaksopt = "breaks=seq (as.integer (min (" . $x . "))-0.5, as.integer (max (" . $x ."))+0.5)";
+		$breaks = "Integers";
+	} else if (!empty ($breaks)) {
+		$breaksopt = "breaks=\"" . $breaks . "\"";
+	}
 ?>
-rk.header ("Histogram", list ("Frequency", "<? getRK ("scale"); ?>"))
+rk.header ("Histogram", list ("Frequency", "<? echo $scale; ?>", "Breaks algorithm", <? echo ("\"" . $breaks . "\""); ?>, "Variable", rk.get.description (<? echo ($x); ?>)))
 cat ("<h1>Histogram</h1>")
 rk.graph.on ()
-plot (hist (<? getRK ("x"); ?>, freq = <? getRK ("scale"); ?>)<? getRK ("plotoptions.code.printout"); ?>)
+hist (<? echo ($x); ?>, <? echo ($breaksopt); ?>, freq = <? echo $scale; ?><? getRK ("plotoptions.code.printout"); ?>)
 rk.graph.off ()
 <?
 	}
