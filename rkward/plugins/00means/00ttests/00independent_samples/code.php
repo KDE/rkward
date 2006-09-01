699410f92ca93b1ae2aab1efd168ae2064ed6f15
Thomas Friedrichsmeier <thomas.friedrichsmeier@ruhr-uni-bochum.de>
2006-09-01 12:15:41 +0000

    Small fixes by sjar
    svn path=/trunk/rkward/; revision=683

--- rkward/plugins/00means/00ttests/00independent_samples/code.php
+++ rkward/plugins/00means/00ttests/00independent_samples/code.php
@@ -15,11 +15,11 @@ rk.temp.print.conf.level <- <? if (getRK_val ("confint")) echo "TRUE"; else echo
 ?>cat ("<h1>T-test (independent samples)</h1>\n")
 cat (paste ("<h2>Comparing", deparse (rk.temp.x), "against", deparse (rk.temp.y), "</h2>\n"))
 if (rk.temp$alternative == "less") {
-	cat (paste ("<h3>H1:", rk.get.short.name (rk.temp.y), "is greater than", rk.get.short.name (rk.temp.x), "<h3>"))
+	cat (paste ("<h3>H1:", rk.get.short.name (rk.temp.y), "is greater than", rk.get.short.name (rk.temp.x), "</h3>"))
 } else if (rk.temp$alternative == "greater") {
-	cat (paste ("<h3>H1:", rk.get.short.name (rk.temp.x), "is greater than", rk.get.short.name (rk.temp.y), "<h3>"))
+	cat (paste ("<h3>H1:", rk.get.short.name (rk.temp.x), "is greater than", rk.get.short.name (rk.temp.y), "</h3>"))
 } else {
-	cat (paste ("<h3>H1:", rk.get.short.name (rk.temp.x), "and", rk.get.short.name (rk.temp.y), "differ<h3>"))
+	cat (paste ("<h3>H1:", rk.get.short.name (rk.temp.x), "and", rk.get.short.name (rk.temp.y), "differ</h3>"))
 }
 if (rk.temp.var.equal) {
 	cat ("<h4>Assuming equal variances</h4>")
@@ -33,7 +33,7 @@ cat ("</tr>\n")
 cat (paste ("<tr><td>", rk.get.description (rk.temp.x), "</td><td>", rk.temp$estimate[1], "</td><td rowspan=\"2\">", rk.temp$parameter, "</td><td rowspan=\"2\">", rk.temp$statistic, "</td><td rowspan=\"2\">", rk.temp$p.value, "</td>", sep=""))
 if (rk.temp.print.conf.level) cat (paste ("<td rowspan=\"2\">[", rk.temp$conf.int[1], " .. ", rk.temp$conf.int[2],"]</td>", sep=""))
 cat ("</tr>\n")
-cat (paste ("<tr><td>", rk.get.description (rk.temp.y), "</td><td>", rk.temp$estimate[2], "</td><tr>", sep=""))
+cat (paste ("<tr><td>", rk.get.description (rk.temp.y), "</td><td>", rk.temp$estimate[2], "</td></tr>", sep=""))
 cat ("</table>")
 <?
 	}
