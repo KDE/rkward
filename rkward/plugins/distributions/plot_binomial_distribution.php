110ba026db6335a0d2be1435d03255213de766d3
Thomas Friedrichsmeier <thomas.friedrichsmeier@ruhr-uni-bochum.de>
2006-09-14 14:58:51 +0000

    Implemented plot binomial distribution
    svn path=/trunk/rkward/; revision=728

--- rkward/plugins/distributions/plot_binomial_distribution.php
+++ rkward/plugins/distributions/plot_binomial_distribution.php
@@ -6,10 +6,32 @@
 	}
 	
 	function printout () {
-?>rk.header ("Plot density <? getRK ("function"); ?>", list ("Number of Observations", "<? getRK ("n"); ?>", "Minimum", "<? getRK ("min"); ?>", "Maximum", "<? getRK ("max"); ?>", "Mean", "<? getRK ("mean"); ?>", "Standard Deviation", "<? getRK ("sd"); ?>", "Function", "<? getRK ("function"); ?>"));
+		$fun = getRK_val ("function");
+		if ($fun == "dbinom") $label = "density";
+		else $label = "distribution";
+	
+		$min = getRK_val ("min");
+		$nq = getRK_val ("nq");
+		$space = getRK_val ("space");
+		$size = getRK_val ("size");
+		$prob = getRK_val ("prob");
+	
+		if (getRK_val ("log") == "1") {
+			if ($fun == "dbinom") {
+				$log = ", log=TRUE";
+			} else {
+				$log = ", log.p=TRUE";
+			}
+			$log_label="logarithmic";
+		} else {
+			$log = "";
+			$log_label="normal";
+		}
+
+?>rk.header ("Plot binomial <? echo ($label); ?>", list ("Lowest quantile", "<? echo ($min); ?>", "Largest quantile", "<? echo ($min + ($nq * $space)); ?>", "Quantile spacing", "<? echo ($space); ?>", "Number of trials", "<? echo ($size); ?>", "Probability of success on each trial", "<? echo ($prob); ?>", "Function", "<? getRK ("function"); ?>", "Scaling", "<? echo ($log_label); ?>"));
 
 rk.graph.on ()
-plot (<? getRK ("function"); ?> (seq(<? getRK ("min"); ?> ,<? getRK ("max"); ?>, length= <? getRK ("n"); ?>) , mean = <? getRK ("mean"); ?>, sd = <? getRK ("sd"); ?>))
+plot (<? getRK ("function"); ?> (seq (<? echo ($min); ?>, <? echo ($min + ($nq * $space)); ?>, by=<? echo ($space); ?>) , size=<? echo ($size); ?>, prob=<? echo ($prob); echo ($log); ?>))
 rk.graph.off ()
 <?
 	}
