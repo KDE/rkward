f0730399a4e81b01a6a73d08963d1f264c38cd18
Thomas Friedrichsmeier <thomas.friedrichsmeier@ruhr-uni-bochum.de>
2006-10-12 15:31:12 +0000

    Distribution-plugin updates by sjar part 2: Poisson probabilities
    svn path=/trunk/rkward/; revision=861

--- rkward/plugins/distributions/poisson_probabilities.php
+++ rkward/plugins/distributions/poisson_probabilities.php
@@ -1,40 +1,26 @@
 <?
-	function preprocess () {
-	}
-	
-	function calculate () {
-		$vars = "substitute (" . str_replace ("\n", "), substitute (", trim (getRK_val ("x"))) . ")";
-?>
-rk.temp.options <- list (doquantile=<? getRK ("samplequantile"); ?>)
-rk.temp.results <- list ()
-i=0; for (var in list (<? echo ($vars); ?>)) {
-	i = i+1
-	rk.temp.results[[i]] <- list ()
-	rk.temp.results[[i]]$object <- var
-	if (rk.temp.options$doquantile) try (rk.temp.results[[i]]$quantile <- quantile (eval (var), na.rm= <? getRK ("narm"); ?>, type = <? getRK ("samplequantile"); ?>, name = <? getRK ("names"); ?>, probs = seq(<? getRK ("probs1"); ?>, <? getRK ("probs2"); ?>, <? getRK ("probs3"); ?>)))
-}<?
-	}
-	
-	function printout () {
-?>
-cat ("<h1>Descriptive statistics</h1>")
-
-cat ("<table border=\"1\"><tr><td>Variable</td>")
-if (rk.temp.options$doquantile) cat ("<td>quantile</td>")
-cat ("</tr>")
-
-for (i in 1:length (rk.temp.results)) {
-	cat (paste ("<tr><td>", rk.get.description (rk.temp.results[[i]]$object), "</td>"))
-	if (rk.temp.options$doquantile) cat (paste ("<td>", rk.temp.results[[i]]$quantile, "</td>"))
-	cat ("</tr>")
+function preprocess () {
 }
-cat ("</table>")
+
+function calculate () {
+	global $q;
+	$q = "c (" . preg_replace ("/[, ]+/", ", ", getRK_val ("q")) . ")";
+?>
+rk.temp = (ppois (q = <? echo ($q); ?>, lambda = <? getRK ("lambda"); ?>, <? getRK ("tail"); ?>, <? getRK("logp"); ?>))
 <?
-	}
-	
-	function cleanup () {
-?>rm (rk.temp.options)
-rm (rk.temp.results)
+}
+
+function printout () {
+	global $q;
+?>
+rk.header ("Poisson probabilities", list ("Vector of quantiles", "<? echo ($q); ?>", "Lambda", "<? getRK ("lambda"); ?>", "Tail", "<? getRK ("tail"); ?>", "Probabilities p are given as", "<? getRK ("logp"); ?>"))
+cat ("<h3>Poisson probabilities:  ", rk.temp, "</h3>")
 <?
-	}
+}
+
+function cleanup () {
+?>
+rm (rk.temp)
+<?
+}
 ?>
