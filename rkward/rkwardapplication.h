23ec3b2d43097fa02fa0766dfbeeff30d9486e82
Thomas Friedrichsmeier <thomas.friedrichsmeier@ruhr-uni-bochum.de>
2006-11-26 17:58:55 +0000

    some API documentation
    svn path=/trunk/rkward/; revision=940

--- rkward/rkwardapplication.h
+++ rkward/rkwardapplication.h
@@ -22,7 +22,7 @@
 
 #include <qmap.h>
 
-/** The purpose of subclassing KApplication as RKWardApplication, is to obtain raw access to X11 events. This is needed to detect the creation of new toplevel windows (R X11 windows). */
+/** The purpose of subclassing KApplication as RKWardApplication, is to obtain raw access to X11 events. This is needed to detect the creation of new toplevel windows (R X11 windows), and changed in the caption of those windows. */
 
 class RKWardApplication : public KApplication {
 public:
@@ -32,13 +32,18 @@ public:
 	/** like KApplication::kApplication () (and actually, this should always return the same pointer), but without the need to cast */
 	static RKWardApplication *getApp ();
 
-	/** reimplemented from KApplication to look for CreateNotify events */
+	/** reimplemented from KApplication to look for CreateNotify and PropertyNotify events */
 	bool x11EventFilter (XEvent *e);
 
+	/** start looking for new top-level windows created on the screen */
 	void startWindowCreationDetection ();
+	/** stop looking for new top-level windows created on the screen
+	@returns the window id of the last top-level window created after the last call to startWindowCreation, hoping it was only one. 0 if no window was created/detected. */
 	WId endWindowCreationDetection ();
 
+	/** watch the given window for changes in its WM_NAME property (i.e. changes in caption). When a change is detected, the caption will be set on watcher. WARNING: Do not use to watch windows managed by Qt! Will override the event mask for this window (within qt_xdisplay ()). WARNING: Remember to call unregisterNameWatcher, when watcher is deleted! */
 	void registerNameWatcher (WId watched, QWidget *watcher);
+	/** remove a watch created with registerNameWatcher */
 	void unregisterNameWatcher (WId watched);
 private:
 	static RKWardApplication *rkapp;
