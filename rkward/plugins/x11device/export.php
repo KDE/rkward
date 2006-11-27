8056c4dfad15212f6673c1b857f273017f964656
Thomas Friedrichsmeier <thomas.friedrichsmeier@ruhr-uni-bochum.de>
2006-11-27 17:42:18 +0000

    Makefile.am, started implementing export plugin
    svn path=/trunk/rkward/; revision=951

---
+++ rkward/plugins/x11device/export.php
@@ -0,0 +1,18 @@
+<?php
+function preprocess () {
+}
+
+function calculate () {
+?>
+<?
+}
+	
+function printout () {
+?>## TODO: need to set the correct device!
+<?
+}
+	
+function cleanup () {
+?><?
+}
+?>
