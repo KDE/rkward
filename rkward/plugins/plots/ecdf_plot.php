bb0db8d78eb52e64a4fb390a6df9b4d34dada637
Stefan Rödiger <stefan_roediger@gmx.de>
2006-12-17 00:08:00 +0000

    verticals and do.points to ECDF added GUI changed Description of output gives now min, max and length
    svn path=/trunk/rkward/; revision=1010

--- rkward/plugins/plots/ecdf_plot.php
+++ rkward/plugins/plots/ecdf_plot.php
@@ -3,18 +3,29 @@
 	}
 	
 	function calculate () {
+?>	rk.min = min (<? getRK("x"); ?>)
+	rk.max = max (<? getRK("x"); ?>)
+	rk.length = length (<? getRK ("x"); ?>)
+<?
 	}
 	
 	function printout () {
 	$x = getRK_val ("x");
+	$col = getRK_val ("col");
 ?>
-rk.header ("ECDF", list ("Variable", rk.get.description (<? echo ($x); ?>)))
+rk.header ("ECDF", list ("Variable", rk.get.description (<? echo ($x); ?>), "Minimum", rk.min, "Maximum", rk.max, "Length", rk.length))
 rk.graph.on ()
-plot.ecdf (<? echo ($x); ?>, <? getRK ("plotoptions.code.printout"); ?>)
+plot.ecdf (<? echo ($x); ?>, <? getRK ("plotoptions.code.printout"); ?>, <? getRK ("dopoints"); ?>, <? getRK ("vertical"); ?>, datadensity="rug")
+curve(pnorm, from= rk.min, to= rk.max, add=TRUE, col="<? echo ($col); ?>")
+rug(<? echo ($x); ?>)
 rk.graph.off ()
 <?
 	}
 	
 	function cleanup () {
+?>	rm (rk.min)
+	rm (rk.max)
+	rm (rk.length)
+<?
 	}
 ?>
