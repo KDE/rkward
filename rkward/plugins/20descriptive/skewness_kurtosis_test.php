e958f295672cf01f07cf4d1304554cc9648c1267
Stefan Rödiger <stefan_roediger@gmx.de>
2006-12-31 16:29:54 +0000

    * initial support for skewness kurtosis (moments package) and adopted all.pluginmap * minor corrections in agostino plug-in
    svn path=/trunk/rkward/; revision=1021

---
+++ rkward/plugins/20descriptive/skewness_kurtosis_test.php
@@ -0,0 +1,62 @@
+<?
+        function preprocess () {
+        }
+
+	function calculate () {
+	$vars = "substitute (" . str_replace ("\n", "), substitute (", trim (getRK_val ("x"))) . ")";
+
+?>
+	
+	require(moments)
+
+	rk.temp.options <- list (doskewness=<? getRK ("skewness"); ?>, dokurtosis=<? getRK ("kurtosis"); ?>, dolength=<? getRK ("length"); ?>, donacount=<? getRK ("nacount"); ?>)
+
+	rk.temp.results <- list ()
+	i=0; for (var in list (<? echo ($vars); ?>)) {
+	i = i+1
+	rk.temp.results[[i]] <- list ()
+	rk.temp.results[[i]]$object <- rk.get.description (var, is.substitute=TRUE)
+	if (rk.temp.options$doskewness) try (rk.temp.results[[i]]$skewness <- skewness (eval (var), <? getRK ("narm_skewness"); ?>))
+	if (rk.temp.options$dokurtosis) try (rk.temp.results[[i]]$kurtosis <- kurtosis (eval (var), <? getRK ("narm_kurtosis"); ?>))
+	if (rk.temp.options$dolength) try (rk.temp.results[[i]]$length <- length (eval (var)))
+	if (rk.temp.options$donacount) try (rk.temp.results[[i]]$nacount <- length (which(is.na(eval (var)))))
+}
+
+
+<?
+        }
+	function printout () {
+?>	cat ("<h1>Skewness and Kurtosis</h1>\n")
+
+cat ("<table border=\"1\">")
+	cat ("<tbody>")
+		cat ("<tr>")
+			cat ("<td>Variable Name</td>")
+			if (rk.temp.options$dolength) cat ("<td>Length</td>")
+			if (rk.temp.options$donacount) cat ("<td>NAs</td>")
+			if (rk.temp.options$doskewness) cat ("<td>Skewness</td>")
+			if (rk.temp.options$dokurtosis) cat ("<td>Kurtosis</td>")
+		cat ("</tr>")
+for (i in 1:length (rk.temp.results)) {
+		cat ("<tr><td>", rk.temp.results[[i]]$object, "</td>")
+		if (rk.temp.options$dolength) cat ("<td>", rk.temp.results[[i]]$length, "</td>")
+		if (rk.temp.options$donacount) cat ("<td>", rk.temp.results[[i]]$nacount, "</td>")
+		if (rk.temp.options$doskewness) cat (paste ("<td>", rk.temp.results[[i]]$skewness,"</td>"))
+		if (rk.temp.options$dokurtosis) cat (paste ("<td>", rk.temp.results[[i]]$kurtosis,"</td>"))
+		cat ("</tr>")
+}
+	cat ("</tbody>")
+cat ("</table>")
+
+
+<?
+        }
+	function cleanup () {
+
+?>
+	rm (rk.temp.results)
+	rm (rk.temp.options)
+	rm (var)
+<?
+        }
+?>
