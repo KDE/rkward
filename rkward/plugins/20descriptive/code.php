11f5c79777a8bbd8786ead375a317df747385a9f
Thomas Friedrichsmeier <thomas.friedrichsmeier@ruhr-uni-bochum.de>
2007-01-02 16:56:03 +0000

    Fixes and improvments for descriptive statistics plugin
    svn path=/trunk/rkward/; revision=1052

--- rkward/plugins/20descriptive/code.php
+++ rkward/plugins/20descriptive/code.php
@@ -3,11 +3,13 @@
 	}
 	
 	function calculate () {
+		global $mad_type;
+		global $constMad;
+
 		$vars = "substitute (" . str_replace ("\n", "), substitute (", trim (getRK_val ("x"))) . ")";
 		$trim = getRK_val ("trim"); //the fraction (0 to 0.5) of observations to be trimmed from each end of x before the mean is computed
 		$constMad = getRK_val ("constMad");
-		$low = getRK_val ("low");
-		$high = getRK_val ("high");
+		$mad_type = getRK_val ("mad_type");
 
 ?>
 rk.temp.options <- list (domean=<? getRK ("mean"); ?>, domedian=<? getRK ("median"); ?>, dorange=<? getRK ("range"); ?>, dosd=<? getRK ("sd"); ?>, dosum=<? getRK ("sum"); ?>, doprod=<? getRK ("prod"); ?>, domad=<? getRK ("mad"); ?>, dolength=<? getRK ("length"); ?>, donacount=<? getRK ("nacount"); ?>)
@@ -31,17 +33,24 @@ for (rk.temp.var in rk.temp.vars) {
 	if (rk.temp.options$dosd) rk.temp.results$sd[i] <- try (sd (eval (rk.temp.var), na.rm=TRUE))
 	if (rk.temp.options$dosum) rk.temp.results$sum[i] <- try (sum (eval (rk.temp.var), na.rm=TRUE))
 	if (rk.temp.options$doprod) rk.temp.results$prod[i] <- try (prod (eval (rk.temp.var), na.rm=TRUE))
-	if (rk.temp.options$domad) rk.temp.results$mad[i] <- try (mad (eval (rk.temp.var), constant = <?echo ($constMad) ;?>, <?echo ($low) ;?>, <?echo ($high) ;?>, na.rm=TRUE))
+	if (rk.temp.options$domad) rk.temp.results$mad[i] <- try (mad (eval (rk.temp.var), constant = <? echo ($constMad);
+		if ($mad_type == "low") echo (", low=TRUE");
+		elseif ($mad_type == "high") echo (", high=TRUE"); ?>, na.rm=TRUE))
 	if (rk.temp.options$dolength) rk.temp.results$length[i] <- try (length (eval (rk.temp.var)))
 	if (rk.temp.options$donacount) rk.temp.results$nacount[i] <- try (length (which(is.na(eval (rk.temp.var)))))
 }<?
 	}
 	
 	function printout () {
+		global $mad_type;
+		global $constMad;
 ?>
 rk.header ("Descriptive statistics", parameters=list ("Trim of mean", <?getRK ("trim") ;?>,
 					if (rk.temp.options$domad) "Median Absolute Deviation",
-					if (rk.temp.options$domad) paste ("constant:", <?echo ($constMad) ;?>, "lo-median:", <?getRK ("low") ;?>, "hi-median:", <? getRK ("high") ;?>)))
+					if (rk.temp.options$domad) paste ("constant:", <?echo ($constMad) ;?>, <?
+						if ($mad_type == "low") echo ('"lo-median"');
+						elseif ($mad_type == '"hi-median"');
+						else echo ('"average"'); ?>)))
 
 rk.results (rk.temp.results,
 	titles = c ("Object",
