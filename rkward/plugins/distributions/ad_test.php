8da24853c3f9346786b085c9ff8e6a1727dc457a
Thomas Friedrichsmeier <thomas.friedrichsmeier@ruhr-uni-bochum.de>
2007-01-04 01:04:27 +0000

    Explicitly setting the titles in the output is redundant, and can be removed by given better names to the fields
    svn path=/trunk/rkward/; revision=1059

--- rkward/plugins/distributions/ad_test.php
+++ rkward/plugins/distributions/ad_test.php
@@ -11,17 +11,17 @@ require(nortest)
 rk.temp.options <- list (dolength=<? getRK ("length"); ?>, donacount=<? getRK ("nacount"); ?>)
 
 rk.temp.vars <- list (<? echo ($vars); ?>)
-rk.temp.results <- data.frame (object=rep (NA, length (rk.temp.vars)))
+rk.temp.results <- data.frame ('Variable Name'=rep (NA, length (rk.temp.vars)))
 
 i=0;
 for (rk.temp.var in rk.temp.vars) {
 	i = i+1
-	rk.temp.results$object[i] <- rk.get.description (rk.temp.var, is.substitute=TRUE)
-	if (rk.temp.options$dolength) try (rk.temp.results$length[i] <- length (eval (rk.temp.var)))
-	if (rk.temp.options$donacount) try (rk.temp.results$nacount[i] <- length (which(is.na(eval (rk.temp.var)))))
+	rk.temp.results$'Variable Name'[i] <- rk.get.description (rk.temp.var, is.substitute=TRUE)
+	if (rk.temp.options$dolength) try (rk.temp.results$'Length'[i] <- length (eval (rk.temp.var)))
+	if (rk.temp.options$donacount) try (rk.temp.results$'NAs'[i] <- length (which(is.na(eval (rk.temp.var)))))
 	rk.temp.test <- ad.test (eval (rk.temp.var))
-	rk.temp.results$statistic[i] <- paste (names (rk.temp.test$statistic), rk.temp.test$statistic, sep=" = ")
-	rk.temp.results$p.value[i] <- rk.temp.test$p.value
+	rk.temp.results$'Statistic'[i] <- paste (names (rk.temp.test$statistic), rk.temp.test$statistic, sep=" = ")
+	rk.temp.results$'p-value'[i] <- rk.temp.test$p.value
 }
 
 <?
@@ -31,7 +31,7 @@ function printout () {
 ?>
 rk.header ("Anderson-Darling Normality Test")
 
-rk.results (rk.temp.results, titles=c ("Variable Name", if (rk.temp.options$dolength) "Length", if (rk.temp.options$donacount) "NAs", "Statistic", "p-value"))
+rk.results (rk.temp.results)
 <?
 }
 
