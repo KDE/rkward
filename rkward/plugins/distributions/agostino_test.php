17be9f0e4a765d6f4f306255b1901c95608e9de5
Thomas Friedrichsmeier <thomas.friedrichsmeier@ruhr-uni-bochum.de>
2007-01-07 15:53:38 +0000

    Plugin coding style
    svn path=/trunk/rkward/; revision=1073

--- rkward/plugins/distributions/agostino_test.php
+++ rkward/plugins/distributions/agostino_test.php
@@ -5,60 +5,43 @@
 	function calculate () {
 	$vars = "substitute (" . str_replace ("\n", "), substitute (", trim (getRK_val ("x"))) . ")";
 
-?>	
+?>
+require(moments)
 
-	require(moments)	
-
-	rk.temp.options <- list (dolength=<? getRK ("length"); ?>, donacount=<? getRK ("nacount"); ?>)
-
-	rk.temp.results <- list ()
-	i=0; for (var in list (<? echo ($vars); ?>)) {
+rk.temp.vars <- list (<? echo ($vars); ?>)
+rk.temp.results <- data.frame ('Variable Name'=rep (NA, length (rk.temp.vars)), check.names=FALSE)
+i=0;
+for (var in rk.temp.vars) {
 	i = i+1
-	rk.temp.results[[i]] <- list ()
-	rk.temp.results[[i]]$object <- rk.get.description (var, is.substitute=TRUE)
-	rk.temp.results[[i]]$agostino <- agostino.test (eval (var), alternative = c("<? getRK ("alternative"); ?>"))
-	if (rk.temp.options$dolength) try (rk.temp.results[[i]]$length <- length (eval (var)))
-	if (rk.temp.options$donacount) try (rk.temp.results[[i]]$nacount <- length (which(is.na(eval (var)))))
+	rk.temp.results$'Variable Name'[i] <- rk.get.description (var, is.substitute=TRUE)
+	<? if (getRK_val ("length")) { ?>
+	rk.temp.results$'Length'[i] <- try (length (eval (var)))
+	<? }
+	if (getRK_val ("nacount")) { ?>
+	rk.temp.results$'NAs'[i] <- try (length (which(is.na(eval (var)))))
+	<? } ?>
+	try ({
+		rk.temp.test <- agostino.test (eval (var), alternative = c("<? getRK ("alternative"); ?>"))
+		rk.temp.results$'Statistic'[i] <- paste (names (rk.temp.test$statistic), rk.temp.test$statistic, sep=" = ")
+		rk.temp.results$'p-value'[i] <- rk.temp.test$p.value
+	})
+
 }
-
-
 <?
         }
 	function printout () {
-?>	
-	rk.header ("D'Agostino test for skewness in normally distributed data")
-
-cat ("<table border=\"1\">")
-	cat ("<tbody>")
-		cat ("<tr>")
-			cat ("<td>Variable Name</td>")
-			if (rk.temp.options$dolength) cat ("<td>Length</td>")
-			if (rk.temp.options$donacount) cat ("<td>NAs</td>")
-			cat ("<td>Skewness and Transformation</td>")
-			cat ("<td>p-value</td>")
-			cat ("<td>alternative</td>")
-			cat ("<td>Method</td>")
-			cat ("<td>variable</td>")
-		cat ("</tr>")
-for (i in 1:length (rk.temp.results)) {
-		cat ("<tr><td>", rk.temp.results[[i]]$object, "</td>")
-		if (rk.temp.options$dolength) cat ("<td>", rk.temp.results[[i]]$length, "</td>")
-		if (rk.temp.options$donacount) cat ("<td>", rk.temp.results[[i]]$nacount, "</td>")
-		cat (paste ("<td>", rk.temp.results[[i]]$agostino,"</td>"))
-		cat ("</tr>")
-}
-	cat ("</tbody>")
-cat ("</table>")
-
-
+?>
+rk.header ("D'Agostino test for skewness in normally distributed data")
+rk.results (rk.temp.results)
 <?
         }
 	function cleanup () {
 
 ?>
-	rm (rk.temp.results)
-	rm (rk.temp.options)
-	rm (var)
+rm (rk.temp.results)
+rm (rk.temp.test)
+rm (rk.temp.vars)
+rm (var)
 <?
         }
 ?>
