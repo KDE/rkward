8b5639e3f3103b4aa8490f954f64a6070f3d1318
Thomas Friedrichsmeier <thomas.friedrichsmeier@ruhr-uni-bochum.de>
2007-01-08 14:41:19 +0000

    Copyright year
    svn path=/trunk/rkward/; revision=1094

--- rkward/misc/rkcommonfunctions.cpp
+++ rkward/misc/rkcommonfunctions.cpp
@@ -2,7 +2,7 @@
                           rkcommonfunctions  -  description
                              -------------------
     begin                : Mon Oct 17 2005
-    copyright            : (C) 2005, 2006 by Thomas Friedrichsmeier
+    copyright            : (C) 2005, 2006, 2007 by Thomas Friedrichsmeier
     email                : tfry@users.sourceforge.net
  ***************************************************************************/
 
