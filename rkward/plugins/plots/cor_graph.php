33ef94606b431066301263a17e2f5d4b8c3c6278
Thomas Friedrichsmeier <thomas.friedrichsmeier@ruhr-uni-bochum.de>
2007-01-14 15:23:15 +0000

    Update of correlation matrix plot plugin (I. Soumpasis) (NOT for 0.4.5)
    svn path=/trunk/rkward/; revision=1139

--- rkward/plugins/plots/cor_graph.php
+++ rkward/plugins/plots/cor_graph.php
@@ -1,26 +1,25 @@
 <?
 	function preprocess () { ?>
-		rk.temp.cor.graph <- function(x) {
-			panel.cor <- function(x, y, digits=<? getRK ("digits"); ?>, cex.cor) {
-    				usr <- par("usr"); on.exit(par(usr))
-    				par(usr = c(0, 1, 0, 1))
-    				r <- abs(cor(x, y, use="<? getRK ("use"); ?>", method="<? getRK ("method"); ?>"))
-    				txt <- format(c(r, 0.123456789), digits=digits)[1]
-    				txt <- paste(txt, sep="")
-    				if(missing(cex.cor)) cex <- 0.8/strwidth(txt)
+	rk.temp.cor.graph <- function(x) {
+		panel.cor <- function(x, y, digits=<? getRK ("digits"); ?>, cex.cor, use="<? getRK ("use"); ?>", method="<? getRK ("method"); ?>") {
+			usr <- par("usr"); on.exit(par(usr))
+			par(usr = c(0, 1, 0, 1))
+    			r <- abs(cor(x, y, use=use, method=method))
+    			txt <- format(c(r, 0.123456789), digits=digits)[1]
+    			txt <- paste(txt, sep="")
+    			if(missing(cex.cor)) cex <- 0.8/strwidth(txt)
      		
-     				test <- cor.test(x,y, use="<? getRK ("use"); ?>", method="<? getRK ("method"); ?>")
-				Signif <- symnum(test$p.value, corr = FALSE, na = FALSE,
-                  				cutpoints = c(0, 0.001, 0.01, 0.05, 0.1, 1),
-                  				symbols = c("***", "**", "*", ".", " "))
+     			test <- cor.test(x,y, use=use, method=method)
+			Signif <- symnum(test$p.value, corr = FALSE, na = FALSE,
+           				cutpoints = c(0, 0.001, 0.01, 0.05, 0.1, 1),
+                  			symbols = c("***", "**", "*", ".", " "))
     
-    				if("<? getRK ("scale"); ?>"=="yes") text(0.5, 0.5, txt, cex = cex * r)
-    				if("<? getRK ("scale"); ?>"=="no") text(0.5, 0.5, txt, cex = cex)
-    				text(.8, .8, Signif, cex=cex, col=2)
-			}
-			pairs(x, 
-  			lower.panel=panel.smooth, upper.panel=panel.cor)
+    			if(<? getRK ("scale"); ?>)  text(0.5, 0.5, txt, cex = cex * r)
+    			else text(0.5, 0.5, txt, cex = cex)
+    			text(.8, .8, Signif, cex=cex, col=2)
 		}
+		pairs(x, lower.panel=panel.smooth, upper.panel=panel.cor)
+	}
 	<?		}
 	
 	function calculate () {
@@ -34,7 +33,7 @@
 
 	rk.temp.x<- data.frame (<? echo ($vars); ?>)
 
-	rk.header ("Correlation Matrix Plot")
+	rk.header ("Correlation Matrix Plot", parameters=list ("Method", "<? getRK ("method"); ?>", "Exclusion", "<? getRK ("use"); ?>", "Precision", "<? getRK ("digits"); ?> digits", "Scale text", "<? getRK ("scale"); ?>"))
 
 	rk.graph.on ()
 
