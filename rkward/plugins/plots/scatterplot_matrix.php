f86ce21463c288fe9eb0e7dbac8c5e3f93c0a13b
Thomas Friedrichsmeier <thomas.friedrichsmeier@ruhr-uni-bochum.de>
2007-01-15 15:38:24 +0000

    Spaces
    svn path=/trunk/rkward/; revision=1158

--- rkward/plugins/plots/scatterplot_matrix.php
+++ rkward/plugins/plots/scatterplot_matrix.php
@@ -11,17 +11,13 @@ require(car)
 	function printout () {
 	$vars = str_replace ("\n", ",", trim (getRK_val ("x"))) ;
 ?>
+rk.temp.x <- data.frame (<? echo ($vars); ?>)
 
-rk.temp.x<- data.frame (<? echo ($vars); ?>)
-
-rk.header ("Correlation Matrix Plot", parameters=list ("Diagonal Panels", "<? getRK("diag") ?>", "Plot points", "<? getRK ("plot_points"); ?>", "Smooth", "<? getRK ("smooth"); ?>", "Ellipses", "<? getRK ("ellipse"); ?> at 0.5 and 0.9 (normal) probability or confidence levels."))
+rk.header ("Scatterplot Matrix", parameters=list ("Diagonal Panels", "<? getRK("diag") ?>", "Plot points", "<? getRK ("plot_points"); ?>", "Smooth", "<? getRK ("smooth"); ?>", "Ellipses", "<? getRK ("ellipse"); ?> at 0.5 and 0.9 (normal) probability or confidence levels."))
 
 rk.graph.on ()
-
-scatterplot.matrix(rk.temp.x, diagonal= "<? getRK("diag") ?>", plot.points=<? getRK ("plot_points"); ?>, smooth=<? getRK ("smooth"); ?>, ellipse=<? getRK ("ellipse"); ?>)
-
+scatterplot.matrix(rk.temp.x, diagonal="<? getRK("diag") ?>", plot.points=<? getRK ("plot_points"); ?>, smooth=<? getRK ("smooth"); ?>, ellipse=<? getRK ("ellipse"); ?>)
 rk.graph.off ()
-
 <?
 	}
 	
