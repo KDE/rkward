b7091b31ac489c5a31357918427d17fd0eb268a5
Stefan Rödiger <stefan_roediger@gmx.de>
2007-01-22 23:40:14 +0000

    skeletons for Wilcoxon rank sum tests work still in progress!
    svn path=/trunk/rkward/; revision=1194

---
+++ rkward/plugins/wilcoxon/wilcoxon_test.php
@@ -0,0 +1,44 @@
+<?
+	function preprocess () {
+	}
+	
+	function calculate () {
+?>rk.temp.x <- substitute (<? getRK ("x"); ?>)
+rk.temp.y <- substitute (<? getRK ("y"); ?>)
+rk.temp <- t.test (eval (rk.temp.x), eval (rk.temp.y), alternative = c("<? getRK ("hypothesis"); ?>"), mu = <? getRK ("mu"); ?>, paired = <? getRK ("paired"); ?>, exact = <? getRK ("exact"); ?>, correct = <? getRK ("correct"); ?>, conf.int = <? getRK ("confint"); ?> <?
+if (($conflevel = getRK_val ("conflevel")) != "0.95") echo (", conf.level=" . $conflevel); ?>)
+rk.temp.print.conf.level <- <? if (getRK_val ("confint")) echo "TRUE"; else echo "FALSE"; ?>
+
+<?
+	}
+	
+	function printout () {
+?>
+rk.header ("Wilcoxon Test", 
+	parameters=list ("Comparing", paste (rk.get.description (rk.temp.x, is.substitute=TRUE), "against", rk.get.description (rk.temp.y, is.substitute=TRUE)),
+	"H1", if (rk.temp$alternative == "less")
+		paste (rk.get.short.name (rk.temp.y), "is greater than", rk.get.short.name (rk.temp.x))
+	      else if (rk.temp$alternative == "greater")
+		paste (rk.get.short.name (rk.temp.x), "is greater than", rk.get.short.name (rk.temp.y))
+	      else
+		paste (rk.get.short.name (rk.temp.x), "and", rk.get.short.name (rk.temp.y), "differ")))
+
+rk.results (list (
+	'Variable Name'=rk.get.description (rk.temp.x),
+	'statistic'=rk.temp$statistic,
+	'degrees of freedom'=rk.temp$parameter,
+	'mu'=rk.temp$null.value,
+	'alternative'=rk.temp$alternative,
+	p=rk.temp$p.value<?
+	if (getRK_val ("confint")) { ?>,
+	'confidence interval percent'=(100 * attr(rk.temp$conf.int, "conf.level")),
+	'confidence interval of difference'=rk.temp$conf.int <? } ?>))
+<?
+	}
+	
+	function cleanup () {
+?>
+rm (list=grep ("^rk.temp", ls (), value=TRUE))
+<?
+	}
+?>
