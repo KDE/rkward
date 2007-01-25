5ce6eb0c0c496f956ee059515404369f39f6ebf6
Ilias Soumpasis <nono.231@gmail.com>
2007-01-25 17:52:16 +0000

    PHP wraping of function at .php. Added the vector option at .xml and .rkh.
    svn path=/trunk/rkward/; revision=1222

--- rkward/plugins/plots/barplot.php
+++ rkward/plugins/plots/barplot.php
@@ -1,22 +1,5 @@
 <?
 	function preprocess () {
-	?>
-rk.temp.barplot2<-function(x, beside=<? getRK ("beside"); ?>, legend.text=<? getRK ("legend"); ?>, rainbow=<? getRK ("rainbow"); ?>, labels=<? getRK ("labels"); ?>, place=<? getRK ("place"); ?>){
-if(beside){
-		if(rainbow) {		
-		rk.temp.barplot <- barplot((x), col=rainbow(x), beside=beside, legend.text=legend.text,  ylim = range(x) * c(0, 1.2))
-		}
-		else {
-		rk.temp.barplot <- barplot((x), beside=beside, legend.text=legend.text,  ylim = range(x) * c(0, 1.2))
-		}
-		if(labels) text(rk.temp.barplot,x, labels=x, pos=place, offset=.5)
-	}
-	else {
-		if(rainbow) barplot((x), col=rainbow(x), legend.text=legend.text)
-		else barplot((x), legend.text=legend.text)
-}
-}
-<?
 }
 	
 	function calculate () {
@@ -26,18 +9,30 @@ if(beside){
 	$vars = str_replace ("\n", ",", trim (getRK_val ("x"))) ; 
 	?>
 
-rk.temp.x2 <- (<? echo ($vars); ?>)
-if(is.matrix(rk.temp.x2)) rk.temp.x=rk.temp.x2
-if(is.factor(rk.temp.x2)) rk.temp.x=summary(rk.temp.x2)
+rk.temp.x <- (<? echo ($vars); ?>)
+if(is.factor(rk.temp.x)) rk.temp.x=summary(rk.temp.x)
 
 rk.header ("Barplot", parameters=list ("Rainbow colors", "<? getRK ("rainbow"); ?>", "Beside", "<? getRK ("beside"); ?>", "Legend", "<? getRK ("legend"); ?>"))
 
 rk.graph.on ()
 try ({
-rk.temp.barplot2(rk.temp.x)	
+	<?
+	if (getRK_val ("beside") == "TRUE") {
+	?>rk.temp.barplot <-barplot((rk.temp.x)<? if (getRK_val ("rainbow")=="TRUE") { ?>,
+col=rainbow(rk.temp.x) <? } ?>, beside=<? getRK ("beside"); ?>, legend.text=<?
+getRK ("legend"); ?>,  ylim = range(rk.temp.x) * c(0, 1.2))
+	 <?
+	}
+	if ((getRK_val ("beside") == "TRUE") && getRK_val ("labels")=="TRUE") { ?>
+text(rk.temp.barplot, rk.temp.x, labels=rk.temp.x, pos=<? getRK ("place"); ?>,
+offset=.5)
+	<? } 
+	if (getRK_val ("beside") == "FALSE") {
+?> barplot((rk.temp.x)<? if (getRK_val ("rainbow")=="TRUE") { ?>,
+col=rainbow(rk.temp.x) <? } ?>, legend.text=<? getRK ("legend"); ?>)
+	<? } ?>
 })
 rk.graph.off ()
-
 
 <?
 	}
