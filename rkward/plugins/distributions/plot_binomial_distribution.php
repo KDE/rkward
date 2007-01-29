55a918d9221a3d8cccd3dc91bb07ea200c25a109
Thomas Friedrichsmeier <thomas.friedrichsmeier@ruhr-uni-bochum.de>
2007-01-29 13:25:10 +0000

    Additional parameters, use plot.function to get better x axis labelling (Prasenjit Kapat)
    svn path=/trunk/rkward/; revision=1239

--- rkward/plugins/distributions/plot_binomial_distribution.php
+++ rkward/plugins/distributions/plot_binomial_distribution.php
@@ -1,41 +1,41 @@
 <?
 	function preprocess () {
 	}
-	
+
 	function calculate () {
 	}
-	
-	function printout () {
-		$fun = getRK_val ("function");
-		if ($fun == "dbinom") $label = "density";
-		else $label = "distribution";
-	
-		$min = getRK_val ("min");
-		$nq = getRK_val ("nq");
-		$space = getRK_val ("space");
-		$size = getRK_val ("size");
-		$prob = getRK_val ("prob");
-	
-		if (getRK_val ("log") == "1") {
-			if ($fun == "dbinom") {
-				$log = ", log=TRUE";
-			} else {
-				$log = ", log.p=TRUE";
-			}
-			$log_label="logarithmic";
-		} else {
-			$log = "";
-			$log_label="normal";
-		}
 
-?>rk.header ("Plot binomial <? echo ($label); ?>", list ("Lowest quantile", "<? echo ($min); ?>", "Largest quantile", "<? echo ($min + ($nq * $space)); ?>", "Quantile spacing", "<? echo ($space); ?>", "Number of trials", "<? echo ($size); ?>", "Probability of success on each trial", "<? echo ($prob); ?>", "Function", "<? getRK ("function"); ?>", "Scaling", "<? echo ($log_label); ?>"));
+	function printout () {
+	$fun = getRK_val ("function");
+	if ($fun == "dbinom") {
+		$label = "mass";
+		$lower_tag = "";
+		$tail_tag = "";
+	} else {
+		$label = "distribution";
+		if (getRK_val("lower") == "1") {
+			$lower_tag = ", lower.tail = 1";
+			$tail_tag = ", \"Tail\",\"Lower\"";
+		} else {
+			$lower_tag = ", lower.tail = 0";
+			$tail_tag = ", \"Tail\",\"Upper\"";
+		}
+	}
+	$min = getRK_val ("min");
+	$size = getRK_val ("size");
+	$prob = getRK_val ("prob");
+	$max = getRK_val ("max");
+	if (getRK_val ("log") == "1") $log_label="logarithmic";
+	else $log_label="normal";
+
+?>rk.header ("Binomial <? echo ($label); ?> function", list ("Lower quantile", "<? getRK ("min"); ?>", "Upper quantile", "<? getRK ("max"); ?>", "Number of trials", "<? echo ($size); ?>", "Probability of success on each trial", "<? echo ($prob); ?>", "Scaling", "<? echo ($log_label); ?>"<? echo ($tail_tag); ?>, "Function", "<? getRK ("function"); ?>"));
 
 rk.graph.on ()
-try (plot (<? getRK ("function"); ?> (seq (<? echo ($min); ?>, <? echo ($min + ($nq * $space)); ?>, by=<? echo ($space); ?>) , size=<? echo ($size); ?>, prob=<? echo ($prob); echo ($log); ?>)))
+try (plot (function (x) <? getRK ("function"); ?> (x, size = <? getRK ("size"); ?>, prob=<? echo ($prob); ?>, log = <? getRK ("log"); ?><? echo ($lower_tag); ?>), from=<? echo ($min); ?>, to=<? echo ($max); ?>, n=<? echo ($max - $min + 1); ?>, type="p"))
 rk.graph.off ()
 <?
 	}
-	
+
 	function cleanup () {
 	}
 ?>
