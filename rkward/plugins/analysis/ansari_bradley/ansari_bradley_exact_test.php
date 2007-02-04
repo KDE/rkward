bd169b464a6601ed302fea6d27ec83c1e76b75f7
Stefan Rödiger <stefan_roediger@gmx.de>
2007-02-04 19:46:05 +0000

    some unused/wrong parts removed
    svn path=/trunk/rkward/; revision=1303

--- rkward/plugins/analysis/ansari_bradley/ansari_bradley_exact_test.php
+++ rkward/plugins/analysis/ansari_bradley/ansari_bradley_exact_test.php
@@ -9,7 +9,6 @@ rk.temp.x <- substitute (<? getRK ("x"); ?>)
 rk.temp.y <- substitute (<? getRK ("y"); ?>)
 rk.temp <- ansari.exact (eval (rk.temp.x), eval (rk.temp.y), alternative = c("<? getRK ("alternative"); ?>"), exact = <? getRK ("exact"); ?>, conf.int = <? getRK ("confint"); ?> <?
 if (($conflevel = getRK_val ("conflevel")) != "0.95") echo (", conf.level=" . $conflevel); ?>)
-rk.temp.print.conf.level <- <? if (getRK_val ("confint")) echo "TRUE"; else echo "FALSE"; ?>
 
 <?
 	}
@@ -17,13 +16,7 @@ rk.temp.print.conf.level <- <? if (getRK_val ("confint")) echo "TRUE"; else echo
 	function printout () {
 ?>
 rk.header ("Ansari-Bradley two-sample exact test", 
-	parameters=list ("Comparing", paste (rk.get.description (rk.temp.x, is.substitute=TRUE), "against", rk.get.description (rk.temp.y, is.substitute=TRUE)),
-	"H1", if (rk.temp$alternative == "less")
-		paste (rk.get.short.name (rk.temp.y), "is greater than", rk.get.short.name (rk.temp.x))
-	      else if (rk.temp$alternative == "greater")
-		paste (rk.get.short.name (rk.temp.x), "is greater than", rk.get.short.name (rk.temp.y))
-	      else
-		paste (rk.get.short.name (rk.temp.x), "and", rk.get.short.name (rk.temp.y), "differ"),"Confidence Interval", "<? getRK ("confint"); ?>", "Confidence Level","Compute exact p-value", "<? getRK ("exact"); ?>"))
+	parameters=list ("Comparing", paste (rk.get.description (rk.temp.x, is.substitute=TRUE), "against", rk.get.description (rk.temp.y, is.substitute=TRUE)),"Confidence Interval", "<? getRK ("confint"); ?>", "Confidence Level","Compute exact p-value", "<? getRK ("exact"); ?>"))
 
 rk.results (list (
 	'Variable Names'=rk.get.description (rk.temp.x, rk.temp.y, is.substitute=TRUE),
--- rkward/plugins/analysis/ansari_bradley/ansari_bradley_test.php
+++ rkward/plugins/analysis/ansari_bradley/ansari_bradley_test.php
@@ -4,12 +4,10 @@
 	
 	function calculate () {
 ?>
-rk.temp.length.x <- length (<? getRK ("x"); ?>)
 rk.temp.x <- substitute (<? getRK ("x"); ?>)
 rk.temp.y <- substitute (<? getRK ("y"); ?>)
 rk.temp <- ansari.test (eval (rk.temp.x), eval (rk.temp.y), alternative = c("<? getRK ("alternative"); ?>"), exact = <? getRK ("exact"); ?>, conf.int = <? getRK ("confint"); ?> <?
 if (($conflevel = getRK_val ("conflevel")) != "0.95") echo (", conf.level=" . $conflevel); ?>)
-rk.temp.print.conf.level <- <? if (getRK_val ("confint")) echo "TRUE"; else echo "FALSE"; ?>
 
 <?
 	}
@@ -17,14 +15,7 @@ rk.temp.print.conf.level <- <? if (getRK_val ("confint")) echo "TRUE"; else echo
 	function printout () {
 ?>
 rk.header ("Ansari-Bradley two-sample test",
-	parameters=list ("Comparing", paste (rk.get.description (rk.temp.x, is.substitute=TRUE), "against", rk.get.description (rk.temp.y, is.substitute=TRUE)),
-	"H1", if (rk.temp$alternative == "less")
-		paste (rk.get.short.name (rk.temp.y), "is greater than", rk.get.short.name (rk.temp.x))
-	      else if (rk.temp$alternative == "greater")
-		paste (rk.get.short.name (rk.temp.x), "is greater than", rk.get.short.name (rk.temp.y))
-	      else
-		paste (rk.get.short.name (rk.temp.x), "and", rk.get.short.name (rk.temp.y), "differ"),
- 	"Note", if (rk.temp.length.x < 50) paste ("You have less then 50 values. An exact test is recommended.") else paste("Length is", (rk.temp.length.x)),"Compute exact p-value", "<? getRK ("confint"); ?>","Confidence Interval", "<? getRK ("exact"); ?>"))
+	parameters=list ("Comparing", paste (rk.get.description (rk.temp.x, is.substitute=TRUE), "against", rk.get.description (rk.temp.y, is.substitute=TRUE)),"Compute exact p-value", "<? getRK ("confint"); ?>","Confidence Interval", "<? getRK ("exact"); ?>"))
 
 rk.results (list (
 	'Variable Names'=rk.get.description (rk.temp.x, rk.temp.y, is.substitute=TRUE),
