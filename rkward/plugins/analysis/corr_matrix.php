5b5b3d0e64b90d16bb3f817d0d18fcbcd0e9c3fd
Thomas Friedrichsmeier <thomas.friedrichsmeier@ruhr-uni-bochum.de>
2007-02-07 12:54:36 +0000

    consistency
    svn path=/trunk/rkward/; revision=1346

--- rkward/plugins/analysis/corr_matrix.php
+++ rkward/plugins/analysis/corr_matrix.php
@@ -4,6 +4,10 @@ function preprocess () {
 	
 function calculate () {
 	global $use;
+	global $method;
+	global $do_p;
+
+	$do_p = getRK_val ("do_p");
 
 	$vars = "substitute (" . str_replace ("\n", "), substitute (", trim (getRK_val ("x"))) . ")";
 	$use = getRK_val ("use");
@@ -14,6 +18,7 @@ function calculate () {
 		$exclude_whole = true;
 		$use = "\"complete.obs\"";
 	}
+	$method = "\"" . getRK_val ("method") . "\"";
 
 ?>rk.temp.objects <- list (<? echo ($vars); ?>)
 
@@ -22,8 +27,8 @@ function calculate () {
 rk.temp.frame <- data.frame (lapply (rk.temp.objects, function (x) eval (x, envir=globalenv ())))
 
 # calculate correlation matrix
-rk.temp <- cor (rk.temp.frame, use=<? echo ($use); ?>, method="<? getRK ("method"); ?>")
-<?	if (getRK_val ("do_p")) { ?>
+rk.temp <- cor (rk.temp.frame, use=<? echo ($use); ?>, method=<? echo ($method); ?>)
+<?	if ($do_p) { ?>
 # calculate matrix of probabilities
 rk.temp.p <- matrix (nrow = length (rk.temp.frame), ncol = length (rk.temp.frame))
 local ({
@@ -35,7 +40,7 @@ local ({
 	for (i in 1:length (rk.temp.frame)) {
 		for (j in i:length (rk.temp.frame)) {
 			if (i != j) {
-				t <- cor.test (rk.temp.frame[[i]], rk.temp.frame[[j]], method="<? getRK ("method"); ?>")
+				t <- cor.test (rk.temp.frame[[i]], rk.temp.frame[[j]], method=<? echo ($method); ?>)
 				rk.temp.p[i, j] <<- t$p.value
 				rk.temp.p[j, i] <<- sum (complete.cases (rk.temp.frame[[i]], rk.temp.frame[[j]]))
 			}
@@ -47,13 +52,15 @@ local ({
 
 function printout () {
 	global $use;
+	global $method;
+	global $do_p;
 ?>
-rk.header ("Correlation Matrix", parameters=list ("Method", "<? getRK ("method"); ?>", "Exclusion", <? echo ($use); ?>))
+rk.header ("Correlation Matrix", parameters=list ("Method", <? echo ($method); ?>, "Exclusion", <? echo ($use); ?>))
 
 rk.temp <- data.frame (I (sapply (rk.temp.objects, FUN=function (x) rk.get.description (x, is.substitute=TRUE))), as.data.frame (rk.temp))
 rk.results (rk.temp, titles=c ('Coefficient', sapply (rk.temp.objects, rk.get.short.name)))
 
-<?	if (getRK_val ("do_p")) { ?>
+<?	if ($do_p) { ?>
 rk.temp.p <- data.frame (I (sapply (rk.temp.objects, FUN=function (x) rk.get.description (x, is.substitute=TRUE))), as.data.frame (rk.temp.p))
 rk.results (rk.temp.p, titles=c ('n \\ p', sapply (rk.temp.objects, rk.get.short.name)))
 <?	}
