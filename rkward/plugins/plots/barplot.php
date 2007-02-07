5a4fe5526fa4d2a9665843e2c460f1dbba3865f7
Thomas Friedrichsmeier <thomas.friedrichsmeier@ruhr-uni-bochum.de>
2007-02-07 21:25:20 +0000

    Deal better with vectors with attributes, vectors spanning below 0, and a bit of formatting
    svn path=/trunk/rkward/; revision=1349

--- rkward/plugins/plots/barplot.php
+++ rkward/plugins/plots/barplot.php
@@ -9,11 +9,10 @@ function printout () {
 	doPrintout (true);
 }
 	
-function cleanup () {
-	?>
+function cleanup () { ?>
 rm (list=grep ("^rk.temp", ls (), value=TRUE))
-	<?
-	}
+<?
+}
 	
 function preview () {
 	preprocess ();
@@ -23,11 +22,16 @@ function preview () {
 }
 	
 function doPrintout ($final) {
-	$vars = str_replace ("\n", ",", trim (getRK_val ("x"))) ; 
+	$vars = str_replace ("\n", ",", trim (getRK_val ("x"))) ;
+	$labels = (getRK_val ("labels")=="TRUE");
 ?>
-
 rk.temp.x <- (<? echo ($vars); ?>)
-if(is.factor(rk.temp.x)) rk.temp.x=summary(rk.temp.x)
+if(is.factor(rk.temp.x)) {
+	rk.temp.x <- summary(rk.temp.x)
+} else {
+	# barplot is a bit picky about attributes, so we need to convert to vector explicitely
+	if(!is.matrix(rk.temp.x)) rk.temp.x <- as.vector(rk.temp.x)
+}
 
 <? 
 	if ($final) { ?>
@@ -37,20 +41,20 @@ rk.graph.on ()
 <?	}
 ?>
 try ({
-	<?
-	if (getRK_val ("beside") == "TRUE") {
-	?>
-rk.temp.barplot <-barplot((rk.temp.x)<? if (getRK_val ("rainbow")=="TRUE") { ?>, col=rainbow( if(is.matrix(rk.temp.x)) dim(rk.temp.x) else length(rk.temp.x)) <? } ?>, beside=<? getRK ("beside"); ?>, legend.text=<? getRK ("legend"); ?>,  ylim = range(rk.temp.x) * c(0, 1.2))
-	<?
-	}
-	if ((getRK_val ("beside") == "TRUE") && getRK_val ("labels")=="TRUE") { 
-	?>
+<?	if (getRK_val ("beside") == "TRUE") { ?>
+<?		if ($labels) { ?>
+# adjust the range so that the labels will fit
+rk.temp.range <- range (rk.temp.x, na.rm=TRUE) * 1.2
+if (rk.temp.range[1] > 0) rk.temp.range[1] <- 0
+if (rk.temp.range[2] < 0) rk.temp.range[2] <- 0
+<?		} ?>
+rk.temp.barplot <- barplot((rk.temp.x)<? if (getRK_val ("rainbow")=="TRUE") { ?>, col=rainbow( if(is.matrix(rk.temp.x)) dim(rk.temp.x) else length(rk.temp.x)) <? } ?>, beside=<? getRK ("beside"); ?>, legend.text=<? getRK ("legend"); ?><? if ($labels) echo (", ylim = rk.temp.range"); ?>)
+<?		if ($labels) { ?>
 text(rk.temp.barplot, rk.temp.x, labels=rk.temp.x, pos=<? getRK ("place"); ?>, offset=.5)
-	<? } 
-	if (getRK_val ("beside") == "FALSE") { 
-	?> 
+<?		}
+	} else { ?>
 barplot((rk.temp.x)<? if (getRK_val ("rainbow")=="TRUE") { ?>, col=rainbow( if(is.matrix(rk.temp.x)) dim(rk.temp.x) else length(rk.temp.x)) <? } ?>, legend.text=<? getRK ("legend"); ?>)
-	<? } 
+<?	}
 	?>
 })
 <?	if ($final) { ?>
