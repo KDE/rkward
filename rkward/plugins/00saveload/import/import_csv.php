d1b5352d619628cbe109263a155517cdb5312a91
Thomas Friedrichsmeier <thomas.friedrichsmeier@ruhr-uni-bochum.de>
2007-02-11 17:55:56 +0000

    Several improvements to import CSV plugin
    svn path=/trunk/rkward/; revision=1374

--- rkward/plugins/00saveload/import/import_csv.php
+++ rkward/plugins/00saveload/import/import_csv.php
@@ -1,9 +1,21 @@
 <?
-	function preprocess () {
-	}
-	
-	function calculate () {
-getRK("name"); ?> <- read.table (file="<? getRK("file"); ?>", header=<? getRK("header"); ?>, sep=<? getRK("sep"); ?>, dec=<? getRK("dec") ?>, <? # doing row names (what a pity...) 
+// internal helper function
+function quoteString ($string) {
+	return ('"' . strtr ($string, array ('"'=>'\\"')) . '"');
+}
+
+function preprocess () {
+}
+
+function calculate () {
+	$dec = getRK_val ("dec");
+	if ($dec == "other") $dec = quoteString (getRK_val ("custom_dec"));
+	$sep = getRK_val ("sep");
+	if ($sep == "other") $sep = quoteString (getRK_val ("custom_sep"));
+	$quote = getRK_val ("quote");
+	if ($quote == "other") $quote = quoteString (getRK_val ("custom_quote"));
+
+getRK("name"); ?> <- read.table (file="<? getRK("file"); ?>", header=<? getRK("header"); ?>, sep=<? echo ($sep); ?>, quote=<? echo ($quote); ?>, dec=<? echo ($dec); ?>, <? # doing row names (what a pity...) 
 if (getRK_val("rowname")!="NULL") {
 	echo( "row.names = ");
 	if (getRK_val("rowname")=="rowcol") echo (getRK("nomrow") . ",");
@@ -13,15 +25,15 @@ if (getRK_val("rowname")!="NULL") {
 if (getRK_val("colname") == "custoCol") echo ( "col.names = " . getRK_val ("colnames") . ",");
 # doing col class (what a pity...)
 if (getRK_val("colclass") == "custoClass") echo( "colClasses = " . getRK_val ("custoClasses") . ",");
-#doing what is left?> na.strings = "<? getRK("na")  ?>" ,  nrows = <? getRK("nrows") ; ?> , skip =  <? getRK("skip") ; ?> , check.names = <? getRK("checkname") ; ?> , fill = <? getRK("fill") ; ?> , strip.white =  <? getRK("stripwhite") ; ?>, blank.lines.skip = <? getRK("blanklinesskip") ; ?> ,comment.char = "<? getRK("commentchar") ; ?>" )
+#doing what is left?> na.strings = "<? getRK("na")  ?>", nrows = <? getRK("nrows") ; ?>, skip =  <? getRK("skip") ; ?>, check.names = <? getRK("checkname") ; ?>, fill = <? getRK("fill") ; ?>, strip.white = <? getRK("stripwhite") ; ?>, blank.lines.skip = <? getRK("blanklinesskip") ; ?>, comment.char=<? echo (quoteString (getRK_val("commentchar"))); ?><? getRK("allow_escapes"); ?><? getRK("flush"); ?><? getRK("strings_as_factors"); ?>)
 
 <?
-	}
-	
-	function printout () {
-	// produce the output
-	}
-	
-	function cleanup () {
-	}
+}
+
+function printout () {
+// produce the output
+}
+
+function cleanup () {
+}
 ?>
