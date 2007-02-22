503c382c7557459dda234d77baabc3e909c990e6
Thomas Friedrichsmeier <thomas.friedrichsmeier@ruhr-uni-bochum.de>
2007-02-22 21:30:17 +0000

    Make default main title work. Allow specification of ylim. require xtable only when needed. Some small cleanups
    svn path=/trunk/rkward/; revision=1448

--- rkward/plugins/plots/pareto.php
+++ rkward/plugins/plots/pareto.php
@@ -1,29 +1,30 @@
 <?
-function preprocess () {
-?> require(qcc) 
+function preprocess () { ?>
+require(qcc)
+<?	if (getRK_val ("descriptives")=="TRUE") { ?>
 require(xtable)
-<?
+<?	}
 }
-	
+
 function calculate () {
 }
 
 function printout () {
 	doPrintout (true);
 }
-	
+
 function cleanup () { ?>
 rm (list=grep ("^rk.temp", ls (), value=TRUE))
 <?
 }
-	
+
 function preview () {
 	preprocess ();
 	calculate ();
 	doPrintout (false);
 	cleanup ();
 }
-	
+
 function doPrintout ($final) {
 	$vars = getRK_val ("x");
 	$descriptives = getRK_val ("descriptives")=="TRUE";
@@ -33,20 +34,21 @@ if(is.factor(rk.temp.x)) {
 	rk.temp.x <- summary(rk.temp.x)
 } 
 
-<? 
-	if ($final) { ?>
+<?	if ($final) { ?>
 rk.header ("Pareto chart")
 
 rk.graph.on ()
 <?	}
 ?>
-try (
-<?
-if ($final && $descriptives) { ?>
-rk.print(xtable(<?}?>pareto.chart(rk.temp.x <? getRK ("plotoptions.code.printout"); ?>)<? if ($final && $descriptives){ ?>))
-<?}
-?>
-)
+try ({
+<?	if ($final && $descriptives) { ?>
+	rk.temp.descriptives <- <?
+	} else echo ("\t"); ?>
+pareto.chart(rk.temp.x<? getRK ("plotoptions.code.printout"); ?>)
+<?	if ($final && $descriptives) { ?>
+	rk.print(xtable(rk.temp.descriptives))
+<?	} ?>
+})
 <?	if ($final) { ?>
 rk.graph.off ()
 <? }
