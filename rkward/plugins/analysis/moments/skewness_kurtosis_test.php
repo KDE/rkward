d00f43411aeef24cf8307bf5712d17dbd9a1d64c
Thomas Friedrichsmeier <thomas.friedrichsmeier@ruhr-uni-bochum.de>
2007-02-27 14:39:42 +0000

    Fix indentation of generated code
    svn path=/trunk/rkward/; revision=1496

--- rkward/plugins/analysis/moments/skewness_kurtosis_test.php
+++ rkward/plugins/analysis/moments/skewness_kurtosis_test.php
@@ -16,25 +16,25 @@ i=0;
 for (var in rk.temp.objects) {
 	i = i+1
 	rk.temp.results$'Variable Name'[i] <- rk.get.description (var, is.substitute=TRUE)
-	<? 
+<?
 	if (getRK_val ("skewness")) { ?>
 	try (rk.temp.results$'Skewness'[i] <- skewness (eval (var)<? echo ($narm); ?>))
-	<? }
+<?	}
 	if (getRK_val ("kurtosis")) { ?>
 	try ({
 		rk.temp.results$'Kurtosis'[i] <- kurtosis (eval (var)<? echo ($narm); ?>)
 		rk.temp.results$'Excess Kurtosis'[i] <- rk.temp.results$'Kurtosis'[i] - 3
 	})
-	<? }
+<?	}
 	if (getRK_val ("geary")) { ?>
 	try (rk.temp.results$'Geary kurtosis'[i] <- geary (eval (var)<? echo ($narm); ?>))
-	<? }
+<?	}
 	if (getRK_val ("length")) { ?>
 	try (rk.temp.results$'Length'[i] <- length (eval (var)))
-	<? }
+<?	}
 	if (getRK_val ("nacount")) { ?>
 	try (rk.temp.results$'NAs'[i] <- length (which(is.na(eval (var)))))
-	<? } ?>
+<?	} ?>
 }
 <?
 }
