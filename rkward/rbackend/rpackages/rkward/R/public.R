35d37d49e96ee7fab0129f6318858efe0e72e058
Thomas Friedrichsmeier <thomas.friedrichsmeier@ruhr-uni-bochum.de>
2007-02-28 13:12:33 +0000

    Add rk.describe.alternative for dealing with htests
    svn path=/trunk/rkward/; revision=1503

--- rkward/rbackend/rpackages/rkward/R/public.R
+++ rkward/rbackend/rpackages/rkward/R/public.R
@@ -208,6 +208,25 @@
 	cat ("<pre>", paste (x, collapse="\n"), "</pre>\n", sep="", file=rk.get.output.html.file(), append=TRUE);
 }
 
+# Describe the alternative (H1) of an htest.
+# This code adapted from stats:::print.htest
+"rk.describe.alternative" <- function (x) {
+	res <- ""
+	if (!is.null(x$alternative)) {
+		if (!is.null(x$null.value)) {
+ 			if (length(x$null.value) == 1) {
+ 				alt.char <- switch(x$alternative, two.sided = "not equal to", less = "less than", greater = "greater than")
+ 				res <- paste ("true", names(x$null.value), "is", alt.char, x$null.value)
+ 			} else {
+ 				res <- paste (x$alternative, "\nnull values:\n", x$null.value)
+ 			}
+		} else {
+			res <-  (x$alternative)
+		}
+	}
+	res
+}
+
 "rk.make.repos.string" <- function () {
 	x <- options ("repos")$repos
 	len <- length (x)
