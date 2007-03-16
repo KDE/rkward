33311de18c7f7c36ec3c939e3be427e3ccb678ee
Thomas Friedrichsmeier <thomas.friedrichsmeier@ruhr-uni-bochum.de>
2007-03-16 14:37:01 +0000

    Remove give.Rkern option from histogram. It is not useful in this context
    svn path=/trunk/rkward/; revision=1608

--- rkward/plugins/plots/histogram.php
+++ rkward/plugins/plots/histogram.php
@@ -9,14 +9,10 @@ function printout () {
 	doPrintout (true);
 }
 
-function cleanup () {
-}
-
 function preview () {
 	preprocess ();
 	calculate ();
 	doPrintout (false);	// only this one actually needed...
-	cleanup ();
 }
 
 // internal helper functions
@@ -24,8 +20,7 @@ function doPrintout ($final) {
 	$densityscaled = getRK_val ("densityscaled");
 	$bw =  getRK_val ("bw");
 	$adjust = getRK_val ("adjust");
-	$narm =  getRK_val ("narm");
-	$giveRkern =  getRK_val ("giveRkern");
+	$narm = getRK_val ("narm");
 	$x = getRK_val ("x");
 
 	if ($final) { ?>
@@ -37,7 +32,7 @@ rk.graph.on ()
 try ({
 	hist (<? echo ($x); getRK ("histogram_opt.code.calculate"); getRK ("histogram_opt.code.printout"); getRK ("plotoptions.code.printout"); ?>)
 <?	if (($densityscaled) && getRK_val ("density")) { ?>
-	lines(density(<? echo ($x); ?>, bw="<? echo ($bw); ?>", adjust = <? echo ($adjust); ?>, <? echo ($giveRkern); ?>, <? echo ($narm); ?>, n = <? getRK ("n"); ?>)<? getRK ("col_density.code.printout"); ?>)
+	lines(density(<? echo ($x); ?>, bw="<? echo ($bw); ?>", adjust = <? echo ($adjust); ?>, <? echo ($narm); ?>, n = <? getRK ("n"); ?>)<? getRK ("col_density.code.printout"); ?>)
 <?	} ?>
 })
 <?	if ($final) { ?>
