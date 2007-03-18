519924fe0373e2fe1c6062d3b74a31a363533ab5
Thomas Friedrichsmeier <thomas.friedrichsmeier@ruhr-uni-bochum.de>
2007-03-18 14:54:32 +0000

    Convert ECDF plot to use plot_stepfun_options plugin. Add help page
    svn path=/trunk/rkward/; revision=1613

--- rkward/plugins/plots/ecdf_plot.php
+++ rkward/plugins/plots/ecdf_plot.php
@@ -1,51 +1,43 @@
 <?
-	function preprocess () {
+function preprocess () {
 ?>
-rk.temp.range <- range (<? getRK("x"); ?>, na.rm=TRUE)
+yrange <- range (<? getRK("x"); ?>, na.rm=TRUE)
 <?
-	}
-	
-	function calculate () {
-	}
-	
-	function printout () {
-		doPrintout (true);
-	}
-	
-	function cleanup () {
-?>
-rm (rk.temp.range)
-<?
-	}
+}
 
-	function preview () {
-		preprocess ();
-		calculate ();
-		doPrintout (false);
-		cleanup ();
-	}
+function calculate () {
+}
 
-	function doPrintout ($final) {
-		$x = getRK_val ("x");
-		$col = getRK_val ("col");
+function printout () {
+	doPrintout (true);
+}
+
+function preview () {
+	preprocess ();
+	calculate ();
+	doPrintout (false);
+}
+
+function doPrintout ($final) {
+	$x = getRK_val ("x");
 	
-		if ($final) { ?>
-rk.header ("Empirical Cumulative Distribution Function", list ("Variable", rk.get.description (<? echo ($x); ?>), "Minimum", rk.temp.range[1], "Maximum", rk.temp.range[2]))
+	if ($final) { ?>
+rk.header ("Empirical Cumulative Distribution Function", list ("Variable", rk.get.description (<? echo ($x); ?>), "Minimum", yrange[1], "Maximum", yrange[2]))
 
 rk.graph.on ()
-<?		} ?>
+<?	} ?>
 try ({
-	plot.ecdf (<? echo ($x); ?>, <? getRK ("dopoints"); ?>, <? getRK ("verticals"); ?> <? getRK ("plotoptions.code.printout"); ?>)
+	plot.ecdf (<? echo ($x); ?>, <? getRK ("stepfun_options.code.printout"); ?><? getRK ("plotoptions.code.printout"); ?>)
 <?	if (getRK_val ("th_pnorm")) { ?>
-	curve (pnorm, from=rk.temp.range[1], to=rk.temp.range[2], add=TRUE, col="<? echo ($col); ?>")
+	curve (pnorm, from=yrange[1], to=yrange[2], add=TRUE, <? getRK ("col_thnorm.code.printout"); ?>)
 <?	}
 	if (getRK_val ("rug")) { ?>
-	rug (<? echo ($x); ?>, <? getRK ("ticksize"); ?>, <? getRK ("lwd"); ?>, <? getRK ("side"); ?>, col ="<? getRK ("col_rug"); ?>")
+	rug (<? echo ($x); ?>, <? getRK ("ticksize"); ?>, <? getRK ("lwd"); ?>, <? getRK ("side"); ?><? getRK ("col_rug.code.printout"); ?>)
 <?	} ?>
 })
-<?		if ($final) { ?>
+<?	if ($final) { ?>
 rk.graph.off ()
-<?		}
-	}
+<?	}
+}
 
 ?>
