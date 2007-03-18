85f53db82f00cc59e6d47ad08c81e9b5a6860974
Thomas Friedrichsmeier <thomas.friedrichsmeier@ruhr-uni-bochum.de>
2007-03-18 20:06:54 +0000

    Bring t test up to date: * allow to use for paired samples * adjust to running inside local * use rk.describe.alternative() * add help page * cleaner code * use copy tag for the wizard
    svn path=/trunk/rkward/; revision=1618

--- rkward/plugins/analysis/t_test_two_samples.php
+++ rkward/plugins/analysis/t_test_two_samples.php
@@ -1,45 +1,57 @@
 <?
-	function preprocess () {
-	}
-	
-	function calculate () {
-?>rk.temp.x <- substitute (<? getRK ("x"); ?>)
-rk.temp.y <- substitute (<? getRK ("y"); ?>)
-rk.temp <- t.test (eval (rk.temp.x), eval (rk.temp.y), "<? getRK ("hypothesis"); ?>"<?
-if (getRK_val ("varequal")) echo (", var.equal=TRUE");
-if (($conflevel = getRK_val ("conflevel")) != "0.95") echo (", conf.level=" . $conflevel); ?>)
-rk.temp.print.conf.level <- <? if (getRK_val ("confint")) echo "TRUE"; else echo "FALSE"; ?>
+function preprocess () {
+	global $x;
+	global $y;
 
-<?
-	}
-	
-	function printout () {
+	$x = getRK_val ("x");
+	$y = getRK_val ("y");
 ?>
-rk.header ("T-test (independent samples)", 
-	parameters=list ("Comparing", paste (rk.get.description (rk.temp.x, is.substitute=TRUE), "against", rk.get.description (rk.temp.y, is.substitute=TRUE)),
-	"H1", if (rk.temp$alternative == "less")
-		paste (rk.get.short.name (rk.temp.y), "is greater than", rk.get.short.name (rk.temp.x))
-	      else if (rk.temp$alternative == "greater")
-		paste (rk.get.short.name (rk.temp.x), "is greater than", rk.get.short.name (rk.temp.y))
-	      else
-		paste (rk.get.short.name (rk.temp.x), "and", rk.get.short.name (rk.temp.y), "differ"),
-	"Equal variances", "<? if (!getRK_val ("varequal")) echo "not"; ?> assumed"))
+names <- rk.get.description (<? echo ($x . ", " . $y); ?>)
+<?
+}
+
+function calculate () {
+	global $x;
+	global $y;
+	global $varequal;
+	global $paired;
+
+	$conflevel = getRK_val ("conflevel");
+	$varequal = getRK_val ("varequal");
+	$paired = getRK_val ("paired");
+	$hypothesis = getRK_val ("hypothesis");
+
+	$options = ", alternative=\"" . $hypothesis . "\"";
+	if ($paired) $options .= ", paired=TRUE";
+	if ((!$paired) && $varequal) $options .= ", var.equal=TRUE";
+	if ($conflevel != "0.95") $options .= ", conf.level=" . $conflevel;
+?>
+result <- t.test (<? echo ($x . ", " . $y . $options); ?>)
+<?
+}
+
+function printout () {
+	global $varequal;
+	global $paired;
+?>
+rk.header (result$method, 
+	parameters=list ("Comparing", paste (names[1], "against", names[2]),
+	"H1", rk.describe.alternative (result)<?
+	if (!$paired) { ?>
+,
+	"Equal variances", "<? if (!$varequal) echo ("not"); ?> assumed"<?
+	} ?>))
 
 rk.results (list (
-	'Variable Name'=rk.get.description (rk.temp.x, rk.temp.y, is.substitute=TRUE),
-	'estimated mean'=rk.temp$estimate,
-	'degrees of freedom'=rk.temp$parameter,
-	t=rk.temp$statistic,
-	p=rk.temp$p.value<?
+	'Variable Name'=names,
+	'estimated mean'=result$estimate,
+	'degrees of freedom'=result$parameter,
+	t=result$statistic,
+	p=result$p.value<?
 	if (getRK_val ("confint")) { ?>,
-	'confidence interval percent'=(100 * attr(rk.temp$conf.int, "conf.level")),
-	'confidence interval of difference'=rk.temp$conf.int <? } ?>))
+	'confidence interval percent'=(100 * attr(result$conf.int, "conf.level")),
+	'confidence interval of difference'=result$conf.int <? } ?>))
 <?
-	}
-	
-	function cleanup () {
-?>
-rm (list=grep ("^rk.temp", ls (), value=TRUE))
-<?
-	}
+}
+
 ?>
