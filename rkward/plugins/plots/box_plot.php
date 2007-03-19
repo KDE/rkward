d43d67400568c472479fe5d74ffee832a9e84569
Thomas Friedrichsmeier <thomas.friedrichsmeier@ruhr-uni-bochum.de>
2007-03-19 22:51:39 +0000

    Add preview to boxplot, PHP indentation
    svn path=/trunk/rkward/; revision=1648

--- rkward/plugins/plots/box_plot.php
+++ rkward/plugins/plots/box_plot.php
@@ -1,20 +1,31 @@
 <?
-	function preprocess () {
-	}
-	
-	function calculate () {
-	}
-	
-	function printout () {
+function preprocess () {
+}
+
+function calculate () {
+}
+
+function preview () {
+	preprocess ();
+	calculate ();
+	doPrintout (false);
+}
+
+function printout () {
+	doPrintout (true);
+}
+
+function doPrintout ($final) {
 	$xvarsstring = join (", ", split ("\n", getRK_val ("x")));
+
+	if ($final) {
 ?>
-rk.header ("Boxplot", list ("Variable(s)", rk.get.description (<?echo ($xvarsstring); ?>, paste.sep=", ")))
+rk.header ("Boxplot", list ("Variable(s)", rk.get.description (<? echo ($xvarsstring); ?>, paste.sep=", ")))
 rk.graph.on()
+<?	} ?>
 try (boxplot (list (<? echo ($xvarsstring); ?>), notch = <? getRK ("notch") ?>, outline = <? getRK("outline")?>, horizontal = <? getRK("orientation") ?><? getRK ("plotoptions.code.printout"); ?>))
+<?	if ($final) { ?>
 rk.graph.off ()
-<?
-	}
-	
-	function cleanup () {
-	}
+<?	}
+}
 ?>
