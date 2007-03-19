9a07239691dfaa0ed46b64fa58c805a10e4724e2
Thomas Friedrichsmeier <thomas.friedrichsmeier@ruhr-uni-bochum.de>
2007-03-19 19:57:21 +0000

    Add help files for load() and source() plugins, and some improvments to those
    svn path=/trunk/rkward/; revision=1640

--- rkward/plugins/00saveload/import/load_data.php
+++ rkward/plugins/00saveload/import/load_data.php
@@ -1,10 +1,19 @@
 <?
 function preprocess () {
 }
-	
+
 function calculate () {
-?>
-load (file="<? getRK("file"); ?>", envir=globalenv())
+	if (getRK_val ("other_env")) {
+		$other_env = true;
+		$envir = getRK_val ("envir");
+	} else {
+		$envir = "globalenv()";
+	}
+
+	if ($other_env) { ?>
+<? echo ($envir); ?> <<- new.env (parent=globalenv())
+<?	} ?>
+load (file="<? getRK("file"); ?>", envir=<? echo ($envir); ?>)
 <?
 }
 
