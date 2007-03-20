76892fcc2a8aa74e2c412642fbc73ca67c8cdf47
Thomas Friedrichsmeier <thomas.friedrichsmeier@ruhr-uni-bochum.de>
2007-03-20 00:00:29 +0000

    Update density plot: * adjustments to local() * plot only either regular density plot, or highest density regions plot. Both at once should rarely be useful * remove the 'no estimation' option. It is not useful in this context * properly enable/disable elements according to whether they are needed * some layout adjustments * assorted small fixes
    svn path=/trunk/rkward/; revision=1651

--- rkward/plugins/plots/density_plot.php
+++ rkward/plugins/plots/density_plot.php
@@ -1,55 +1,60 @@
 <?
-	function preprocess () {
-	}
-	
-	function calculate () {
-	}
-	
-	function printout () {
-	$giveRkern = getRK_val ("giveRkern");
-	$adjust = getRK_val ("adjust");
-	$bw = getRK_val ("bw");
-	$x = getRK_val ("x");
-	$narm = getRK_val ("narm");
-	$kern = "c(\"" . getRK_val ("kern") . "\")";
-	$dodensity_plot = getRK_val ("dodensity_plot");
-	$dohdrcde_plot = getRK_val ("dohdrcde_plot");
-	$dorug_density = getRK_val ("rug_density");
-	$dorug_hdrcde = getRK_val ("rug_hdrcde");
+function preprocess () {
+	global $dodensity_plot;
+	$dodensity_plot = (getRK_val ("plot_type") == "density_plot");
 
-	if ($dodensity_plot) { ?>
-rk.header ("Density Plot", list ("Variable", rk.get.description (<? echo ($x); ?>), "Band Width", c("<? getRK ("bw"); ?>"), "Estimate Density", <? echo ($giveRkern) ?>, "Adjust", <? echo ($adjust) ?>, "Remove Missing Values", <? echo ($narm) ?>, "Length", length (<? echo ($x); ?>), "n", <? getRK ("n"); ?>, "Smoothing Kernel", <? echo ($kern); ?>))
+	if (!$dodensity_plot) { ?>
+require(hdrcde)
 <?	}
-	if ($dohdrcde_plot) { ?>
+}
+
+function calculate () {
+}
+
+function preview () {
+	preprocess ();
+	calculate ();
+	doPrintout (false);
+}
+
+function printout () {
+	doPrintout (true);
+}
+
+function doPrintout ($final) {
+	global $dodensity_plot;
+	$adjust = getRK_val ("adjust");
+	$x = getRK_val ("x");
+	$resolution = getRK_val ("n");
+	$narm = getRK_val ("narm");
+	$kern = getRK_val ("kern");
+	if ($kern == "gaussian") {
+		$bw = getRK_val ("bw");
+	}
+	$dorug = getRK_val ("rug");
+
+	if ($final) {
+		if ($dodensity_plot) { ?>
+rk.header ("Density Plot", list ("Variable", rk.get.description (<? echo ($x); ?>)<? if (!empty ($bw)) { ?>, "Band Width", "<? echo ($bw); ?>"<? } ?>, "Adjust", <? echo ($adjust) ?>, "Remove Missing Values", <? echo ($narm) ?>, "Length", length (<? echo ($x); ?>), "Resolution", <? echo ($resolution); ?>, "Smoothing Kernel", "<? echo ($kern); ?>"))
+<?		} else { ?>
 rk.header ("Highest density regions", list ("Variable", rk.get.description (<? echo ($x); ?>)))
-<?	} ?>	
+<?		} ?>
 
 rk.graph.on ()
-try ({
-<?	if ($dohdrcde_plot && $dodensity_plot) { ?>
-	par(mfrow=c(1,2))
-<?	} ?>
-
-<?	if ($dodensity_plot) { ?>
-	plot (density(<? echo ($x); ?>, bw = "<? echo ($bw); ?>", adjust = <? echo ($adjust); ?>, <? echo ($giveRkern); ?>, kern = <? echo ($kern); ?>, n = <? getRK ("n"); ?>, <? echo ($narm); ?><? getRK ("plotoptions.code.printout"); ?>))
-<?	} ?>
-
-<?	if ($dorug_density) { ?>
-	rug(<? echo ($x); ?>, <? getRK ("ticksize"); ?>, <? getRK ("lwd"); ?>, <? getRK ("side"); ?>, col ="<? getRK ("col_rug"); ?>")
 <?	}
-
-	if ($dohdrcde_plot) { ?>
-	require(hdrcde)
+		?>
+try ({
+<?	if ($dodensity_plot) { ?>
+	plot (density(<? echo ($x); if (!empty ($bw)) echo (", bw=\"" . $bw . "\""); ?>, adjust = <? echo ($adjust); ?>, kern = "<? echo ($kern); ?>", n = <? echo ($resolution); ?>, <? echo ($narm); ?>)<? getRK ("plotoptions.code.printout"); ?>)
+<?	} else { ?>
 	hdr.den(<? echo ($x); ?><? getRK ("plotoptions.code.printout"); ?>)
 <?	}
-	if ($dorug_hdrcde) { ?>
-	rug(<? echo ($x); ?>, <? getRK ("ticksize"); ?>, <? getRK ("lwd"); ?>, <? getRK ("side"); ?>, col ="<? getRK ("col_rug"); ?>")
-	<? } ?>
+	if ($dorug) { ?>
+	rug(<? echo ($x); ?>, <? getRK ("rug_ticksize"); ?>, <? getRK ("rug_lwd"); ?>, <? getRK ("rug_side"); ?><? getRK ("rug_col.code.printout"); ?>)
+<?	} ?>
 })
+<?	if ($final) { ?>
 rk.graph.off ()
-<?
-	}
-	
-	function cleanup () {
-	}
+<?	}
+}
 ?>
