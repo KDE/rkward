6e8ef56c0960db5bb86d7aede1036ae7e57c367d
Thomas Friedrichsmeier <thomas.friedrichsmeier@ruhr-uni-bochum.de>
2007-03-21 20:08:59 +0000

    local() adjustments, and disable ellipse and smooth by default in scatterplot matrix plugin, as it may fail sometimes
    svn path=/trunk/rkward/; revision=1690

--- rkward/plugins/plots/scatterplot_matrix.php
+++ rkward/plugins/plots/scatterplot_matrix.php
@@ -12,30 +12,23 @@ function printout () {
 	doPrintout (true);
 }
 
-function cleanup () {
-?>
-rm(rk.temp.x)
-<?
-}
-
 function preview () {
 	preprocess ();
 	calculate ();
 	doPrintout (false);
-	cleanup ();
 }
 
 function doPrintout ($final) {
 	$vars = str_replace ("\n", ",", trim (getRK_val ("x"))) ;
 ?>
-rk.temp.x <- data.frame (<? echo ($vars); ?>)
+data <- data.frame (<? echo ($vars); ?>)
 
 <? 	if ($final) { ?>
 rk.header ("Scatterplot Matrix", parameters=list ("Diagonal Panels", "<? getRK("diag") ?>", "Plot points", "<? getRK ("plot_points"); ?>", "Smooth", "<? getRK ("smooth"); ?>", "Ellipses", "<? getRK ("ellipse"); ?> at 0.5 and 0.9 levels."))
 
 rk.graph.on ()
 <?	} ?>
-try (scatterplot.matrix(rk.temp.x, diagonal="<? getRK("diag") ?>", plot.points=<? getRK ("plot_points"); ?>, smooth=<? getRK ("smooth"); ?>, ellipse=<? getRK ("ellipse"); ?>))
+try (scatterplot.matrix(data, diagonal="<? getRK("diag") ?>", plot.points=<? getRK ("plot_points"); ?>, smooth=<? getRK ("smooth"); ?>, ellipse=<? getRK ("ellipse"); ?>))
 <?	if ($final) { ?>
 rk.graph.off ()
 <?	}
