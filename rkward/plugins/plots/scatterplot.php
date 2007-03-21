0902ba0fd43452c3000aa3a8795b63a439cd99d2
Thomas Friedrichsmeier <thomas.friedrichsmeier@ruhr-uni-bochum.de>
2007-03-21 19:52:28 +0000

    Clean up scatterplot plugin * local adjustments * some names changed * use plot_options where possible (unfortunately, plot_options does not offer different options per var, yet)
    svn path=/trunk/rkward/; revision=1689

--- rkward/plugins/plots/scatterplot.php
+++ rkward/plugins/plots/scatterplot.php
@@ -1,54 +1,38 @@
 <?
 function preprocess () {
-?>
-rk.temp <- list ()
-<?
 }
 
 function calculate () {
-	global $Xname;
-	global $Yname;
-	global $main;
-	global $sub;
-
 	$x = str_replace ("\n", ",", trim (getRK_val ("x"))) ;
 	$y = str_replace ("\n", ",", trim (getRK_val ("y"))) ;
 	
-	/** fetch some values which are needed in more than one place, to avoid mulitple transfer */
-	$type = getRK_val ("type");
-	$typeCusto = getRK_val ("typeCusto");
-	if ($type == "custoType") $type_string = $typeCusto;
-	else $type_string = $type;
+	if (getRK_val ("manual_type") == "true") {
+		$type = getRK_val ("custom_type");
+	} else {
+		$type = "c ('" . getRK_val ("pointtype") . "')";
+	}
 	$col = getRK_val ("col");
 	$pch = getRK_val ("pch");
 	$cex = getRK_val ("cex");
-	if (getRK_val("isXaxis") == "1") $Xname = getRK_val ("Xname"); else $Xname = "";
-	if (getRK_val("isYaxis") == "1") $Yname = getRK_val ("Yname"); else $Yname = "";
-	if (getRK_val("isTitle") == "1") $main = getRK_val ("main"); else $main = "";
-	if (getRK_val("isSub") == "1") $sub = getRK_val ("sub"); else $sub = "";
 ?>
 
 <? #input ?>
-rk.temp$Xvar <- list(<? echo ($x) ;?>)
-rk.temp$Yvar <- list(<? echo ($y) ;?>)
-rk.temp$Xval <- <? if (getRK_val("columns") == "custoCol" ) echo (getRK_val("Xscale") . "\n"); else echo ("c(" . $x . ")\n"); ?>
-rk.temp$Yval <- <? if (getRK_val("rows") == "custoRow" ) echo (getRK_val("Yscale") . "\n"); else echo ("c(" . $y . ")\n"); ?>
+Xvars <- list(<? echo ($x) ;?>)
+Yvars <- list(<? echo ($y) ;?>)
 
 <? # verification (is this needed?) ?>
-rk.temp$ok <- TRUE
-if (length(rk.temp$Xvar) != length(rk.temp$Yvar)) {
-	rk.temp$ok <- FALSE ;
+if (length(Xvars) != length(Yvars)) {
 	stop("Unequal number of X and Y variables given")
 }
 
 # find range of X/Y values needed
-rk.temp$Xdef <- range (rk.temp$Xval, na.rm=TRUE)
-rk.temp$Ydef <- range (rk.temp$Yval, na.rm=TRUE)
+Xrange <- range (c (Xvars), na.rm=TRUE)
+Yrange <- range (c (Yvars), na.rm=TRUE)
 
-rk.temp$type <- rep (<? echo ($type_string); ?>, length.out=length (rk.temp$Xvar));
-rk.temp$col <- rep (<? echo ($col); ?>, length.out=length (rk.temp$Xvar));
-rk.temp$cex <- rep (<? echo ($cex); ?>, length.out=length (rk.temp$Xvar));
-rk.temp$pch <- rep (<? echo ($pch); ?>, length.out=length (rk.temp$Xvar));
+type <- rep (<? echo ($type); ?>, length.out=length (Xvars));
+col <- rep (<? echo ($col); ?>, length.out=length (Xvars));
+cex <- rep (<? echo ($cex); ?>, length.out=length (Xvars));
+pch <- rep (<? echo ($pch); ?>, length.out=length (Xvars));
 <?
 }
 
@@ -56,50 +40,35 @@ function printout () {
 	doPrintout (true);
 }
 
-function cleanup () {
-?>
-rm(rk.temp)
-rm(rk.temp.iterator)
-<?
-}
-
 function preview () {
 	preprocess ();
 	calculate ();
 	doPrintout (false);
-	cleanup ();
 }
 
 function doPrintout ($final) {
-	global $Xname;
-	global $Yname;
-	global $main;
-	global $sub;
-?>
-if (!rk.temp$ok) stop ()
-
-<? if ($final) { ?>
+	if ($final) { ?>
 rk.graph.on()
 
 <?	} ?>
 try ({
 	# make frame and axes
-	plot(rk.temp$Xdef, rk.temp$Ydef, type="n", xlab = "<? echo ($Xname); ?>", ylab = "<? echo ($Yname); ?>", main = "<? echo ($main); ?>", sub = "<? echo ($sub); ?>", axes = <? getRK("axes") ;?>, log = "<? getRK("logX") ; getRK("logY") ; ?>")
+	plot(Xrange, Yrange, type="n"<? getRK ("plotoptions.code.printout"); ?>)
 	
 	# plot variables one X/Y pair at a time
-	for (rk.temp.iterator in 1:length(rk.temp$Xvar)) {
+	for (i in 1:length(Xvars)) {
 		points (
-			rk.temp$Xvar[[rk.temp.iterator]],
-			rk.temp$Yvar[[rk.temp.iterator]],
-			type = rk.temp$type[[rk.temp.iterator]],
-			col = rk.temp$col[[rk.temp.iterator]],
-			cex = rk.temp$cex[[rk.temp.iterator]],
-			pch = rk.temp$pch[[rk.temp.iterator]]
+			Xvars[[i]],
+			Yvars[[i]],
+			type = type[[i]],
+			col = col[[i]],
+			cex = cex[[i]],
+			pch = pch[[i]]
 		)
 	}
 })
+<?	if ($final) { ?>
 
-<? if ($final) { ?>
 rk.graph.off()
 <?	}
 }
