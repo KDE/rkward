b833d4321c3eb0dc8f0a64aae48e1a8003fe98ef
Thomas Friedrichsmeier <thomas.friedrichsmeier@ruhr-uni-bochum.de>
2007-03-21 23:35:29 +0000

    Some touchups to bonett, agostino, and ansombe: * Move length and NA out of the try block. This allows them to be shown, even if the test fails * Make showing NAs the default. I think showing length but not NAs may be misleading, and so should not be the default setting * Change option labels for 'alternative' to just 'two-sided', 'greater', 'less'. The old labels were not technically correct * Add an option to turn off the display of verbose alternative hypothesis for each var * Add a stretch on the second tab to make layout look better
    svn path=/trunk/rkward/; revision=1695

--- rkward/plugins/analysis/moments/agostino_test.php
+++ rkward/plugins/analysis/moments/agostino_test.php
@@ -5,7 +5,9 @@ require(moments)
 }
 
 function calculate () {
-	$vars = "substitute (" . str_replace ("\n", "), substitute (", trim (getRK_val ("x"))) . ")";
+	global $alternative;
+
+	$vars = "substitute (" . str_replace ("\n", "), substitute (", trim (getRK_val ("x"))) . ")";	$alternative = getRK_val ("alternative");
 ?>
 
 vars <- list (<? echo ($vars); ?>)
@@ -16,28 +18,31 @@ for (i in 1:length(vars)) {
 	var <- eval (vars[[i]], envir=globalenv ())
 	results[i, 'Error'] <- tryCatch ({
 		# This is the core of the calculation
-		t <- agostino.test (var, alternative = "<? getRK ("alternative"); ?>")
+		t <- agostino.test (var, alternative = "<? echo ($alternative); ?>")
 		results[i, 'skewness estimator (skew)'] <- t$statistic["skew"]
 		results[i, 'transformation (z)'] <- t$statistic["z"]
 		results[i, 'p-value'] <- t$p.value
-		results[i, 'Alternative Hypothesis']<- rk.describe.alternative (t)
-<?	if (getRK_val ("length")) { ?>
-		results[i, 'Length'] <- length (var)
-<?	}
-	if (getRK_val ("nacount")) { ?>
-		results[i, 'NAs'] <- length (which(is.na(var)))
-<? 	} ?>
+<? 	if (getRK_val ("show_alternative")) { ?>
+		results[i, 'Alternative Hypothesis'] <- rk.describe.alternative (t)
+<?	} ?>
 		NA				# no error
 	}, error=function (e) e$message)	# catch any errors
+<?	if (getRK_val ("length")) { ?>
+	results[i, 'Length'] <- length (var)
+<?	}
+	if (getRK_val ("nacount")) { ?>
+	results[i, 'NAs'] <- length (which(is.na(var)))
+<? 	} ?>
 }
 if (all (is.na (results$'Error'))) results$'Error' <- NULL
 <?
 }
 
 function printout () {
+	global $alternative;
 ?>
 rk.header ("D'Agostino test of skewness",
-	parameters=list ("Alternative Hypothesis", "<? getRK ("alternative"); ?>"))
+	parameters=list ("Alternative Hypothesis", "<? echo ($alternative); ?>"))
 rk.results (results)
 <?
 }
--- rkward/plugins/analysis/moments/bonett_test.php
+++ rkward/plugins/analysis/moments/bonett_test.php
@@ -5,8 +5,10 @@ require(moments)
 <?}
 
 function calculate () {
-	$vars = "substitute (" . str_replace ("\n", "), substitute (", trim (getRK_val ("x"))) . ")";
+	global $alternative;
 
+	$vars = "substitute (" . str_replace ("\n", "), substitute (", trim (getRK_val ("x"))) . ")";
+	$alternative = getRK_val ("alternative");
 ?>
 
 vars <- list (<? echo ($vars); ?>)
@@ -16,28 +18,31 @@ for (i in 1:length(vars)) {
 	results[i, 'Variable Name'] <- rk.get.description (vars[[i]], is.substitute=TRUE)
 	var <- eval (vars[[i]], envir=globalenv ())
 	results[i, 'Error'] <- tryCatch ({
-		t <- bonett.test (var, alternative = "<? getRK ("alternative"); ?>")
+		t <- bonett.test (var, alternative = "<? echo ($alternative); ?>")
 		results[i, 'Kurtosis estimator (tau)'] <- t$statistic["tau"]
 		results[i, 'Transformation (z)'] <- t$statistic["z"]
 		results[i, 'p-value'] <- t$p.value
-		results[i, 'Alternative Hypothesis']<- rk.describe.alternative (t)
+<? 	if (getRK_val ("show_alternative")) { ?>
+		results[i, 'Alternative Hypothesis'] <- rk.describe.alternative (t)
+<?	} ?>
+		NA				# no error
+	}, error=function (e) e$message)	# catch any errors
 <?	if (getRK_val ("length")) { ?>
 		results[i, 'Length'] <- length (var)
 <?	}
 	if (getRK_val ("nacount")) { ?>
 		results[i, 'NAs'] <- length (which(is.na(var)))
 <? 	} ?>
-		NA				# no error
-	}, error=function (e) e$message)	# catch any errors
 }
 if (all (is.na (results$'Error'))) results$'Error' <- NULL
 <?
 }
 
 function printout () {
+	global $alternative;
 ?>
 rk.header ("Bonett-Seier test of Geary's kurtosis",
-	parameters=list ("Alternative Hypothesis", "<? getRK ("alternative"); ?>"))
+	parameters=list ("Alternative Hypothesis", "<? echo ($alternative); ?>"))
 rk.results (results)
 <?
 }
