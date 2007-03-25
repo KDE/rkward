48ff0fe77d7ccd76811f1eb9aeb43c02f7f58fd7
Thomas Friedrichsmeier <thomas.friedrichsmeier@ruhr-uni-bochum.de>
2007-03-25 22:16:58 +0000

    NA handling
    svn path=/trunk/rkward/; revision=1725

--- rkward/plugins/analysis/outliers/grubbs_test.php
+++ rkward/plugins/analysis/outliers/grubbs_test.php
@@ -12,9 +12,17 @@ vars <- list (<? echo ($vars); ?>)
 results <- data.frame ('Variable Name'=rep (NA, length (vars)), check.names=FALSE)
 for (i in 1:length(vars)) {
 	results[i, 'Variable Name'] <- rk.get.description (vars[[i]], is.substitute=TRUE)
-	var <- na.omit(eval (vars[[i]], envir=globalenv ()))
-	# var_w_na is similiar to var but NAs are not removed
-	var_w_na <- eval (vars[[i]], envir=globalenv ())
+<?	if (getRK_val ("length")) { ?>
+	var <- eval (vars[[i]], envir=globalenv ())
+
+	results[i, 'Length'] <- length (var)
+	results[i, 'NAs'] <- sum (is.na(var))
+
+	var <- na.omit (var) 	# omit NAs for all further calculations
+<? 	} else { ?>
+	var <- eval (vars[[i]], envir=globalenv ())
+<?	} ?>
+
 	results[i, 'Error'] <- tryCatch ({
 		# This is the core of the calculation
 		t <- grubbs.test (var, type = <? getRK ("type"); ?>, opposite = <? getRK ("opposite"); ?>, two.sided = <? getRK ("two_sided"); ?>)
@@ -24,25 +32,19 @@ for (i in 1:length(vars)) {
 		results[i, 'Alternative Hypothesis']<- rk.describe.alternative (t)
 <?	if (getRK_val ("mean")) { ?>
 		results[i, 'Mean'] <- mean (var)
-<?	} ?>
-<?	if (getRK_val ("sd")) { ?>
+<?	}
+	if (getRK_val ("sd")) { ?>
 		results[i, 'Standard Deviation'] <-  sd (var)
-<?	} ?>
-<?	if (getRK_val ("median")) { ?>
+<?	}
+	if (getRK_val ("median")) { ?>
 		results[i, 'Median'] <- median (var)
-<?	} ?>
-<?	if (getRK_val ("min")) { ?>
+<?	}
+	if (getRK_val ("min")) { ?>
 		results[i, 'Minimum'] <- min (var)
-<?	} ?>
-<?	if (getRK_val ("max")) { ?>
+<?	}
+	if (getRK_val ("max")) { ?>
 		results[i, 'Maximum'] <- max (var)
 <?	} ?>
-<?	if (getRK_val ("length")) { ?>
-		results[i, 'Length'] <- length (var)
-<?	}
-	if (getRK_val ("nacount")) { ?>
-		results[i, 'NAs'] <- length (which(is.na(var_w_na)))
-<? 	} ?>
 		NA				# no error
 	}, error=function (e) e$message)	# catch any errors
 }
