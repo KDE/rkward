211bf07dac867f1bdbdd35bb338314d60ec9ef83
Thomas Friedrichsmeier <thomas.friedrichsmeier@ruhr-uni-bochum.de>
2007-03-26 17:57:02 +0000

    Adjustments to mood.test * local() adjustments * remove unused tabbook * show alternative only in rk.header, names only in rk.results * reworded help a bit
    svn path=/trunk/rkward/; revision=1734

--- rkward/plugins/analysis/TESTS/mood_test.php
+++ rkward/plugins/analysis/TESTS/mood_test.php
@@ -3,32 +3,22 @@ function preprocess () {
 }
 
 function calculate () {
-
 ?>
-rk.temp.x <- substitute (<? getRK ("x"); ?>)
-rk.temp.y <- substitute (<? getRK ("y"); ?>)
-rk.temp <- mood.test (eval (rk.temp.x), eval (rk.temp.y), alternative = "<? getRK ("alternative"); ?>")
-
+result <- mood.test (<? getRK ("x"); ?>, <? getRK ("y"); ?>, alternative = "<? getRK ("alternative"); ?>")
 <?
 }
 
 function printout () {
 ?>
-rk.header ("Mood Two-Sample Test of Scale",
-	parameters=list ("Comparing", paste (rk.get.description (rk.temp.x, is.substitute=TRUE), "and", rk.get.description (rk.temp.y, is.substitute=TRUE)), "Alternative Hypothesis", "<? getRK ("alternative"); ?>"))
+names <- rk.get.description (<? getRK ("x"); ?>, <? getRK ("y"); ?>)
+
+rk.header (result$method,
+	parameters=list ("Alternative Hypothesis", rk.describe.alternative (result)))
 
 rk.results (list (
-	'Variables'=rk.get.description (rk.temp.x, rk.temp.y, is.substitute=TRUE),
-	'Z'=rk.temp$statistic,
-	'p-value'=rk.temp$p.value,
-	'Alternative Hypothesis'=rk.describe.alternative(rk.temp)
-	))
-<?
-}
-
-function cleanup () {
-?>
-rm (list=grep ("^rk.temp", ls (), value=TRUE))
+	'Variables'=names,
+	'Z'=result$statistic,
+	'p-value'=result$p.value))
 <?
 }
 ?>
