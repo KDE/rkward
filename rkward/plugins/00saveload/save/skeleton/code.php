c7aca6fd72dd8c57e3de868463d0d5ebdd801a2b
Prasenjit Kapat <kapatp@gmail.com>
2007-03-30 23:08:31 +0000

    Quotes error and properly renaming the menu entry
    svn path=/trunk/rkward/; revision=1771

--- rkward/plugins/00saveload/save/skeleton/code.php
+++ rkward/plugins/00saveload/save/skeleton/code.php
@@ -5,7 +5,7 @@ function preprocess () {
 function calculate () {
 $vars = str_replace ("\n", "','", trim (getRK_val ("data"))) ;
 ?>
-package.skeleton(name="<? getRK("name"); ?>", list=c('<? echo ($vars); ?>'), path="<? getRK("path"); ?>", force= "<? getRK("force"); ?>")
+package.skeleton(name="<? getRK("name"); ?>", list=c('<? echo ($vars); ?>'), path="<? getRK("path"); ?>", force= <? getRK("force"); ?>)
 <?
 }
 
