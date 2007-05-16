b209a96832e66f363ce6164f653f9aa84ba1a38a
Thomas Friedrichsmeier <thomas.friedrichsmeier@ruhr-uni-bochum.de>
2007-05-16 16:56:15 +0000

    Restructure crosstabs plugin, and make it use barplot_embed. Not entirely happy with some aspects, yet
    svn path=/trunk/rkward/; revision=1906

--- rkward/plugins/analysis/crosstab.php
+++ rkward/plugins/analysis/crosstab.php
@@ -6,75 +6,71 @@ require(xtable)
 }
 	
 function calculate () {
+	$x = getRK_val ("x") ;
+	$yvarsstring = join (", ", split ("\n", getRK_val ("y")));
+	$labels = getRK_val ("labels")=="TRUE";
+
+?>
+x <- <? echo ($x . "\n"); ?>
+yvars <- list (substitute (<? echo ($yvarsstring); ?>))
+results <- list()
+descriptions <- list ()
+
+# calculate crosstabs
+for (i in length (yvars)) {
+	yvar <- eval (yvars[[i]], envir=globalenv ())
+	results[[i]] <- table(x, yvar)
+
+	descriptions[[i]] <- list ('Dependent'=rk.get.description (<? echo ($x); ?>), 'Independent'=rk.get.description (yvars[[i]], is.substitute=TRUE))
+}
+<?
+	if (getRK_val ("chisq") == "TRUE") { ?>
+
+# calculate chisquares
+chisquares <- list ()
+for (i in length (results)) {
+	chisquares[[i]] <- chisq.test (results[[i]], simulate.p.value = <? getRK ("simpv");?> <?if (getRK_val ("monte") == "TRUE") { ?>,B=(<? getRK ("B"); ?>) <?}?>)
+}
+<?	}
+
 }
 
 function printout () {
 	doPrintout (true);
 }
-	
-	
+
 function preview () {
 	preprocess ();
 	calculate ();
 	doPrintout (false);
 }
-	
+
 function doPrintout ($final) {
-	$x = getRK_val ("x") ;
-	$yvarsstring = join (", ", split ("\n", getRK_val ("y")));
-	$labels = getRK_val ("labels")=="TRUE";
-	
-?>
-x <- (<? echo ($x); ?>)
-y <- data.frame (<? echo ($yvarsstring); ?>)
+	if ($final) { ?>
+for (i in 1:length (results)) {
+	rk.header ("Crosstabs", parameters=list ("Dependent", descriptions[[i]][['Dependent']], "Independent", descriptions[[i]][['Independent']]))
+	rk.print (xtable (cbind (results[[i]]), digits=0))
+<?		if (getRK_val ("chisq") == "TRUE") { ?>
 
-for (i in 1:dim(y)[2]){
-xy<-table(y[,i],x)
-rk.header ("Crosstabs", list ("Dependent", rk.get.description (<? echo ($x); ?>), "Independent", rk.get.description (<? echo ($yvarsstring); ?>)[i]))
-
-<?	if ($final) { ?>
-rk.print(xtable(cbind(xy), digits=0))
-<?	if (getRK_val ("chisq") == "TRUE") { ?>
-rk.header ("Pearson's Chi Square Test for Crosstabs", list ("Dependent", rk.get.description (<? echo ($x); ?>), "Independent", rk.get.description (<? echo ($yvarsstring); ?>)[i], "Simulate p vlaue", "<? getRK ("simpv");?>", "Monte Carlo", "<? getRK ("monte"); ?>" <? if (getRK_val ("monte") == "TRUE") { ?>,  "Number of replicates", <? getRK ("B"); }?> ))
-xsquared<-cbind(
-chisq.test(xy, simulate.p.value = <? getRK ("simpv");?> <?if (getRK_val ("monte") == "TRUE") { ?>,B=(<? getRK ("B"); ?>) <?}?> )$statistic,
-<?if (getRK_val ("simpv") == "FALSE") {?> chisq.test(xy, simulate.p.value = <? getRK ("simpv");?> <?if (getRK_val ("monte") == "TRUE") { ?>,B=(<? getRK ("B"); ?>) <?}?> )$parameter, <?}?>
-chisq.test(xy, simulate.p.value = <? getRK ("simpv");?> <?if (getRK_val ("monte") == "TRUE") { ?>,B=(<? getRK ("B"); ?>) <?}?> )$p.value)
- colnames(xsquared)<-c("Statistic", <?if (getRK_val ("simpv") == "FALSE") {?> "df", <?}?> "p-value")
- rk.print(xtable(xsquared))
-<? } ?>
-<?	if (getRK_val ("barplot") == "TRUE") { ?>
-rk.header ("Barplot for Crosstabs", list ("Dependent", rk.get.description (<? echo ($x); ?>), "Independent", rk.get.description (<? echo ($yvarsstring); ?>)[i] <? if (getRK_val ("barplot") == "TRUE") { ?> , "Rainbow colors", "<? getRK ("rainbow"); ?>", "Beside", "<? getRK ("beside"); ?>", "Legend", "<? getRK ("legend"); ?>"<? } ?>)) 
-rk.graph.on ()
-<?	} 
-}
-if (getRK_val ("barplot") == "TRUE") {
-?>
-try ({
-par(bg="gray95") 
-<?	if (getRK_val ("beside") == "TRUE") { ?>
-<?		if ($labels) { ?>
-# adjust the range so that the labels will fit
-yrange <- range (xy, na.rm=TRUE) * 1.2
-if (yrange[1] > 0) yrange[1] <- 0
-if (yrange[2] < 0) yrange[2] <- 0
-<?		} ?>
-bplot <- barplot((xy)<? if (getRK_val ("rainbow")=="TRUE") { ?>, col=rainbow( if(is.matrix(xy)) dim(xy) else length(xy)) <? } ?>, beside=<? getRK ("beside"); ?>, legend.text=<? getRK ("legend"); ?><? if ($labels) echo (", ylim = yrange"); ?>)
-<?		if ($labels) { ?>
-text(bplot, xy, labels=xy, pos=<? getRK ("place"); ?>, offset=.5)
+	rk.header ("Pearson's Chi Square Test for Crosstabs", list ("Dependent", descriptions[[i]][['Dependent']], "Independent", descriptions[[i]][['Independent']], "Simulate p vlaue", "<? getRK ("simpv");?>", "Monte Carlo", "<? getRK ("monte"); ?>" <? if (getRK_val ("monte") == "TRUE") { ?>,  "Number of replicates", <? getRK ("B"); }?> ))
+	rk.results (list ('Statistic'=chisquares[[i]][['statistic']], 'df'=chisquares[[i]][['parameter']], 'p'=chisquares[[i]][['p.value']]))
 <?		}
-	} else { ?>
-barplot((xy)<? if (getRK_val ("rainbow")=="TRUE") { ?>, col=rainbow( if(is.matrix(xy)) dim(xy) else length(xy)) <? } ?>, legend.text=<? getRK ("legend"); ?>)
-<?	}
-	?>
-})
 
-<?	}
-if ($final) { ?>
-<?	if (getRK_val ("barplot") == "TRUE") { ?>
-rk.graph.off ()
-<? } 
-} ?>
+		if (getRK_val ("barplot") == "TRUE") { ?>
+
+	rk.header ("Barplot for Crosstabs", list ("Dependent", descriptions[[i]][['Dependent']], "Independent", descriptions[[i]][['Independent']]<? getRK ('barplot_embed.code.preprocess'); ?>))
+	rk.graph.on ()
+	try ({
+<?			printIndented ("\t\t", getRK_val ('barplot_embed.code.printout')); ?>
+	})
+	rk.graph.off ()
+<?		} ?>
 }
-<? }
+<?	} else {
+		// produce a single barplot of the first result ?>
+i <- 1
+<? getRK ('barplot_embed.code.printout'); ?>
+<?	}
+
+}
 ?>
