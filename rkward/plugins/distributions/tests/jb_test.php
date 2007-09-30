3d44957028e4525dca2a0f922158dd31c8ae9923
Thomas Friedrichsmeier <thomas.friedrichsmeier@ruhr-uni-bochum.de>
2007-09-30 17:00:24 +0000

    NOT FOR 0.4.8: Add Jarque-Bera test (by German Marquez Mejia)
    svn path=/trunk/rkward/; revision=2010

---
+++ rkward/plugins/distributions/tests/jb_test.php
@@ -0,0 +1,34 @@
+<?
+function preprocess () { ?>
+require(tseries)
+<?
+}
+
+function calculate () {
+	$vars = "substitute (" . str_replace ("\n", "), substitute (", trim (getRK_val ("x"))) . ")";
+
+?>
+vars <- list (<? echo ($vars); ?>)
+results <- data.frame ('Variable Name'=rep (NA, length (vars)), check.names=FALSE)
+for (i in 1:length (vars)) {
+	results[i, 'Variable Name'] <- rk.get.description (vars[[i]], is.substitute=TRUE)
+	var <- eval (vars[[i]], envir=globalenv ())
+<?	if (getRK_val ("length")) { ?>
+	results[i, 'Length'] <- length (var)
+<?	} ?>
+	try ({
+		test <- jarque.bera.test (var)
+		results[i, 'Statistic'] <- paste (names (test$statistic), test$statistic, sep=" = ")
+		results[i, 'p-value'] <- test$p.value
+	})
+}
+<?
+}
+
+function printout () {
+?>
+rk.header ("Jarque-Bera Normality Test", parameters=list ("Degrees of freedom", test$parameter))
+rk.results (results)
+<?
+}
+?>
