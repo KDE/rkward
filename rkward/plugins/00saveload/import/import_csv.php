38709c16ac4d8527368ae48d899e4dde3c4f326a
Prasenjit Kapat <kapatp@gmail.com>
2007-11-02 08:02:24 +0000

    Modified Import Text/CSV plugin using read.{csv,csv2,delim,delim2}
    svn path=/trunk/rkward/; revision=2155

--- rkward/plugins/00saveload/import/import_csv.php
+++ rkward/plugins/00saveload/import/import_csv.php
@@ -8,14 +8,23 @@ function preprocess () {
 }
 
 function calculate () {
-	$dec = getRK_val ("dec");
-	if ($dec == "other") $dec = quoteString (getRK_val ("custom_dec"));
-	$sep = getRK_val ("sep");
-	if ($sep == "other") $sep = quoteString (getRK_val ("custom_sep"));
-	$quote = getRK_val ("quote");
-	if ($quote == "other") $quote = quoteString (getRK_val ("custom_quote"));
-
-getRK("name"); ?> <<- read.table (file="<? getRK("file"); ?>", header=<? getRK("header"); ?>, sep=<? echo ($sep); ?>, quote=<? echo ($quote); ?>, dec=<? echo ($dec); ?>, <? # doing row names (what a pity...) 
+	$quick = getRK_val ("quick");
+	if ($quick == "table") {
+		$dec = getRK_val ("dec");
+		if ($dec == "other") $dec = quoteString (getRK_val ("custom_dec"));
+		$sep = getRK_val ("sep");
+		if ($sep == "other") $sep = quoteString (getRK_val ("custom_sep"));
+		$quote = getRK_val ("quote");
+		if ($quote == "other") $quote = quoteString (getRK_val ("custom_quote"));
+		$header = getRK_val ("header");
+		$fill = getRK_val ("fill");
+		$comchar = quoteString (getRK_val ("commentchar"));
+		$tableOptions = ", header={$header}, sep={$sep}, quote={$quote}, dec={$dec}, fill={$fill}, comment.char={$comchar}";
+	} else {
+		$tableOptions = "";
+	}
+// Other method is to use read.table and show all the options - more transparent
+getRK("name"); ?> <<- read.<? echo ($quick); ?> (file="<? getRK("file"); ?>"<? echo ($tableOptions); ?>, <? # doing row names (what a pity...) 
 	if (getRK_val("rowname")!="NULL") {
 		echo( "row.names = ");
 		if (getRK_val("rowname")=="rowcol") echo (getRK("nomrow") . ",");
@@ -26,7 +35,7 @@ getRK("name"); ?> <<- read.table (file="<? getRK("file"); ?>", header=<? getRK("
 	# doing col class (what a pity...)
 	if (getRK_val("colclass") == "custoClass") echo( "colClasses = " . getRK_val ("custoClasses") . ",");
 	#doing what is left
-?> na.strings = "<? getRK("na")  ?>", nrows = <? getRK("nrows") ; ?>, skip = <? getRK("skip") ; ?>, check.names = <? getRK("checkname") ; ?>, fill = <? getRK("fill") ; ?>, strip.white = <? getRK("stripwhite") ; ?>, blank.lines.skip = <? getRK("blanklinesskip") ; ?>, comment.char=<? echo (quoteString (getRK_val("commentchar"))); ?><? getRK("allow_escapes"); ?><? getRK("flush"); ?><? getRK("strings_as_factors"); ?>)
+?> na.strings = "<? getRK("na")  ?>", nrows = <? getRK("nrows") ; ?>, skip = <? getRK("skip") ; ?>, check.names = <? getRK("checkname") ; ?>, strip.white = <? getRK("stripwhite") ; ?>, blank.lines.skip = <? getRK("blanklinesskip") ; ?><? getRK("allow_escapes"); ?><? getRK("flush"); ?><? getRK("strings_as_factors"); ?>)
 <?	if (getRK_val ("doedit")) { ?>
 
 rk.edit (<? getRK ("name"); ?>)
