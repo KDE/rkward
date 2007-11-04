0affa2bfdc3ac8486b891a966a82cc525291738e
Stefan Rödiger <stefan_roediger@gmx.de>
2007-11-04 19:47:38 +0000

    enabled grid
    svn path=/trunk/rkward/; revision=2171

--- rkward/plugins/plots/stripchart_plot.php
+++ rkward/plugins/plots/stripchart_plot.php
@@ -22,6 +22,7 @@ function doPrintout ($final) {
 	$jitter = getRK_val ("jitter");
 	$offset = getRK_val ("offset");
 	$vertical = getRK_val ("vertical");
+	$plot_adds = getRK_val ("plotoptions.code.calculate"); //add grid and alike
 
 	if ($final) { ?>
 rk.header ("Stripchart", list ("Variable", rk.get.description (<? echo ($x); ?>), "Group", rk.get.description (<? echo ($g); ?>), "Method", <? echo ($method); ?>, "Jitter", <? echo ($jitter); ?>, "Plot drawn vertically", <? echo ($vertical); ?>, "Offset", <? echo ($offset); ?>))
@@ -29,6 +30,12 @@ rk.header ("Stripchart", list ("Variable", rk.get.description (<? echo ($x); ?>)
 rk.graph.on ()
 <?	} ?>
 try (stripchart (<? echo ($x); ?> ~ (<? echo ($g); ?>), vertical=<? echo ($vertical); ?>, method = <? echo ($method); ?>, jitter = <? echo ($jitter); ?>, offset = <? echo ($offset); ?><? getRK ("plotoptions.code.printout"); ?>))
+<?	if (!empty ($plot_adds)) { ?>
+
+<?		// print the grid() related code
+		printIndented ("\t", $plot_adds);
+	}
+?>
 <?	if ($final) { ?>
 rk.graph.off ()
 <?	}
