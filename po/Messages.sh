eba4be44fbd415b10f79995a3aada9b4c326e5e0
Thomas Friedrichsmeier <thomas.friedrichsmeier@ruhr-uni-bochum.de>
2007-11-18 22:23:10 +0000

    Message extraction/merging seems to work (mostly)
    svn path=/branches/KDE4_port/; revision=2243

--- po/Messages.sh
+++ po/Messages.sh
@@ -1,19 +1,39 @@
 #!/bin/sh
-BASEDIR="../rkward/"
-DIR=`pwd`
+BASEDIR="../rkward/"	# root of translatable sources
+PROJECT="rkward"	# project name
+WDIR=`pwd`		# working dir
+
+
+echo "Preparing rc files"
+cd ${BASEDIR}
+find . -name '*.rc' -o -name '*.ui' -o -name '*.kcfg' | sort > ${WDIR}/rcfiles.list
+xargs --arg-file=${WDIR}/rcfiles.list extractrc > ${WDIR}/extractedrc.cpp
+cd ${WDIR}
+echo "Done preparing rc files"
+
 
 echo "Extracting messages"
-
 cd ${BASEDIR}
-find . -name '*.cpp' -o -name '*.h' -o -name '*.c' | sort > ${DIR}/infiles.list
-cd ${DIR}
-xgettext -C -ci18n -ki18n -ktr2i18n -kI18N_NOOP -kI18N_NOOP2 --files-from=infiles.list -D ${BASEDIR} -o rkward.pot
-rm infiles.list
+find . -name '*.cpp' -o -name '*.h' -o -name '*.c' | sort > ${WDIR}/infiles.list
+echo "extractedrc.cpp" >> ${WDIR}/infiles.list
+cd ${WDIR}
+xgettext -C -ci18n -ki18n -kki18n -ktr2i18n -kI18N_NOOP -kI18N_NOOP2 --files-from=infiles.list -D ${BASEDIR} -D ${WDIR} -o ${PROJECT}.pot
+echo "Done extracting messages"
+
 
 echo "Merging translations"
-
 catalogs=`find . -name '*.po'`
 for cat in $catalogs; do
-  msgmerge -o $cat.new $cat rkward.pot
+  echo $cat
+  msgmerge -o $cat.new $cat ${PROJECT}.pot
   mv $cat.new $cat
 done
+echo "Done merging translations"
+
+
+echo "Cleaning up"
+cd ${WDIR}
+rm rcfiles.list
+rm infiles.list
+rm extractedrc.cpp
+echo "Done"
