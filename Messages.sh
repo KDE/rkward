6f3f433783b82b7d40793b90f8a08114586cdeaf
Thomas Friedrichsmeier <thomas.friedrichsmeier@ruhr-uni-bochum.de>
2007-11-18 21:56:42 +0000

    More on message extraction.
    svn path=/branches/KDE4_port/; revision=2241

--- Messages.sh
+++ Messages.sh
@@ -1,6 +1,19 @@
 #!/bin/sh
+BASEDIR="../rkward/"
 DIR=`pwd`
+
+echo "Extracting messages"
+
 cd ${BASEDIR}
-find . -name '*.cpp' -o -name '*.h' -o -name '*.c' > ${DIR}/infiles.list
-cd $DIR
+find . -name '*.cpp' -o -name '*.h' -o -name '*.c' | sort > ${DIR}/infiles.list
+cd ${DIR}
 xgettext -C -ci18n -ki18n -ktr2i18n -kI18N_NOOP -kI18N_NOOP2 --files-from=infiles.list -D ${BASEDIR} -o rkward.pot
+rm infiles.list
+
+echo "Merging translations"
+
+catalogs=`find . -name '*.po'`
+for cat in $catalogs; do
+  msgmerge -o $cat.new $cat rkward.pot
+  mv $cat.new $cat
+done
