1e7a4a322867645c7b0cfaa1d0fbcfa814215cac
Thomas Friedrichsmeier <thomas.friedrichsmeier@ruhr-uni-bochum.de>
2007-11-18 23:12:18 +0000

    Copy correct xgettext options from KDE extract-messages.sh
    svn path=/branches/KDE4_port/; revision=2245

--- po/Messages.sh
+++ po/Messages.sh
@@ -19,7 +19,10 @@ cd ${BASEDIR}
 find . -name '*.cpp' -o -name '*.h' -o -name '*.c' | sort > ${WDIR}/infiles.list
 echo "extractedrc.cpp" >> ${WDIR}/infiles.list
 cd ${WDIR}
-xgettext -C -ci18n -ki18n -kki18n -ktr2i18n -kI18N_NOOP -kI18N_NOOP2 --files-from=infiles.list -D ${BASEDIR} -D ${WDIR} -o ${PROJECT}.pot
+xgettext --from-code=UTF-8 -C -ci18n -ki18n:1 -ki18nc:1c,2 -ki18np:1,2 -ki18ncp:1c,2,3 -ktr2i18n:1 \
+	-kI18N_NOOP:1 -kI18N_NOOP2:1c,2 -kaliasLocale -kki18n:1 -kki18nc:1c,2 -kki18np:1,2 -kki18ncp:1c,2,3 \
+	--msgid-bugs-address="http://sourceforge.net/tracker/?group_id=50231&atid=459007" \
+	--files-from=infiles.list -D ${BASEDIR} -D ${WDIR} -o ${PROJECT}.pot
 echo "Done extracting messages"
 
 
