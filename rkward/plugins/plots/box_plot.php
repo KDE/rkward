f3f0355758ebc42007e92cbc433c015af3d65331
Stefan Rödiger <stefan_roediger@gmx.de>
2008-11-09 18:32:54 +0000

    This is an extended version of the Box plot. * From now on it's possible to show the mean (arithmetic mean only) and the standard deviation (both sites equal only) in a box plot.
    Known issues:
    * The code works with data from RKWard internal data sheets or data sets of equal length. For user data with unequal length this plot option will fail.
    * Moreover some aspects of the "plot options" need to be enabled or disabled.
    
    svn path=/branches/release_branch_0.4.9/; revision=2376

--- rkward/plugins/plots/box_plot.php
+++ rkward/plugins/plots/box_plot.php
@@ -17,13 +17,51 @@ function printout () {
 
 function doPrintout ($final) {
 	$xvarsstring = join (", ", split ("\n", getRK_val ("x")));
+	$names_mode = getRK_val ("names_mode");
+	$mean = getRK_val ("mean");
+	$sd = getRK_val ("sd");
+	$horizontal = getRK_val ("orientation");
+	$plot_adds = getRK_val ("plotoptions.code.calculate"); //add grid and alike
+
+?>
+	data_list <- list (<?echo ($xvarsstring); ?>)		#convert single sample variables to list
+<?
+	if ($names_mode == "rexp") {
+		echo ("names(data_list) <- " . getRK_val ("names_exp") . "\n");
+	} else if ($names_mode == "custom") {
+		echo ("names(data_list) <- c (\"" . str_replace (";", "\", \"", trim (getRK_val ("names_custom"))) . "\")\n");
+	}
 
 	if ($final) {
 ?>
 rk.header ("Boxplot", list ("Variable(s)", rk.get.description (<? echo ($xvarsstring); ?>, paste.sep=", ")))
 rk.graph.on()
 <?	} ?>
-try (boxplot (list (<? echo ($xvarsstring); ?>), notch = <? getRK ("notch") ?>, outline = <? getRK("outline")?>, horizontal = <? getRK("orientation") ?><? getRK ("plotoptions.code.printout"); ?>))
+try (boxplot (data_list, notch = <? getRK ("notch") ?>, outline = <? getRK("outline")?>, horizontal = <? getRK("orientation"); ?><? getRK ("plotoptions.code.printout"); ?>)) #actuall boxplot function
+<?	if (($mean == "TRUE") && ($horizontal == "TRUE")) {?>
+	try (points(1:length(data_list) ~ apply(data.frame(<? echo ($xvarsstring); ?>),2,mean,na.rm = TRUE),pch=19, cex = <? getRK ("cex_sd_mean"); ?><? getRK ("sd_mean_color.code.printout"); ?>)) #calculates the mean for all data and adds a point at the corresponding position
+<?	} if (($mean == "TRUE") && ($horizontal == "FALSE")) {?>
+	try (points(apply(data.frame(<? echo ($xvarsstring); ?>),2,mean,na.rm = TRUE),pch=19, cex = <? getRK ("cex_sd_mean"); ?><? getRK ("sd_mean_color.code.printout"); ?>)) #calculates the mean for all data and adds a point at the corresponding position
+<?	}
+?>
+<?	if (($sd == "TRUE") && ($horizontal == "FALSE")) {?>
+	sd_low <- (apply(data.frame(<? echo ($xvarsstring); ?>),2,mean,na.rm = TRUE)) - (apply(data.frame(<? echo ($xvarsstring); ?>),2,sd,na.rm = TRUE))
+	sd_high <- (apply(data.frame(<? echo ($xvarsstring); ?>),2,mean,na.rm = TRUE)) + (apply(data.frame(<? echo ($xvarsstring); ?>),2,sd,na.rm = TRUE))
+	points(sd_low,pch=3, cex = <? getRK ("cex_sd_mean"); ?><? getRK ("sd_mean_color.code.printout"); ?>)
+	points(sd_high,pch=3, cex = <? getRK ("cex_sd_mean"); ?><? getRK ("sd_mean_color.code.printout"); ?>)
+<?	} if (($sd == "TRUE") && ($horizontal == "TRUE")) {?>
+	sd_low <- (apply(data.frame(<? echo ($xvarsstring); ?>),2,mean,na.rm = TRUE)) - (apply(data.frame(<? echo ($xvarsstring); ?>),2,sd,na.rm = TRUE))
+	sd_high <- (apply(data.frame(<? echo ($xvarsstring); ?>),2,mean,na.rm = TRUE)) + (apply(data.frame(<? echo ($xvarsstring); ?>),2,sd,na.rm = TRUE))
+	points(1:length(data_list) ~ sd_low,pch=3, cex = <? getRK ("cex_sd_mean"); ?><? getRK ("sd_mean_color.code.printout"); ?>)
+	points(1:length(data_list) ~ sd_high,pch=3, cex = <? getRK ("cex_sd_mean"); ?><? getRK ("sd_mean_color.code.printout"); ?>)
+<?	}
+?>
+<?	if (!empty ($plot_adds)) { ?>
+
+<?		// print the grid() related code
+		printIndented ("\t", $plot_adds);
+	}
+?>
 <?	if ($final) { ?>
 rk.graph.off ()
 <?	}
