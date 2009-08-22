b9ec8c6503f97de5ec89ceac411a5f489462c5bb
Thomas Friedrichsmeier <thomas.friedrichsmeier@ruhr-uni-bochum.de>
2009-08-22 20:27:25 +0000

    Beef up wilcoxon_exact_test the same way as wilcoxon_test, and add a very basic help file.
    svn path=/trunk/rkward/; revision=2620

--- rkward/plugins/analysis/wilcoxon/wilcoxon_exact_test.php
+++ rkward/plugins/analysis/wilcoxon/wilcoxon_exact_test.php
@@ -1,21 +1,30 @@
 <?
-function preprocess () { ?>
-require(coin)
+function preprocess () {
+	global $y;
+	$y = getRK_val ("y");
+	if ($y != "") $y = ", " . $y;
+?>
+require(exactRankTests)
 
-names <- rk.get.description (<? getRK ("x"); ?>, <? getRK ("y"); ?>)
+names = rk.get.description (<? getRK ("x"); echo ($y); ?>)
 <?
 }
 
 function calculate () {
+	global $y;
+
 	$exact_setting = getRK_val ("exact");
 	if ($exact_setting == "yes") {
 		$exact_opt = ", exact=TRUE";
 	} else if ($exact_setting == "no") {
 		$exact_opt = ", exact=FALSE";
 	}
+	if ($y != "") $paired_opt = ", paired = " . getRK_val ("paired");
+	if (getRK_val ("confint") == "TRUE") {
+		if (($conflevel = getRK_val("conflevel")) != "0.95") $conflevel_opt = ", conf.level=" . $conflevel;
+	}
 ?>
-result <- wilcox.exact (<? getRK ("x"); ?>, <? getRK ("y"); ?>, alternative = "<? getRK ("alternative"); ?>", mu = <? getRK ("mu"); ?>, paired = <? getRK ("paired"); ?><? echo ($exact_opt); ?>, correct = <? getRK ("correct"); ?>, conf.int = <? getRK ("confint"); ?> <?
-if (($conflevel = getRK_val ("conflevel")) != "0.95") echo (", conf.level=" . $conflevel); ?>)
+result <- wilcox.exact (<? getRK ("x"); echo ($y); ?>, alternative = "<? getRK ("alternative"); ?>", mu = <? getRK ("mu"); echo ($paired_opt); echo ($exact_opt); ?>, correct = <? getRK ("correct"); ?>, conf.int = <? getRK ("confint"); echo ($conflevel_opt); ?>)
 
 <?
 }
@@ -23,7 +32,7 @@ if (($conflevel = getRK_val ("conflevel")) != "0.95") echo (", conf.level=" . $c
 function printout () {
 ?>
 rk.header (result$method,
-	parameters=list ("Comparing", paste (names[1], "against", names[2]),
+	parameters=list ("Comparing", paste (names, collapse=" against "),
 	"H1", rk.describe.alternative (result),
 	"Continuity correction in normal approximation for p-value", "<? getRK ("correct"); ?>",
 	"Compute exact p-value", "<? getRK ("exact"); ?>", "Paired test", "<? getRK ("paired"); ?>",
@@ -35,7 +44,7 @@ rk.results (list (
 	'Location Shift'=result$null.value,
 	'Hypothesis'=result$alternative,
 	p=result$p.value<?
-	if (getRK_val ("confint")== "TRUE") { ?>,
+	if (getRK_val ("confint") == "TRUE") { ?>,
 	'confidence interval percent'=(100 * attr(result$conf.int, "conf.level")),
 	'confidence interval of difference'=result$conf.int,
 	'Difference in Location' = result$estimate<? } ?>))
