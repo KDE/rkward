05b5d12968c531e539c6bc85016f57a1873d375a
Thomas Friedrichsmeier <thomas.friedrichsmeier@ruhr-uni-bochum.de>
2009-09-01 11:02:17 +0000

    Add option not to exclude missing values.
    svn path=/trunk/rkward/; revision=2632

--- rkward/plugins/analysis/crosstab_multi.php
+++ rkward/plugins/analysis/crosstab_multi.php
@@ -2,20 +2,20 @@
 function preprocess () {
 $vars = str_replace ("\n", ",", trim (getRK_val ("x"))) ;
 ?>
-data <- data.frame (<? echo ($vars); ?>)
+data <- data.frame (<? echo ($vars); ?>, check.names=FALSE)
 datadescription <- paste (rk.get.description (<? echo ($vars); ?>), collapse=", ");
 <?
 }
 
 function calculate () {
 ?>
-result <- ftable (data);
+result <- ftable (data<? if (!getRK_val ("exclude_nas.state")) echo (", exclude=NULL"); ?>);
 <?
 }
 
 function printout () {
 ?>
-rk.header ("Crosstabs (n to n)", parameters=list ("Variables", datadescription))
+rk.header ("Crosstabs (n to n)", parameters=list ("Variables"=datadescription))
 
 rk.print (result)
 <?
