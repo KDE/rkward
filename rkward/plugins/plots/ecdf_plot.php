d50b9d631583c86e33dc043e06df7c3f581e64da
Thomas Friedrichsmeier <thomas.friedrichsmeier@ruhr-uni-bochum.de>
2009-09-24 16:14:34 +0000

    Add option to adjust the plotted normal curve to the mean and sd
    svn path=/trunk/rkward/; revision=2662

--- rkward/plugins/plots/ecdf_plot.php
+++ rkward/plugins/plots/ecdf_plot.php
@@ -1,8 +1,14 @@
 <?
 function preprocess () {
+	$x = getRK_val ("x");
 ?>
-yrange <- range (<? getRK("x"); ?>, na.rm=TRUE)
+yrange <- range (<? echo ($x); ?>, na.rm=TRUE)
 <?
+	if (getRK_val ("th_pnorm") && getRK_val ("adjust_th_pnorm")) { ?>
+data.mean <- mean (<? echo ($x); ?>, na.rm=TRUE)
+data.sd <- sd (<? echo ($x); ?>, na.rm=TRUE)
+<?
+	}
 }
 
 function calculate () {
@@ -29,7 +35,7 @@ rk.graph.on ()
 try ({
 	plot.ecdf (<? echo ($x); ?>, <? getRK ("stepfun_options.code.printout"); ?><? getRK ("plotoptions.code.printout"); ?>)
 <?	if (getRK_val ("th_pnorm")) { ?>
-	curve (pnorm, from=yrange[1], to=yrange[2], add=TRUE, <? getRK ("col_thnorm.code.printout"); ?>)
+	curve (pnorm<? if (getRK_val ("adjust_th_pnorm")) echo " (x, mean=data.mean, sd=data.sd)"; ?>, from=yrange[1], to=yrange[2], add=TRUE, <? getRK ("col_thnorm.code.printout"); ?>)
 <?	}
 	if (getRK_val ("rug")) { ?>
 	rug (<? echo ($x); ?>, <? getRK ("ticksize"); ?>, <? getRK ("lwd"); ?>, <? getRK ("side"); ?><? getRK ("col_rug.code.printout"); ?>)
