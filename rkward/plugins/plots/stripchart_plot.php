3b735b44bba5cfe98ce0265db5a4c41ac7587ade
Thomas Friedrichsmeier <thomas.friedrichsmeier@ruhr-uni-bochum.de>
2009-09-25 11:59:10 +0000

    Nicer handling of the jitter and offset options
    svn path=/trunk/rkward/; revision=2667

--- rkward/plugins/plots/stripchart_plot.php
+++ rkward/plugins/plots/stripchart_plot.php
@@ -19,17 +19,23 @@ function doPrintout ($final) {
 	$x = getRK_val ("x");
 	$g = getRK_val ("g");
 	$method = '"' . getRK_val ("method") . '"';
-	$jitter = getRK_val ("jitter");
-	$offset = getRK_val ("offset");
-	$vertical = getRK_val ("vertical");
+	if ($method == "\"jitter\"") {
+		$opts .= ", jitter = " . getRK_val ("jitter");
+		$params .= ", \"Jitter\" = " . getRK_val ("jitter");
+	} else if ($method == "\"stack\"") {
+		$opts .= ", offset = " . getRK_val ("offset");
+		$params .= ", \"Offset\" = " . getRK_val ("offset");
+	}
+	$orientation = getRK_val ("orientation");
+	if ($orientation == "Vertical") $opts .= ", vertical = TRUE";
 	$plot_adds = getRK_val ("plotoptions.code.calculate"); //add grid and alike
 
 	if ($final) { ?>
-rk.header ("Stripchart", list ("Variable", rk.get.description (<? echo ($x); ?>), "Group", rk.get.description (<? echo ($g); ?>), "Method", <? echo ($method); ?>, "Jitter", <? echo ($jitter); ?>, "Plot drawn vertically", <? echo ($vertical); ?>, "Offset", <? echo ($offset); ?>))
+rk.header ("Stripchart", list ("Variable"=rk.get.description (<? echo ($x); ?>), "Group"=rk.get.description (<? echo ($g); ?>), "Method"=<? echo ($method); echo ($params); ?>, "Orientation"="<? echo ($orientation); ?>"))
 
 rk.graph.on ()
 <?	} ?>
-try (stripchart (<? echo ($x); ?> ~ (<? echo ($g); ?>), vertical=<? echo ($vertical); ?>, method = <? echo ($method); ?>, jitter = <? echo ($jitter); ?>, offset = <? echo ($offset); ?><? getRK ("plotoptions.code.printout"); ?>))
+try (stripchart (<? echo ($x); ?> ~ (<? echo ($g); ?>), method = <? echo ($method); echo ($opts); getRK ("plotoptions.code.printout"); ?>))
 <?	if (!empty ($plot_adds)) { ?>
 
 <?		// print the grid() related code
