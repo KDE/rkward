137c0ee0199a77202ebc1f1ab87e649285352c7e
Thomas Friedrichsmeier <thomas.friedrichsmeier@ruhr-uni-bochum.de>
2009-09-28 10:41:16 +0000

    updates for Import STATA plugin (Michael ash)
    svn path=/trunk/rkward/; revision=2669

--- rkward/plugins/00saveload/import/import_stata.php
+++ rkward/plugins/00saveload/import/import_stata.php
@@ -1,22 +1,7 @@
 <?php
 function preprocess () { ?>
 require (foreign)
-<?	if (getRK_val ("do_locale_conversion")) { ?>
-
-# helper function to convert all strings to the current encoding
-iconv.recursive <- function (x, from) {
-	attribs <- attributes (x);
-	if (is.character (x)) {
-		x <- iconv (x, from=from, to="", sub="")
-	} else if (is.list (x)) {
-		x <- lapply (x, function (sub) iconv.recursive (sub, from))
-	}
-	# convert factor levels and all other attributes
-	attributes (x) <- lapply (attribs, function (sub) iconv.recursive (sub, from))
-	x
-}
 <?	}
-}
 
 function calculate () {
 
@@ -54,28 +39,20 @@ function calculate () {
 	$object = getRK_val ("saveto");
 ?>
 data <- read.dta ("<? getRK ("file"); ?>"<? echo ($convert_dates_opt); echo ($convert_factors_opt); echo ($missing_type_opt); echo ($convert_underscore_opt); echo ($warn_missing_labels);  ?>)
-<?	if (getRK_val ("do_locale_conversion")) {
-		$from_locale = getRK_val ("encoding");
-		if ($from_locale == "other") {
-			$from_locale = getRK_val ("user_encoding");
-		} ?>
 
-# convert all strings to the current encoding
-data <- iconv.recursive (data, from="<? echo ($from_locale); ?>")
-<?	}
-	if (getRK_val ("convert_var_labels")) { ?>
 
 # set variable labels for use in RKWard
-labels <- attr (data, "variable.labels");
+labels <- attr (data, "var.labels")
 if (!is.null (labels)) {
-	for (i in 1:length (labels)) {
-		col <- make.names (names (labels[i]))
-		if (!is.null (col)) {
-			rk.set.label (data[[col]], labels[i])
-		}
-	}
+        for (i in 1:length (labels)) {
+                col <- make.names (attr (data, "names")[i] )
+                if (!is.null (col)) {
+                        rk.set.label (data[[col]], labels[i])
+                }
+        }
 }
-<?	} ?>
+
+
 
 <? echo ($object); ?> <<- data		# assign to globalenv()
 <?
