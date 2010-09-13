a2489894e5fe9ee417b53a6c6f96c6541555da10
Thomas Friedrichsmeier <thomas.friedrichsmeier@ruhr-uni-bochum.de>
2010-09-13 08:01:51 +0000

    Add default parameter values to rk.get.tempfile.name-replacement
    svn path=/trunk/rkward/; revision=3025

--- tests/test_framework.R
+++ tests/test_framework.R
@@ -278,7 +278,7 @@ rktest.initializeEnvironment <- function () {
 	.rk.rerun.plugin.link <<- .rk.make.hr <<- function (...) { list (...) }
 
 	# This should make the output of rk.graph.on() fixed
-	rk.get.tempfile.name <<- function (prefix, extension) paste (prefix, extension, sep="")
+	rk.get.tempfile.name <<- function (prefix="image", extension=".jpg") paste (prefix, extension, sep="")
 	options (rk.graphics.type="PNG", rk.graphics.width=480, rk.graphics.height=480)
 
 	# HACK: Override date, so we don't get a difference for each call of rk.header ()
