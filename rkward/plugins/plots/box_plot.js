d4f87dc7b0a6295d60d4c1436e8184c5bf4cd053
Thomas Friedrichsmeier <thomas.friedrichsmeier@ruhr-uni-bochum.de>
2011-04-27 11:56:34 +0000

    Support grouped data in boxplot. Also simplify the js code a bit, and make use of rk.list(). Add help page.
    svn path=/trunk/rkward/; revision=3531

--- rkward/plugins/plots/box_plot.js
+++ rkward/plugins/plots/box_plot.js
@@ -7,27 +7,22 @@ function printout () {
 }
 
 function doPrintout (full) {
-	var xvarsstring = "";
-	var names_mode = "";
-	var mean = "";
-	var pch_mean = "";
-	var sd = "";
-	var pch_sd_high = "";
-	var pch_sd_low = "";
-	var horizontal = "";
-	var plot_adds = "";
-	xvarsstring = getValue ("x").split ("\n").join (", ");
-	names_mode = getValue ("names_mode");
-	mean = getValue ("mean");
-	pch_mean = getValue ("pch_mean");
-	sd = getValue ("sd");
-	pch_sd_high = getValue ("pch_sd_high");
-	pch_sd_low = getValue ("pch_sd_low");
-	horizontal = getValue ("orientation");
-	plot_adds = getValue ("plotoptions.code.calculate"); //add grid and alike
+	var grouped_mode = getValue ("data_mode_grouped.numeric");
+	var names_mode = getValue ("names_mode");
+	var mean = getValue ("mean");
+	var pch_mean = getValue ("pch_mean");
+	var sd = getValue ("sd");
+	var pch_sd_high = getValue ("pch_sd_high");
+	var pch_sd_low = getValue ("pch_sd_low");
+	var horizontal = getValue ("orientation") == "TRUE";
+	var plot_adds = getValue ("plotoptions.code.calculate"); //add grid and alike
 
-
-	echo ('data_list <- list (' + xvarsstring + ')		#convert single sample variables to list\n');
+	if (grouped_mode) {
+		echo ('groups <- rk.list (' + getValue ("groups").split ("\n").join (", ") + ')\n');
+		echo ('data_list <- split (' + getValue ("outcome") + ', groups)		#split sample by grouping variables\n');
+	} else {
+		echo ('data_list <- rk.list (' + getValue ("x").split ("\n").join (", ") + ')		#convert single sample variables to list\n');
+	}
 	if (names_mode == "rexp") {
 		echo ("names(data_list) <- " + getValue ("names_exp") + "\n");
 	} else if (names_mode == "custom") {
@@ -35,28 +30,32 @@ function doPrintout (full) {
 	}
 
 	if (full) {
-		echo ('rk.header ("Boxplot", list ("Variable(s)", rk.get.description (' + xvarsstring + ', paste.sep=", ")))\n');
+		if (grouped_mode) {
+			echo ('rk.header ("Boxplot", list ("Outcome variable", rk.get.description (' + getValue ("outcome") + '), "Grouping variable(s)", paste (names (groups), collapse=", ")))\n');
+		} else {
+			echo ('rk.header ("Boxplot", list ("Variable(s)", paste (names (data_list), collapse=", ")))\n');
+		}
 		echo ('rk.graph.on()\n');
 	}
 	echo ('try (boxplot (data_list, notch = ' + getValue ("notch") + ', outline = ' + getValue("outline") + ', horizontal = ' + getValue("orientation") + getValue ("plotoptions.code.printout") + ')) #actuall boxplot function\n');
-	if ((mean == "TRUE") && (horizontal == "TRUE")) {
-		echo ('	try (points(1:length(data_list) ~ sapply(data_list,mean,na.rm = TRUE),pch=' + pch_mean + ', cex = ' + getValue ("cex_sd_mean") + getValue ("sd_mean_color.code.printout") + ')) #calculates the mean for all data and adds a point at the corresponding position\n');
-	}
-	if ((mean == "TRUE") && (horizontal == "FALSE")) {
-		echo ('	try (points(sapply(data_list,mean,na.rm = TRUE),pch=' + pch_mean + ', cex = ' + getValue ("cex_sd_mean") + getValue ("sd_mean_color.code.printout") + ')) #calculates the mean for all data and adds a point at the corresponding position\n');
+	if (mean == "TRUE") {
+		if (horizontal) {
+			echo ('	try (points(1:length(data_list) ~ sapply(data_list,mean,na.rm = TRUE),pch=' + pch_mean + ', cex = ' + getValue ("cex_sd_mean") + getValue ("sd_mean_color.code.printout") + ')) #calculates the mean for all data and adds a point at the corresponding position\n');
+		} else {
+			echo ('	try (points(sapply(data_list,mean,na.rm = TRUE),pch=' + pch_mean + ', cex = ' + getValue ("cex_sd_mean") + getValue ("sd_mean_color.code.printout") + ')) #calculates the mean for all data and adds a point at the corresponding position\n');
+		}
 	}
 
-	if ((sd == "TRUE") && (horizontal == "FALSE")) {
+	if (sd == "TRUE") {
 		echo ('	sd_low <- (sapply(data_list,mean,na.rm = TRUE)) - (sapply(data_list,sd,na.rm = TRUE))\n');
 		echo ('	sd_high <- (sapply(data_list,mean,na.rm = TRUE)) + (sapply(data_list,sd,na.rm = TRUE))\n');
-		echo ('	points(sd_low,pch=' + pch_sd_low + ', cex = ' + getValue ("cex_sd_mean") + getValue ("sd_mean_color.code.printout") + ')\n');
-		echo ('	points(sd_high,pch=' + pch_sd_high + ', cex = ' + getValue ("cex_sd_mean") + getValue ("sd_mean_color.code.printout") + ')\n');
-	}
-	if ((sd == "TRUE") && (horizontal == "TRUE")) {
-		echo ('	sd_low <- (sapply(data_list,mean,na.rm = TRUE)) - (sapply(data_list,sd,na.rm = TRUE))\n');
-		echo ('	sd_high <- (sapply(data_list,mean,na.rm = TRUE)) + (sapply(data_list,sd,na.rm = TRUE))\n');
-		echo ('	points(1:length(data_list) ~ sd_low,pch=' + pch_sd_low + ', cex = ' + getValue ("cex_sd_mean") + getValue ("sd_mean_color.code.printout") + ')\n');
-		echo ('	points(1:length(data_list) ~ sd_high,pch=' + pch_sd_high + ', cex = ' + getValue ("cex_sd_mean") + getValue ("sd_mean_color.code.printout") + ')\n');
+		if (horizontal) {
+			echo ('	points(1:length(data_list) ~ sd_low,pch=' + pch_sd_low + ', cex = ' + getValue ("cex_sd_mean") + getValue ("sd_mean_color.code.printout") + ')\n');
+			echo ('	points(1:length(data_list) ~ sd_high,pch=' + pch_sd_high + ', cex = ' + getValue ("cex_sd_mean") + getValue ("sd_mean_color.code.printout") + ')\n');
+		} else {
+			echo ('	points(sd_low,pch=' + pch_sd_low + ', cex = ' + getValue ("cex_sd_mean") + getValue ("sd_mean_color.code.printout") + ')\n');
+			echo ('	points(sd_high,pch=' + pch_sd_high + ', cex = ' + getValue ("cex_sd_mean") + getValue ("sd_mean_color.code.printout") + ')\n');
+		}
 	}
 
 	if (plot_adds.length > 0) {
