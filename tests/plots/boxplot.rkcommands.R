0b95cdef6051e0047ab1bd2d55d0bf087955d17d
Thomas Friedrichsmeier <thomas.friedrichsmeier@ruhr-uni-bochum.de>
2011-09-12 08:40:40 +0000

    Adjustments to changed rk.header() formatting and fix of typo
    svn path=/trunk/rkward/; revision=3755

--- tests/plots/boxplot.rkcommands.R
+++ tests/plots/boxplot.rkcommands.R
@@ -5,7 +5,7 @@ local({
 data_list <- rk.list (women[["weight"]], women[["height"]])		#convert single sample variables to list
 rk.header ("Boxplot", list ("Variable(s)", paste (names (data_list), collapse=", ")))
 rk.graph.on()
-try (boxplot (data_list, notch = FALSE, outline = TRUE, horizontal = FALSE)) #actuall boxplot function
+try (boxplot (data_list, notch = FALSE, outline = TRUE, horizontal = FALSE)) #actual boxplot function
 	try (points(sapply(data_list,mean,na.rm = TRUE),pch=15, cex = 1.00, col="blue")) #calculates the mean for all data and adds a point at the corresponding position
 rk.graph.off ()
 })
--- tests/plots/boxplot_grouped.rkcommands.R
+++ tests/plots/boxplot_grouped.rkcommands.R
@@ -6,6 +6,6 @@ groups <- rk.list (warpbreaks[["tension"]], warpbreaks[["wool"]])
 data_list <- split (warpbreaks[["breaks"]], groups)		#split sample by grouping variables
 rk.header ("Boxplot", list ("Outcome variable", rk.get.description (warpbreaks[["breaks"]]), "Grouping variable(s)", paste (names (groups), collapse=", ")))
 rk.graph.on()
-try (boxplot (data_list, notch = FALSE, outline = FALSE, horizontal = TRUE)) #actuall boxplot function
+try (boxplot (data_list, notch = FALSE, outline = FALSE, horizontal = TRUE)) #actual boxplot function
 rk.graph.off ()
 })
