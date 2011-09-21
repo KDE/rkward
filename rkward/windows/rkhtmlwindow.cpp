KDE/rkward

commit 6663cbba6005e524f88a66766eda41084e2908c3

Make writing plugins documentation available inline. This needed some tweaking to handle help:-urls correctly.
svn path=/trunk/rkward/; revision=3807

rkward/windows/rkhtmlwindow.cpp
@@ -298,11 +298,15 @@ bool RKHTMLWindow::openURL (const KUrl &url) {
 		if ((host == "127.0.0.1") || (host == "localhost") || host == QHostInfo::localHostName ()) {
 			KIO::TransferJob *job = KIO::get (url, KIO::Reload);
 			connect (job, SIGNAL (mimetype(KIO::Job*, const QString&)), this, SLOT (mimeTypeDetermined(KIO::Job*, const QString&)));
-			return true;
 		}
+		return true;
+	} else if (url.protocol ().toLower ().startsWith ("help")) {
+		khtmlpart->openUrl (url);
+		changeURL (url);
+		return true;
 	}
 
-	RKWorkplace::mainWorkplace ()->openAnyUrl (url);
+	RKWorkplace::mainWorkplace ()->openAnyUrl (url, QString (), KMimeType::findByUrl (url)->is ("text/html"));	// NOTE: text/html type urls, which we have not handled, above, are forced to be opened externally, to avoid recursion. E.g. help:// protocol urls.
 	return true;
 }
 

rkward/pages/rkward_for_rkward_devs.rkh
@@ -22,7 +22,7 @@
 <ul>
 	<li>For more functions of interest see: <link href="rkward://page/rkward_public_functions"/></li>
 	<li>Do read the the online <link href="http://sourceforge.net/apps/mediawiki/rkward/index.php?title=Developer_Information">wiki</link> as well.</li>
-	<li>To write and share your own GUI dialogs, see the online <link href="http://rkward.sourceforge.net/documents/devel/plugins/index.html">Introduction to Writing Plugins for RKWard</link>.</li>
+	<li>To write and share your own GUI dialogs, see the online <link href="help:rkwardplugins/">Introduction to Writing Plugins for RKWard</link>.</li>
 </ul>
 	</related>
 

rkward/pages/rkward_welcome.rkh
@@ -31,6 +31,6 @@
 	</section>
 
 	<section title="Missing something?" id="missing_something">
-	Are you missing a particular GUI dialog in the current menu? Check out our online <link href="http://rkward.sourceforge.net/documents/devel/plugins/index.html">Introduction to Writing Plugins for RKWard</link> to learn how easy it is to enhance RKWard with your custom dialogs. And if you like, how to share your work with the community of RKWard users.
+	Are you missing a particular GUI dialog in the current menu? Check out our online <link href="help:rkwardplugins">Introduction to Writing Plugins for RKWard</link> to learn how easy it is to enhance RKWard with your custom dialogs. And if you like, how to share your work with the community of RKWard users.
 	</section>
 </document>

rkward/rkwardapplication.cpp
@@ -68,6 +68,8 @@ RKWardApplication::RKWardApplication () : KApplication () {
 
 	// Don't complain when linking rkward://-pages from Rd pages
 	KAuthorized::allowUrlAction ("redirect", KUrl ("http://"), KUrl ("rkward://"));
+	// Don't complain when trying to open help pages
+	KAuthorized::allowUrlAction ("redirect", KUrl ("rkward://"), KUrl ("help:"));
 
 	rkapp = this;
 	detect_x11_creations = false;

rkward/windows/rkworkplace.cpp
@@ -207,7 +207,7 @@ void RKWorkplace::placeInToolWindowBar (RKMDIWindow *window, int position) {
 	}
 }
 
-bool RKWorkplace::openAnyUrl (const KUrl &url, const QString &known_mimetype) {
+bool RKWorkplace::openAnyUrl (const KUrl &url, const QString &known_mimetype, bool force_external) {
 	RK_TRACE (APP);
 
 #warning TODO support rkward:\/\/-protocol, here, too
@@ -215,18 +215,20 @@ bool RKWorkplace::openAnyUrl (const KUrl &url, const QString &known_mimetype) {
 	if (!known_mimetype.isEmpty ()) mimetype = KMimeType::mimeType (known_mimetype);
 	else mimetype = KMimeType::findByUrl (url);
 
-// NOTE: Currently a known mimetype implies that the URL is local or served from the local machine.
-// Thus, external web pages are *not* opened, here. Which is the behavior we want, although the implementation is ugly
-	if (mimetype->is ("text/html")) {
-		openHelpWindow (url, true);
-		return true;	// TODO
-	}
-	if (url.fileName ().toLower ().endsWith (".rdata")) {
-		RKWardMainWindow::getMain ()->fileOpenAskSave (url);
-		return true;	// TODO
-	}
-	if (mimetype->name ().startsWith ("text")) {
-		return (openScriptEditor (url, QString (), RKSettingsModuleCommandEditor::matchesScriptFileFilter (url.fileName())));
+	if (!force_external) {
+	// NOTE: Currently a known mimetype implies that the URL is local or served from the local machine.
+	// Thus, external web pages are *not* opened, here. Which is the behavior we want, although the implementation is ugly
+		if (mimetype->is ("text/html")) {
+			openHelpWindow (url, true);
+			return true;	// TODO
+		}
+		if (url.fileName ().toLower ().endsWith (".rdata")) {
+			RKWardMainWindow::getMain ()->fileOpenAskSave (url);
+			return true;	// TODO
+		}
+		if (mimetype->name ().startsWith ("text")) {
+			return (openScriptEditor (url, QString (), RKSettingsModuleCommandEditor::matchesScriptFileFilter (url.fileName())));
+		}
 	}
 
 	if (KMessageBox::questionYesNo (this, i18n ("The url you are trying to open ('%1') is not a local file or the filetype is not supported by RKWard. Do you want to open the url in the default application?", url.prettyUrl ()), i18n ("Open in default application?")) != KMessageBox::Yes) {

rkward/windows/rkworkplace.h
@@ -92,7 +92,7 @@ public:
 	RKMDIWindow *activeWindow (RKMDIWindow::State state);
 
 /** Opens the given url in the appropriate way. */
-	bool openAnyUrl (const KUrl &url, const QString &known_mimetype = QString ());
+	bool openAnyUrl (const KUrl &url, const QString &known_mimetype = QString (), bool force_external=false);
 
 /** Opens a new script editor
 @param url URL to load. Default option is to open an empty document

commit 168e019a1a36df88e4e15812c147be39db91deca

Fix handling of external http: urls (broken by r3807)
svn path=/trunk/rkward/; revision=3810

rkward/windows/rkhtmlwindow.cpp
@@ -298,8 +298,8 @@ bool RKHTMLWindow::openURL (const KUrl &url) {
 		if ((host == "127.0.0.1") || (host == "localhost") || host == QHostInfo::localHostName ()) {
 			KIO::TransferJob *job = KIO::get (url, KIO::Reload);
 			connect (job, SIGNAL (mimetype(KIO::Job*, const QString&)), this, SLOT (mimeTypeDetermined(KIO::Job*, const QString&)));
+			return true;
 		}
-		return true;
 	} else if (url.protocol ().toLower ().startsWith ("help")) {
 		khtmlpart->openUrl (url);
 		changeURL (url);