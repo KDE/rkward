965d77252a142e87c428ef259ce8e7129ac13032
Stefan Rödiger <stefan_roediger@gmx.de>
2011-12-09 20:40:15 +0000

    * use sheet name or sheet number instead of sheet number only
    svn path=/trunk/rkward/; revision=4090

--- rkward/plugins/00saveload/import/import_xls.js
+++ rkward/plugins/00saveload/import/import_xls.js
@@ -5,18 +5,19 @@ function preprocess () {
 function calculate () {
 	var options = "";
 	
-	var sheet = getValue ("sheet");
 	var header = getValue ("header");
 	var verbose = getValue ("verbose");
+	
+	var sheet = getValue ("sheetname");
 	
 	var quote_char = getValue ("quote");
 	if (quote_char == "other") quote_char = quote (getValue ("custom_quote"));
 		
-	options = ", sheet=" + sheet + ", header=" + header + ", verbose=" + verbose;
+	options = ", header=" + header + ", verbose=" + verbose;
 
 	var object = getValue ("saveto");
 
-	echo ('data <- read.xls ("' + getValue ("file") + '"' + options + ', ');
+	echo ('data <- read.xls ("' + getValue ("file") + '", sheet="' + sheet + '"' + options + ', ');
 	echo (' nrows=' + getValue ("nrows") + ', skip=' + getValue ("skip") + ', na.string="'+ getValue ("na") +'"' + getValue("strings_as_factors") + 
 	      ', check.names = ' + getValue("checkname") + ', strip.white = ' + getValue("stripwhite") + ')\n');
 	echo ('.GlobalEnv$' + object + ' <- data		# assign to globalenv()\n');
@@ -27,7 +28,7 @@ function calculate () {
 
 function printout () {
 	makeHeaderCode ("Import Microsoft EXCEL sheet", new Array("File", getValue ("file"), "Imported to", getValue ("saveto"), 
-								  "Imported Sheet", getValue ("sheet"), "First row as header", getValue ("header"), 
+								  "Imported Sheet", getValue ("sheetname"), "First row as header", getValue ("header"), 
 								  "Max number of rows to skip (-1 for no limit)", getValue ("skip"),
 								  "Number of lines to read (-1 for no limit)", getValue ("nrows"),
 								  "Character for missing values", getValue ("na")));
