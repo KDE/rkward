42c37c87f8b9b205ce764b7a5eaf7543e738446c
Meik Michalke <meik.michalke@uni-duesseldorf.de>
2012-03-19 15:37:11 +0000

    updating DESCRIPTION and adding package.Rd/.R file
    svn path=/trunk/rkward/; revision=4192

---
+++ rkward/rbackend/rpackages/rkward/R/rkward-package.R
@@ -0,0 +1,24 @@
+#' Provides functions related to the RKWard GUI.
+#'
+#' \tabular{ll}{
+#' Package: \tab rkward\cr
+#' Type: \tab Package\cr
+#' Version: \tab 0.5.8\cr
+#' Date: \tab 2012-03-19\cr
+#' Depends: \tab R (>= 2.9.0),methods\cr
+#' Encoding: \tab UTF-8\cr
+#' License: \tab GPL (>= 2)\cr
+#' LazyLoad: \tab yes\cr
+#' URL: \tab http://rkward.sourceforge.net\cr
+#' }
+#'
+#' This package contains functions which are useful in combination with the RKWard GUI. Many of these
+#' functions only needed for the internal communication between RKWard and R, but some are also useful in user scripts.
+#'
+#' @aliases rkward-package rkward
+#' @name rkward-package
+#' @docType package
+#' @title Provides functions related to the RKWard GUI.
+#' @author Thomas Friedrichsmeier \email{thomas.friedrichsmeier@@ruhr-uni-bochum.de}, with contributions from the RKWard Team
+#' @keywords package
+NULL
