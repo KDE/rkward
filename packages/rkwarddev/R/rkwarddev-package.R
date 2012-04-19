7740e7323040ac0a485da702aaa49f75795dcad4
Meik Michalke <meik.michalke@uni-duesseldorf.de>
2012-04-19 20:25:34 +0000

    rkwarddev 0.05-5
    svn path=/trunk/rkward/; revision=4208

--- packages/rkwarddev/R/rkwarddev-package.R
+++ packages/rkwarddev/R/rkwarddev-package.R
@@ -4,7 +4,7 @@
 #' Package: \tab rkwarddev\cr
 #' Type: \tab Package\cr
 #' Version: \tab 0.05-5\cr
-#' Date: \tab 2012-04-18\cr
+#' Date: \tab 2012-04-19\cr
 #' Depends: \tab R (>= 2.9.0),methods,XiMpLe (>= 0.03-12),rkward (>= 0.5.6)\cr
 #' Enhances: \tab rkward\cr
 #' Encoding: \tab UTF-8\cr
