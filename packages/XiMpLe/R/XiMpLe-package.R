26bc198273fc62e8f5ed18fb68053fa476ed338b
Meik Michalke <meik.michalke@uni-duesseldorf.de>
2012-05-10 22:12:08 +0000

    XiMpLe: updated meta data
    svn path=/trunk/rkward/; revision=4252

--- packages/XiMpLe/R/XiMpLe-package.R
+++ packages/XiMpLe/R/XiMpLe-package.R
@@ -4,7 +4,7 @@
 #' Package: \tab XiMpLe\cr
 #' Type: \tab Package\cr
 #' Version: \tab 0.03-14\cr
-#' Date: \tab 2012-05-08\cr
+#' Date: \tab 2012-05-11\cr
 #' Depends: \tab R (>= 2.9.0),methods\cr
 #' Enhances: \tab rkward\cr
 #' Encoding: \tab UTF-8\cr
