35757664bd271e8a4f91ecf6312c07cf05c87a62
Thomas Friedrichsmeier <thomas.friedrichsmeier@ruhr-uni-bochum.de>
2012-09-25 10:28:13 +0000

    Update boxplot tests and standards
    svn path=/trunk/rkward/; revision=4306

--- tests/plots/boxplot.rkcommands.R
+++ tests/plots/boxplot.rkcommands.R
@@ -3,7 +3,25 @@ local({
 data_list <- rk.list (women[["weight"]], women[["height"]])		#convert single sample variables to list
 rk.header ("Boxplot", list ("Variable(s)", paste (names (data_list), collapse=", ")))
 rk.graph.on()
-try (boxplot (data_list, notch = FALSE, outline = TRUE, horizontal = FALSE)) #actual boxplot function
-	try (points(sapply(data_list,mean,na.rm = TRUE),pch=15, cex = 1.00, col="blue")) #calculates the mean for all data and adds a point at the corresponding position
+try ({
+	boxplot (data_list, notch = FALSE, outline = TRUE, horizontal = FALSE) #actual boxplot function
+	points(sapply(data_list, mean, na.rm=TRUE), pch=15, cex = 1.00, col="blue") #calculates the mean for all data and adds a point at the corresponding position
+})
+rk.graph.off ()
+})
+local({
+## Print result
+data_list <- rk.list (women[["weight"]], women[["height"]])		#convert single sample variables to list
+rk.header ("Boxplot", list ("Variable(s)", paste (names (data_list), collapse=", ")))
+rk.graph.on()
+try ({
+	boxplot (data_list, notch = FALSE, outline = TRUE, horizontal = FALSE) #actual boxplot function
+	geo_mean <- function (x) {prod(na.omit(x))^(1/length(na.omit(x)))}	#Calculate geometric mean
+	points(sapply(data_list, geo_mean), pch=15, cex = 1.00, col="blue") #calculates the mean for all data and adds a point at the corresponding position
+	sd_low <- (sapply(data_list, mean, na.rm = TRUE)) - (sapply(data_list,sd,na.rm = TRUE))
+	sd_high <- (sapply(data_list, mean, na.rm = TRUE)) + (sapply(data_list,sd,na.rm = TRUE))
+	points(sd_low, pch=3, cex = 1.00, col="blue")
+	points(sd_high, pch=3, cex = 1.00, col="blue")
+})
 rk.graph.off ()
 })
--- tests/plots/boxplot_grouped.rkcommands.R
+++ tests/plots/boxplot_grouped.rkcommands.R
@@ -4,19 +4,23 @@ groups <- rk.list (warpbreaks[["tension"]], warpbreaks[["wool"]])
 data_list <- split (warpbreaks[["breaks"]], groups)		#split sample by grouping variables
 rk.header ("Boxplot", list ("Outcome variable", rk.get.description (warpbreaks[["breaks"]]), "Grouping variable(s)", paste (names (groups), collapse=", ")))
 rk.graph.on()
-try (boxplot (data_list, notch = FALSE, outline = FALSE, horizontal = TRUE)) #actual boxplot function
+try ({
+	boxplot (data_list, notch = FALSE, outline = FALSE, horizontal = TRUE) #actual boxplot function
+})
 rk.graph.off ()
 })
 local({
 ## Print result
-groups <- rk.list (datasets::warpbreaks[["tension"]], datasets::warpbreaks[["wool"]])
-data_list <- split (datasets::warpbreaks[["breaks"]], groups)		#split sample by grouping variables
+groups <- rk.list (warpbreaks[["tension"]], warpbreaks[["wool"]])
+data_list <- split (warpbreaks[["breaks"]], groups)		#split sample by grouping variables
 # adjust width and position of boxes to achieve dodging
-dodge_size <- nlevels (interaction (datasets::warpbreaks[["tension"]]))
+dodge_size <- nlevels (interaction (warpbreaks[["tension"]]))
 box_width <- 0.80 / dodge_size
 box_positions <- (rep (1:(length (data_list) / dodge_size), each=dodge_size) + (1:dodge_size)*(box_width))
-rk.header ("Boxplot", list ("Outcome variable", rk.get.description (datasets::warpbreaks[["breaks"]]), "Grouping variable(s)", paste (names (groups), collapse=", ")))
+rk.header ("Boxplot", list ("Outcome variable", rk.get.description (warpbreaks[["breaks"]]), "Grouping variable(s)", paste (names (groups), collapse=", ")))
 rk.graph.on()
-try (boxplot (data_list, boxwex=box_width, at=box_positions, xlim=c(min(box_positions)-box_width, max(box_positions)+box_width), notch = FALSE, outline = FALSE, horizontal = TRUE)) #actual boxplot function
+try ({
+	boxplot (data_list, boxwex=box_width, at=box_positions, xlim=c(min(box_positions)-box_width, max(box_positions)+box_width), notch = FALSE, outline = FALSE, horizontal = TRUE) #actual boxplot function
+})
 rk.graph.off ()
 })
