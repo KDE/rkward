1f3ff29847499cbc8f378f6df42702582537b87c
Thomas Friedrichsmeier <thomas.friedrichsmeier@ruhr-uni-bochum.de>
2012-09-26 10:35:46 +0000

    Adjust stripchart test standard for new formatting
    svn path=/trunk/rkward/; revision=4311

--- tests/plots/stripchart.rkcommands.R
+++ tests/plots/stripchart.rkcommands.R
@@ -3,6 +3,8 @@ local({
 rk.header ("Stripchart", list ("Variable"=rk.get.description (warpbreaks[["breaks"]]), "Group"=rk.get.description (warpbreaks[["tension"]]), "Method"="stack", "Offset" = 0.50, "Orientation"="Horizontal"))
 
 rk.graph.on ()
-try (stripchart (warpbreaks[["breaks"]] ~ (warpbreaks[["tension"]]), method = "stack", offset = 0.50))
+try ({
+	stripchart (warpbreaks[["breaks"]] ~ (warpbreaks[["tension"]]), method = "stack", offset = 0.50)
+})
 rk.graph.off ()
 })
