806bc82bc9f4184611736ffb1464f6c6e8e842b3
Thomas Friedrichsmeier <thomas.friedrichsmeier@ruhr-uni-bochum.de>
2012-09-28 15:06:47 +0000

    Make sure to print warnings, and don't correct calculation of percentage when there are fuzzy messages
    svn path=/trunk/rkward/; revision=4322

--- po/compile_po.cmake
+++ po/compile_po.cmake
@@ -1,7 +1,8 @@
 # helper script, which will compile a po into a gmo, if, and only if it is at least 80% translated.
 # this is basically, because ADD_CUSTOM_COMMAND won't capture stdout, and EXECUTE_PROCESS
 # can't be made to re-run when the po-file changes.
-EXECUTE_PROCESS(COMMAND ${GETTEXT_MSGFMT_EXECUTABLE} --check --statistics -o ${_gmoFile} ${_poFile}
+EXECUTE_PROCESS(COMMAND ${GETTEXT_MSGFMT_EXECUTABLE} --check -o ${_gmoFile} ${_poFile})	# For printing any errors / warnings
+EXECUTE_PROCESS(COMMAND ${GETTEXT_MSGFMT_EXECUTABLE} --statistics -o ${_gmoFile} ${_poFile}	# Second run to grab stats
 	ERROR_VARIABLE MSGFMT_STATS ERROR_STRIP_TRAILING_WHITESPACE)
 
 # Try to extract statistics information on translated vs. untranslated strings
@@ -12,8 +13,11 @@ LIST(LENGTH MSGFMT_COUNTS MSGFMT_COUNTS_COUNT)
 IF(MSGFMT_COUNTS_COUNT LESS 2)
 	LIST(APPEND MSGFMT_COUNTS 0)		# This means X translated, and no untranslated strings
 ENDIF(MSGFMT_COUNTS_COUNT LESS 2)
+IF(MSGFMT_COUNTS_COUNT LESS 3)
+	LIST(INSERT MSGFMT_COUNTS 1 0)		# This means X translated, no fuzzy, Y untranslated strings
+ENDIF(MSGFMT_COUNTS_COUNT LESS 3)
 LIST(GET MSGFMT_COUNTS 0 TRANSLATED_COUNT)
-LIST(GET MSGFMT_COUNTS 1 UNTRANSLATED_COUNT)
+LIST(GET MSGFMT_COUNTS 2 UNTRANSLATED_COUNT)
 MATH(EXPR TRANSLATION_RATIO "${TRANSLATED_COUNT}*100/(${TRANSLATED_COUNT}+${UNTRANSLATED_COUNT})")
 
 # Purge hopelessly incomplete translations
