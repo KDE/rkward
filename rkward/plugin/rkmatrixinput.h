b8fc16546d21a5f33cc13d017d0d07e004ee1b24
Thomas Friedrichsmeier <thomas.friedrichsmeier@ruhr-uni-bochum.de>
2012-10-16 08:37:31 +0000

    Mostly complete API-sketch for matrix input element.
    svn path=/trunk/rkward/; revision=4376

--- rkward/plugin/rkmatrixinput.h
+++ rkward/plugin/rkmatrixinput.h
@@ -20,23 +20,34 @@
 
 #include <rkcomponent.h>
 
+#include <QAbstractTableModel>
 #include <QDomElement>
+
+class QTableView;
 
 /** Provides a table for editing one- or two-dimensional data
   *@author Thomas Friedrichsmeier
   */
-class RKMatrixInput : public RKComponent {
+class RKMatrixInput : public RKComponent, public QAbstractTableModel {
 	Q_OBJECT
 public:
 	RKMatrixInput (const QDomElement &element, RKComponent *parent_component, QWidget *parent_widget);
 	~RKMatrixInput ();
 	int type () { return ComponentMatrixInput; };
-	bool isValid ();
+	bool isValid () { return is_valid; };
+	QString value (const QString &modifier);
 private slots:
 	void dimensionPropertyChanged (RKComponentPropertyBase *property);
+	void tsvPropertyChanged (RKComponentPropertyBase *property);
 private:
 	RKComponentPropertyInt *row_count;
 	RKComponentPropertyInt *column_count;
+	RKComponentPropertyBase *tsv_data;
+
+	int rowCount (const QModelIndex &parent = QModelIndex()) const; // implemented for QAbstractTableModel
+	int columnCount (const QModelIndex &parent = QModelIndex()) const; // implemented for QAbstractTableModel
+	QVariant data (const QModelIndex &index, int role = Qt::DisplayRole) const; // re-implemented for QAbstractTableModel
+	bool setData (const QModelIndex &index, const QVariant &value, int role = Qt::EditRole); // re-implemented for QAbstractTableModel
 
 	enum Mode {
 		Integer,
@@ -50,6 +61,26 @@ private:
 	bool allow_missings;
 	bool allow_user_resize_rows;
 	bool allow_user_resize_columns;
+
+	bool isValueValid (const QString &value) const;
+	void updateValidityFlag ();
+
+	bool setCellValue (int row, int column, const QString& value);
+	bool setColumnValue (int column, const QString& value);
+	bool setTableValue (const QString& value);
+
+	bool is_valid;
+
+	// NOTE: The storage may contain more rows / columns than the current dimensions of the table. This is so that no data gets
+	// lost, if a user shrinks a table, accidentally, then re-grows it.
+	struct Column {
+		int valid_up_to_row;	// to save validizing the entire table on each change, we keep track of validity per column
+		QStringList storage;
+		QString cached_joined_string;
+	};
+	QList<Column> columns;
+
+	QTableView *display;
 };
 
 #endif
