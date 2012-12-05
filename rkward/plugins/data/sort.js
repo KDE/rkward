2de87e540cf6582314953a9a41c608fb39fa0554
Thomas Friedrichsmeier <thomas.friedrichsmeier@ruhr-uni-bochum.de>
2012-12-05 15:23:45 +0000

    Make first use of optionset, for fine-tuning of sorting by several columns of a data.frame
    svn path=/trunk/rkward/; revision=4479

--- rkward/plugins/data/sort.js
+++ rkward/plugins/data/sort.js
@@ -1,13 +1,39 @@
+function makeSortItem (objectname, reversed, mode, conversion_custom) {
+	var obj = objectname;
+	if (mode == 'character') obj = 'as.character (' + obj + ')';
+	else if (mode == 'number') obj = 'as.numeric (' + obj + ')';
+	else if (mode == 'custom') {
+		obj = conversion_custom.replace (/_X_/g, obj);
+	}
+	if (reversed) {
+		if (mode != 'number') obj = '-xtfrm (' + obj + ')';
+		else obj = '-' + obj;
+	}
+	return obj;
+}
+
 function calculate () {
-	var object = getValue ("object");
-	var is_data_frame = getValue ("sortby.enabled.numeric");
+	var object = getString ("object");
+	var is_data_frame = getBoolean ("sortby.enabled");
 
 	var saveto = object;
-	if (getValue ("saveto_select") == "other") saveto = getValue ("saveto");
-	var sortby = getValue ("sortby");
-	if (!is_data_frame) sortby = object;
+	if (getString ("saveto_select") == 'other') saveto = getString ("saveto");
+	var sortby;
+	if (is_data_frame) {
+		sortby = "";
+		var sortbylist = getList ("sortby");
+		var sortbyreverse = getList ("multi_sortby.reverse");
+		var sortbymode = getList ("multi_sortby.conversion");
+		var sortbycustom = getList ("multi_sortby.conversion_custom");
+		for (var i = 0; i < sortbylist.length; ++i) {
+			if (i > 0) sortby = sortby + ', ';
+			sortby = sortby + makeSortItem (sortbylist[i], sortbyreverse[i] != "", sortbymode[i], sortbycustom[i]);
+		}
+	} else {
+		sortby = makeSortItem (object, false, getString ("conversion"), getString ("conversion_custom"));
+	}
 
-	echo (".GlobalEnv$" + saveto + " <- " + object + "[order (" + sortby + getValue ("order") + ")");
-	if (is_data_frame) echo (",");
-	echo ("]\n");
+	echo ('.GlobalEnv$' + saveto + ' <- ' + object + '[order (' + sortby + getString ("order") + ')');
+	if (is_data_frame) echo (',');
+	echo (']\n');
 }
