f9bbd5ba8e07c6b62a1ffe9dad9f3e0ecfabef15
Thomas Friedrichsmeier <thomas.friedrichsmeier@ruhr-uni-bochum.de>
2013-04-04 16:17:17 +0000

    Also upload for Ubuntu raring
    svn path=/trunk/rkward/; revision=4665

--- scripts/makeppa_upload.sh
+++ scripts/makeppa_upload.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ## begin: These may need adjusting!
-TARGETS="quantal precise oneiric natty lucid"
+TARGETS="raring quantal precise oneiric natty lucid"
 AUTHOR="Thomas Friedrichsmeier <tfry@users.sourceforge.net>"
 ## end: These may need adjusting!
 
