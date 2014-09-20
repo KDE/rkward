721ae3bd4302b43efc3ee3d902435fdb7c2b8227
Thomas Friedrichsmeier <thomas.friedrichsmeier@ruhr-uni-bochum.de>
2014-09-20 15:42:39 +0000

    Ok, also offer to save full lm-object.
    svn path=/trunk/rkward/; revision=4840

--- rkward/plugins/analysis/regression/linear_regression.js
+++ rkward/plugins/analysis/regression/linear_regression.js
@@ -3,9 +3,10 @@ function calculate () {
 	var intercept = "";
 	if (!getBoolean ("intercept")) intercept = "0 + ";
 
+	var savemodel = getBoolean ("savemodel.active");
 	var savefitted = getBoolean ("savefitted.active");
 	var saveresiduals = getBoolean ("saveresiduals.active");
-	var simple_mode = !(savefitted || saveresiduals);
+	var simple_mode = !(savefitted || saveresiduals || savemodel);
 
 	model = 'lm (' + getValue ("y") + ' ~ ' + intercept + vars;
 	if (!simple_mode) model += ', na.action=na.exclude';	// default action of na.omit is a nuisance for fitted values
@@ -15,9 +16,10 @@ function calculate () {
 		echo ('results <- summary.lm (' + model + ')\n');
 	} else {
 		echo ('model <- ' + model + '\n');
+		if (savemodel) echo ('.GlobalEnv$' + getString ('savemodel') + ' <- model\n');
 		if (savefitted) echo ('.GlobalEnv$' + getString ('savefitted') + ' <- fitted (model)\n');
 		if (saveresiduals) echo ('.GlobalEnv$' + getString ('saveresiduals') + ' <- residuals (model)\n');
-		echo ('results <- summary.lm (model)\n');
+		echo ('results <- summary (model)\n');
 	}
 }
 
@@ -25,4 +27,3 @@ function printout () {
 	echo ('rk.header ("Linear Regression")\n');
 	echo ('rk.print(results)\n');
 }
-
