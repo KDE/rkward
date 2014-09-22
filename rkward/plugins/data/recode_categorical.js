81b5439a66d9ecc84800d08acdd11b524d4debac
Thomas Friedrichsmeier <thomas.friedrichsmeier@ruhr-uni-bochum.de>
2014-09-22 15:37:09 +0000

    Handle input NAs in recode_categorical plugin.
    svn path=/trunk/rkward/; revision=4843

--- rkward/plugins/data/recode_categorical.js
+++ rkward/plugins/data/recode_categorical.js
@@ -25,14 +25,17 @@ function calculate () {
 	}
 
 	// Make replacements
-	var old_values = getList ("set.values");
-	var new_values_types = getList ("set.new_value_types");
-	var new_values_strings = getList ("set.new_value_strings");
+	var old_value_types = getList ("set.old_value_types");
+	var old_value_strings = getList ("set.old_value_strings");
+	var new_value_types = getList ("set.new_value_types");
+	var new_value_strings = getList ("set.new_value_strings");
 
 	var dupes = Array ();
 	var all_old_values = Array ();
-	for (var i = 0; i < old_values.length; ++i) {
-		var old_values_it = old_values[i].split ('\n');
+	for (var i = 0; i < old_value_types.length; ++i) {
+		var old_values_it;
+		if (old_value_types[i] == "na") old_values_it = Array ("NA");
+		else old_values_it = old_value_strings[i].split ('\n');
 
 		// Check for duplicate old values, as the UI does not currently support this!
 		for (var j = 0; j < old_values_it.length; ++j) {
@@ -45,18 +48,19 @@ function calculate () {
 
 		var old_index;
 		if (old_values_it.length > 1) {
-			old_index = ' %in% c(' + old_values_it.join (',') + ')';
+			old_index = 'input %in% c(' + old_values_it.join (',') + ')';
 		} else {
-			old_index = ' == ' + old_values_it;
+			if (old_values_it == "NA") old_index = 'is.na (input)';
+			else old_index = 'input == ' + old_values_it;
 		}
 
 		var replacement = 'NA';
-		if (new_values_types[i] != "na") {
-			replacement = new_values_strings[i];
+		if (new_value_types[i] != "na") {
+			replacement = new_value_strings[i];
 			if (quote_new_values) replacement = quote (replacement);
 		}
 
-		echo ('recoded[input' + old_index + '] <- ' + replacement + '\n');
+		echo ('recoded[' + old_index + '] <- ' + replacement + '\n');
 	}
 
 	// Produce warnings (or should it be errors?)
