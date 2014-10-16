5805ca6c5f6efe1509c3e5960bd28f10079ee731
Thomas Friedrichsmeier <thomas.friedrichsmeier@ruhr-uni-bochum.de>
2014-10-16 17:46:42 +0000

    Update roxygen docs for XiMpLe and rkwarddev.
    svn path=/trunk/rkward/; revision=4915

--- scripts/roxygenize.sh
+++ scripts/roxygenize.sh
@@ -7,13 +7,15 @@ echo "
 	library (roxygen2)
 	packages <- c ( '${BASEDIR}/rkward/rbackend/rpackages/rkward/',
 					# '${BASEDIR}/rkward/rbackend/rpackages/rkwardtests/', # currently excluded due to missing support for slots in roxygen2
-					'${BASEDIR}/packages/rkwarddev/',
-					'${BASEDIR}/packages/XiMpLe/')
+					'${BASEDIR}/packages/XiMpLe/',
+					'${BASEDIR}/packages/rkwarddev/'
+					)
 	for (package in packages) {
+		dummy <- roxygen2:::source_package (package) # See https://github.com/klutometis/roxygen/issues/167
 		roxygenize (package)
 		possibly_empty_dirs <- paste (package, c ('inst/doc', 'inst'), sep='/')
 		for (dir in possibly_empty_dirs) {
-			if (file.exists (dir)) try (file.remove (dir))
+			if (file.exists (dir)) suppressWarnings (try (file.remove (dir)))
 		}
 	}
 	print (warnings())
