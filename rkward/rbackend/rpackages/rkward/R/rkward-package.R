f3c091c37118c18cb9ae1f974b4783566fbcbd28
Thomas Friedrichsmeier <thomas.friedrichsmeier@ruhr-uni-bochum.de>
2014-10-27 18:40:59 +0000

    Update some out-of-date meta information
    svn path=/trunk/rkward/; revision=4965

--- rkward/rbackend/rpackages/rkward/R/rkward-package.R
+++ rkward/rbackend/rpackages/rkward/R/rkward-package.R
@@ -1,17 +1,5 @@
 #' Provides functions related to the RKWard GUI.
 #'
-#' \tabular{ll}{
-#' Package: \tab rkward\cr
-#' Type: \tab Package\cr
-#' Version: \tab 0.6.0\cr
-#' Date: \tab 2012-09-29\cr
-#' Depends: \tab R (>= 2.9.0),methods\cr
-#' Encoding: \tab UTF-8\cr
-#' License: \tab GPL (>= 2)\cr
-#' LazyLoad: \tab yes\cr
-#' URL: \tab http://rkward.sourceforge.net\cr
-#' }
-#'
 #' This package contains functions which are useful in combination with the RKWard GUI. Many of these
 #' functions only needed for the internal communication between RKWard and R, but some are also useful in user scripts.
 #'
@@ -19,6 +7,6 @@
 #' @name rkward-package
 #' @docType package
 #' @title Provides functions related to the RKWard GUI.
-#' @author Thomas Friedrichsmeier \email{thomas.friedrichsmeier@@ruhr-uni-bochum.de}, with contributions from RKWard-devel mailing list \email{rkward-devel@@lists.sourceforge.net}
+#' @author Thomas Friedrichsmeier \email{thomas.friedrichsmeier@@ruhr-uni-bochum.de}, and the RKWard Team \email{rkward-devel@@lists.sourceforge.net}
 #' @keywords package
 NULL
