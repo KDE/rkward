0b796989f598d2e14e0815ce73c3bb2a4fbca811
Thomas Friedrichsmeier <thomas.friedrichsmeier@ruhr-uni-bochum.de>
2014-11-18 17:12:32 +0100

    ahem...

--- po/Messages.sh
+++ po/Messages.sh
@@ -1,10 +1,10 @@
-+#!bin/sh
-+ 
-+# invoke the extractrc script on all .ui, .rc, and .kcfg files in the sources
-+# the results are stored in a pseudo .cpp file to be picked up by xgettext.
-+$EXTRACTRC `find . -name \*.rc -o -name \*.ui -o -name \*.kcfg` >> rc.cpp
-+#
-+# call xgettext on all source files. If your sources have other filename
-+# extensions besides .cc, .cpp, and .h, just add them in the find call.
-+$XGETTEXT `find . -name \*.cpp -o -name \*.h -name \*.c` -o $podir/rkward.pot
+#!bin/sh
+ 
+# invoke the extractrc script on all .ui, .rc, and .kcfg files in the sources
+# the results are stored in a pseudo .cpp file to be picked up by xgettext.
+$EXTRACTRC `find . -name \*.rc -o -name \*.ui -o -name \*.kcfg` >> rc.cpp
+#
+# call xgettext on all source files. If your sources have other filename
+# extensions besides .cc, .cpp, and .h, just add them in the find call.
+$XGETTEXT `find . -name \*.cpp -o -name \*.h -name \*.c` -o $podir/rkward.pot
 
