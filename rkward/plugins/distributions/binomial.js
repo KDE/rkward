dd3d55da50cc25ad642da38bd6b5a3bdaeeb5ea4
Thomas Friedrichsmeier <thomas.friedrichsmeier@ruhr-uni-bochum.de>
2015-01-22 18:28:41 +0100

    Complete re-design for distributions plugins (for now). Beta and binomial are the first two converted.

--- rkward/plugins/distributions/binomial.js
+++ rkward/plugins/distributions/binomial.js
@@ -2,21 +2,10 @@ include ("dist_common.js");
 
 function getDistSpecifics () {
 	var dist = new Object;
-	dist["params"] = "bla";
-	dist["funstem"] = "binom";
-	dist["header"] = new Header (i18n ("Binomial tail probability"));
+	var size = getString ("size");
+	dist["params"] = ', size=' + size + ", prob=" + getString ("prob");
+	dist["funstem"] = 'binom';
+	dist["max_quantile"] = size;
+	dist["header"] = new Header (i18n ('Binomial distribution')).addFromUI ("size").addFromUI ("prob");
 	return dist;
 }
-
-/*function calculate () {
-	q = "c (" + getString ("q").replace (/[, ]+/g, ", ") + ")";
-
-	echo ('result <- (pbinom (q = ' + q + ', size = ' + getValue ("size") + ', prob = ' + getValue ("prob") + ', ' + getValue ("tail") + ', ' + getValue ("logp") + '))\n');
-}
-
-function printout () {
-	echo ('rk.header (, list ("Vector of quantiles", "' + q + '", "Binomial trials", "' + getValue ("size") + '", "Probability of success", "' + getValue ("prob") + '", "Tail", "' + getValue ("tail") + '", "Probabilities p are given as", "' + getValue ("logp") + '"));\n');
-	echo ('rk.results (result, titles="Binomial tail probabilities")\n');
-}
-
-*/
--- rkward/plugins/distributions/dist_common.js
+++ rkward/plugins/distributions/dist_common.js
@@ -1,38 +1,78 @@
 var mode;
-var values;
-var logp;
-var lowert;
+var logpd;
 var dist;
+var invar;
+var outvar;
 
 function calculate () {
 	mode = getString ("mode");
+	dist = getDistSpecifics ();
+	invar = 'q';
+	if (mode == 'q') invar = 'p'
+	outvar = mode;
+
+	var params;
+	if (mode == "d") {
+		logpd = getBoolean ("logd.state");
+		params = logpd ? ', log=TRUE' : '';
+		// NOTE: param lower.tail is not applicable for density function
+	} else {
+		logpd = getBoolean ("logp.state");
+		params = logpd ? ', log.p=TRUE' : '';
+		if (!getBoolean ("lower.state")) {
+			params += ', lower.tail=FALSE';
+		}
+	}
+
+	var values;
 	if (mode == "q") values = getList ("p.0");
 	else values = getList ("q.0");
-	if (values.length > 1) values = 'c (' + values.join (', ') + ')';
+	if (values.length < 1) {
+		var max_auto_sequence_length = 20;
 
-	logp = getBoolean ("logp.state");
-	var logparam = logp ? ', logp=TRUE' : '';
-	lowert = getBoolean ("lower.state");
-	var tailparam = lowert ? '' : ', lower.tail=FALSE';
+		if (invar == 'q') {
+			var maxquant = dist["max_quantile"];
+			if (typeof (maxquant) != 'undefined') {
+				if (maxquant <= max_auto_sequence_length) {
+					values = '0:' + String (maxquant);
+				} else {
+					values = 'seq.int (0, ' + String (maxquant) + ', by=' + String (Math.ceil (maxquant / max_auto_sequence_length)) + ')';
+				}
+			} else {
+				values = 'seq (0, 1, length.out=' + String (max_auto_sequence_length+1) + ')';
+			}
+		} else {    // invar == 'p'
+			if (logpd) {
+				values = '-' + String (max_auto_sequence_length) + ':0';
+			} else {
+				values = 'seq (0, 1, length.out=' + String (max_auto_sequence_length+1) + ')';
+			}
+		}
+	} else {
+		if (values.length > 1) values = 'c (' + values.join (', ') + ')';
+	}
 
-	dist = getDistSpecifics ();
+	echo (invar + ' <- ' + values + '\n');
+	echo (outvar + ' <- ' + mode + dist["funstem"] + ' (' + invar + dist["params"] + params + ')\n');
+}
 
-	var plabel = logp ? i18n ('Log Probability') : i18n ('Probability');
-	var qlabel = i18n ('Quantile');
-	var dlabel = i18n ('Density');
-
-	echo ('result <- data.frame (');
-	echo (mode == "q" ? plabel : qlabel); 
-	echo ('=' + values + ', ');
-	if (mode == "d") echo (dlabel + '=d' + dist["funstem"] + ' (' + values + dist["params"] + logparam + ')');                  // NOTE: param lower.tail is not applicable for density function
-	else if (mode == "p") echo (plabel + '=p' + dist["funstem"] + ' (' + values + dist["params"] + tailparam + logparam + ')');
-	else if (mode == "q") echo (qlabel + '=q' + dist["funstem"] + ' (' + values + dist["params"] + tailparam + logparam + ')');
-	echo (')\n');
+function getLabel (quantity) {
+	if (quantity == "q") return i18n ('Quantile');
+	if (quantity == "d") {
+		if (logpd) return i18nc ('logarithm of density', 'log (Density)');
+		return i18n ('Density');
+	}
+	// quantity == "p"
+	if (logpd) return i18nc ('logarithm of probability', 'log (Probability)');
+	return i18n ('Probability');
 }
 
 function printout () {
 	header = dist["header"];
-	header.add (i18nc ("Tail of distribution function: lower / upper", 'Tail'), lowert ? i18n ('Lower tail: P[X ≤ x]') : i18n ('Upper tail: P[X > x]'));
+	if (mode != "d") {
+		header.add (i18nc ("Tail of distribution function: lower / upper", 'Tail'), getBoolean ("lower.state") ? i18n ('Lower tail: P[X ≤ x]') : i18n ('Upper tail: P[X > x]'));
+	}
 	header.print ();
-	echo ('rk.print (result)\n');
+
+	echo ('rk.results (data.frame (' + getLabel (invar) + '=' + invar + ', ' + getLabel (outvar) + '=' + outvar + ', check.names=FALSE))\n');
 }
