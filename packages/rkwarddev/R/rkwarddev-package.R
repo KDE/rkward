2379da9589cc75537c0964e10f135c251f5f9f5c
m.eik michalke <meik.michalke@uni-duesseldorf.de>
2015-10-04 21:29:14 +0200

    maintainance updates
      - files automatically altered during the package update process

--- packages/rkwarddev/R/rkwarddev-package.R
+++ packages/rkwarddev/R/rkwarddev-package.R
@@ -1,10 +1,10 @@
-#' A collection of tools for RKWard plugin development.
+#' A Collection of Tools for RKWard Plugin Development.
 #'
 #' \tabular{ll}{
 #' Package: \tab rkwarddev\cr
 #' Type: \tab Package\cr
 #' Version: \tab 0.07-4\cr
-#' Date: \tab 2015-06-29\cr
+#' Date: \tab 2015-10-04\cr
 #' Depends: \tab R (>= 2.9.0),methods,XiMpLe (>= 0.03-21),rkward (>= 0.5.7)\cr
 #' Enhances: \tab rkward\cr
 #' Encoding: \tab UTF-8\cr
