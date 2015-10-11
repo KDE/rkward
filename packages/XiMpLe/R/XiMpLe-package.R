64dc858cba88a2d4dc24db07466b521b0b14c06d
m.eik michalke <meik.michalke@uni-duesseldorf.de>
2015-10-11 19:01:30 +0200

    fixed XMLScanDeep()
      - XMLScanDeep() was hardwired to examine attributes, it now also checks node names and values

--- packages/XiMpLe/R/XiMpLe-package.R
+++ packages/XiMpLe/R/XiMpLe-package.R
@@ -1,12 +1,11 @@
-#' A simple XML tree parser and generator.
+#' A Simple XML Tree Parser and Generator.
 #'
 #' \tabular{ll}{
 #' Package: \tab XiMpLe\cr
 #' Type: \tab Package\cr
 #' Version: \tab 0.03-23\cr
-#' Date: \tab 2014-12-14\cr
+#' Date: \tab 2015-10-11\cr
 #' Depends: \tab R (>= 2.9.0),methods\cr
-#' Enhances: \tab rkward\cr
 #' Encoding: \tab UTF-8\cr
 #' License: \tab GPL (>= 3)\cr
 #' LazyLoad: \tab yes\cr
@@ -15,9 +14,9 @@
 #'
 #' This package provides a simple XML tree parser/generator. It includes functions to read XML files into R objects,
 #' get information out of and into nodes, and write R objects back to XML code.
-#' It's not as powerful as the XML package and doesn't aim to be, but for simple XML handling
-#' it could be useful.  It was originally programmed for the R GUI and IDE RKWard, to make plugin
-#' development easier. You can install RKWard from http://rkward.kde.org.
+#' It's not as powerful as the 'XML' package and doesn't aim to be, but for simple XML handling
+#' it could be useful. It was originally developed for the R GUI and IDE RKWard (https://rkward.kde.org), to make plugin
+#' development easier.
 #'
 #' @aliases XiMpLe-package
 #' @name XiMpLe-package
