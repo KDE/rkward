48f671d9008a782e396d423e6e35a761edd8b67d
Thomas Friedrichsmeier <thomas.friedrichsmeier@ruhr-uni-bochum.de>
2015-11-30 15:55:46 +0100

    Fix some typos

--- tests/distributions/weibull_probabilities.rkcommands.R
+++ tests/distributions/weibull_probabilities.rkcommands.R
@@ -3,7 +3,7 @@ local({
 q <- seq (0, qweibull (.99, shape=1.0, scale=1.0), length.out=20)
 p <- pweibull (q, shape=1.0, scale=1.0)
 ## Print result
-rk.header ("Weilbull distribution", parameters=list("Shape"="1.0",
+rk.header ("Weibull distribution", parameters=list("Shape"="1.0",
 	"Scale"="1.0",
 	"Tail"="Lower tail: P[X ≤ x]"))
 rk.results (data.frame ("Quantile"=q, "Probability"=p, check.names=FALSE))
