d63feeaf84bfec28586eeb265deaf50a43dbc9e9
Thomas Friedrichsmeier <thomas.friedrichsmeier@ruhr-uni-bochum.de>
2016-01-14 22:16:17 +0100

    Fix some quirks in gdata-based XLS import plugin, and add automated test.
    Activate both XLS-import plugins (move to import_export.pluginmap)

--- rkward/plugins/00saveload/import/import_xls.js
+++ rkward/plugins/00saveload/import/import_xls.js
@@ -1,8 +1,18 @@
-function preprocess () {
-	echo ('require (gdata)\n');
+function preview () {
+	preprocess (true);
+	calculate (true);
+	// printout ();
 }
 
-function calculate () {
+function preprocess (is_preview) {
+	if (is_preview) {
+		echo ('if (!base::require (gdata)) stop (' + i18n ("Preview not available, because package gdata is not installed or cannot be loaded.") + ')\n');
+	} else {
+		echo ('require (gdata)\n');
+	}
+}
+
+function calculate (is_preview) {
 	var header = getValue ("header");
 	var verbose = getValue ("verbose");
 	var sheet = getValue ("sheetname");
@@ -10,16 +20,20 @@ function calculate () {
 	var quote_char = getValue ("quote");
 	if (quote_char == "other") quote_char = quote (getValue ("custom_quote"));
 
-	var options = ", header=" + header + ", quote=" + quote_char + ", verbose=" + verbose;
+	var options = ", header=" + header + makeOption ("quote", quote_char) + ", verbose=" + verbose;
 
 	echo ('data <- read.xls ("' + getValue ("file") + '", sheet="' + sheet + '"' + options + ', ');
 	echo (' nrows=' + getValue ("nrows") + ', skip=' + getValue ("skip") + ', na.string="'+ getValue ("na") +'"' + getValue("strings_as_factors") + 
-	      ', check.names = ' + getValue("checkname") + ', strip.white = ' + getValue("stripwhite") + ')\n');
+	      ', strip.white = ' + getValue("stripwhite") + ')\n');
 
-	var object = getValue ("saveto");
-	echo ('.GlobalEnv$' + object + ' <- data		'); comment ('assign to globalenv()');
-	if (getValue ("doedit") ) {
-		echo ('rk.edit (.GlobalEnv$' + object + ')\n');
+	if (is_preview) {
+		echo ('preview_data <- data[1:min(50,dim(data)[1]),1:min(50,dim(data)[2])]\n');
+	} else {
+		var object = getValue ("saveto");
+		echo ('.GlobalEnv$' + object + ' <- data		'); comment ('assign to globalenv()');
+		if (getValue ("doedit") ) {
+			echo ('rk.edit (.GlobalEnv$' + object + ')\n');
+		}
 	}
 }
 
