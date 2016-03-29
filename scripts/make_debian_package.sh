38bdc05a137fdc58b2f4506ce19a8352c5bed9f1
Thomas Friedrichsmeier <thomas.friedrichsmeier@ruhr-uni-bochum.de>
2016-03-29 13:20:18 +0200

    Re-introduce split into rkward and rkward-data packages.
    Adjust packaging script to support creating ~rc-release packages.

--- scripts/make_debian_package.sh
+++ scripts/make_debian_package.sh
@@ -2,12 +2,13 @@
 cd `dirname $0`/..
 BASEDIR=`pwd`
 VERSION=`${BASEDIR}/scripts/getversion.sh ${1}`
+DEBSUFFIX=${2}
 DEBDIR=${BASEDIR}/debian-official
 DEBTEMPDIR=${BASEDIR}/debiantemp
 rm -rf ${DEBTEMPDIR}
 mkdir ${DEBTEMPDIR}
 
-if [ -z "`head -n 1 ${DEBDIR}/changelog | grep "rkward (${VERSION}-"`" ]; then
+if [ -z "`head -n 1 ${DEBDIR}/changelog | grep "rkward (${VERSION}${DEBSUFFIX}-"`" ]; then
   echo "Version mismatch! Adjust ${DEBDIR}/changelog, first."
   exit 1
 fi
@@ -18,13 +19,13 @@ if [ ! -f ${BASEDIR}/rkward-$VERSION.tar.gz ]; then
   exit 1
 fi
 
-cp ${BASEDIR}/rkward-$VERSION.tar.gz $DEBTEMPDIR/rkward_$VERSION.orig.tar.gz
+cp ${BASEDIR}/rkward-$VERSION.tar.gz $DEBTEMPDIR/rkward_$VERSION${DEBSUFFIX}.orig.tar.gz
 cd $DEBTEMPDIR
-tar -xzf rkward_$VERSION.orig.tar.gz
+tar -xzf rkward_$VERSION${DEBSUFFIX}.orig.tar.gz
 cd rkward-$VERSION
 cp -a ${DEBDIR} debian
 dpkg-buildpackage -k0x1858CBB6 -rfakeroot --force-sign
 
 cd $DEBTEMPDIR
 dpkg-scansources . | bzip2 > Sources.bz2
-LINTIAN_PROFILE=debian lintian rkward_$VERSION-*.changes
+LINTIAN_PROFILE=debian lintian rkward_$VERSION${DEBSUFFIX}-*.changes
