4e0ac7cd2ae828b602a1cc475cf6d039254a4b97
R.J.V. Bertin <rjvbertin@gmail.com>
2017-05-17 19:58:06 +0200

    assume DBus is running + update variable name

--- rkward/main.cpp
+++ rkward/main.cpp
@@ -225,16 +225,17 @@ int main (int argc, char *argv[]) {
 	RKGlobals::startup_options["backend-debugger"] = parser.value ("backend-debugger");
 
 	// MacOS may need some path adjustments, first
-#ifdef Q_OS_MAC
+#ifdef Q_OS_MACOS
 	QString oldpath = qgetenv ("PATH");
 	if (!oldpath.contains (INSTALL_PATH)) {
 		//ensure that PATH is set to include what we deliver with the bundle
 		qputenv ("PATH", QString ("%1/bin:%1/sbin:%2").arg (INSTALL_PATH).arg (oldpath).toLocal8Bit ());
-		if (debug_level > 3) qDebug ("Adjusting system path to %s", qPrintable (qgetenv ("PATH")));
+		if (RK_Debug_Level > 3) qDebug ("Adjusting system path to %s", qPrintable (qgetenv ("PATH")));
 	}
 	// ensure that RKWard finds its own packages
 	qputenv ("R_LIBS", R_LIBS);
-	QProcess::execute ("launchctl", QStringList () << "load" << "-w" << INSTALL_PATH "/Library/LaunchAgents/org.freedesktop.dbus-session.plist");
+    // do not auto(re)launch the session DBus (or at least not with the -w option!!)
+// 	QProcess::execute ("launchctl", QStringList () << "load" << INSTALL_PATH "/Library/LaunchAgents/org.freedesktop.dbus-session.plist");
 #endif
 
 	// Handle --reuse option, by placing a dbus-call to existing RKWard process (if any) and exiting
