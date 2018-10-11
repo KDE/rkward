15012c947e5eed79bdabc4918012299b28ee3b3d
m.eik michalke <meik.michalke@uni-duesseldorf.de>
2018-10-11 17:46:54 +0200

    added two parameters to rk.check.for.pandoc()
      - the function can now check for valid output formats directly
      - if can also operate in two modes, either just return the info, or stop with an error if soemthing is indeed missing

--- rkward/rbackend/rpackages/rkward/R/rk.check.for.pandoc.R
+++ rkward/rbackend/rpackages/rkward/R/rk.check.for.pandoc.R
@@ -1,8 +1,12 @@
 #' Check for pandoc installation and its features
 #' 
 #' This function looks for a pandoc installation and if found, gathers information
-#' on its path, supported output formats, and the version number. It has no parameters.
+#' on its path, supported output formats, and the version number.
 #' 
+#' @param stop_if_missing Logical, whether an error should be thrown if either pandoc can't be found at all
+#'    or a requested output format is not supported.
+#' @param output_format A character string, name of a requested output format. If not \code{NA},
+#'    the function will check if the format is supported and return a boolean value.
 #' @return A named list with at least the boolean element \code{available}, and if
 #'    that is \code{TRUE}, also the character vectors \code{path}, \code{output_formats}, and \code{version}.
 #' @author Meik Michalke \email{rkward-devel@@kde.org}
@@ -14,7 +18,7 @@
 #'
 #' rk.check.for.pandoc()
 
-"rk.check.for.pandoc" <- function(){
+"rk.check.for.pandoc" <- function(stop_if_missing=FALSE, output_format=NA){
   pandoc <- list(available=FALSE)
   pandoc_path <- Sys.which("pandoc")[["pandoc"]]
   if(!"" %in% pandoc_path){
@@ -22,6 +26,18 @@
     pandoc[["available"]] <- TRUE
     pandoc[["output_formats"]] <- system("pandoc --list-output-formats", intern=TRUE)
     pandoc[["version"]] <- gsub("[[:space:]]*pandoc[[:space:]]*", "", system("pandoc --version", intern=TRUE)[1])
+
+    if(!is.na(output_format)){
+      pandoc <- output_format %in% pandoc[["output_formats"]]
+      if(all(isTRUE(stop_if_missing), !isTRUE(pandoc))){
+        stop(simpleError(
+          paste0("The requested output format '", output_format, "' is not supported by your pandoc installation.")
+        ))
+      } else {}
+    } else {}
+  } else if(isTRUE(stop_if_missing)){
+      stop(simpleError("'pandoc' was not found, please check your installation!"))
   } else {}
+
   return(pandoc)
 }
