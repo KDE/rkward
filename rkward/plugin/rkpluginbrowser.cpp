018f1c69824f15a2f9a0f3030780f3a9c371ba95
Thomas Friedrichsmeier <thomas.friedrichsmeier@ruhr-uni-bochum.de>
2018-11-01 17:57:37 +0100

    Use KDE-style, not Qt-style file name filter.

--- rkward/plugin/rkpluginbrowser.cpp
+++ rkward/plugin/rkpluginbrowser.cpp
@@ -57,7 +57,7 @@ RKPluginBrowser::RKPluginBrowser (const QDomElement &element, RKComponent *paren
 	selector = new GetFileNameWidget (this, mode, only_local, label_string, i18n ("Select"), xml->getStringAttribute (element, "initial", QString (), DL_INFO));
 	QString filter = xml->getStringAttribute (element, "filter", QString (), DL_INFO);
 	if (!filter.isEmpty ()) {
-		filter.append ("\nAll files (*)");
+		filter.append ("\n*|All files (*)");
 		selector->setFilter (filter);
 	}
 	connect (selector, &GetFileNameWidget::locationChanged, this, &RKPluginBrowser::textChangedFromUi);
