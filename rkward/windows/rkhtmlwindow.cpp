aea491ca914037ee37c4e011c7ab895992e823ac
Thomas Friedrichsmeier <thomas.friedrichsmeier@ruhr-uni-bochum.de>
2020-04-06 21:07:48 +0200

    Fix compilation with MSVC

--- rkward/windows/rkhtmlwindow.cpp
+++ rkward/windows/rkhtmlwindow.cpp
@@ -126,7 +126,9 @@ public:
 #else
 	bool supportsContentType (const QString &name) {
 		if (name.startsWith("text")) return true;
+#ifdef __GNUC__
 #warning TODO
+#endif
 		return false;
 	}
 	void downloadUrl (const QUrl& url) {
