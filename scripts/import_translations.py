a67c37d9bd23edc96793c5404987bd12be60b441
Thomas Friedrichsmeier <thomas.friedrichsmeier@ruhr-uni-bochum.de>
2020-06-18 22:00:14 +0200

    Count completeness of translations while importing them.
    Not removing incomplete translations, right now, but I'd like to monitor this, and have an
    easy option to purge translations below a certain level of completeness.
    
    See also: https://invent.kde.org/education/rkward/-/merge_requests/2/diffs

--- scripts/import_translations.py
+++ scripts/import_translations.py
@@ -25,6 +25,7 @@ import sys
 import subprocess
 import os
 import re
+import codecs
 import shutil
 
 SVNROOT = "svn://anonsvn.kde.org/home/kde/trunk/l10n-kf5/"
@@ -33,6 +34,40 @@ SCRIPTDIR = os.path.dirname (os.path.realpath (sys.argv[0]))
 TMPDIR = os.path.join (SCRIPTDIR, "tmp")
 EXPORTDIR = os.path.join (SCRIPTDIR, "..", "i18n", "po")
 IGNOREDPONAMES = {'org.kde.rkward.appdata.po', 'rkward._desktop_.po', 'rkward_xml_mimetypes.po'}
+
+def checkCompleteness(filename):
+    f = codecs.open(filename, 'r', 'utf-8')
+    seek = 1
+    skip = 2
+    read = 0
+    status = seek
+    stringcount = 0
+    transcount = 0
+    for line in f:
+      if status == seek and line.startswith('#, fuzzy'):
+        status = skip
+      elif line.startswith('msgid'):
+        stringcount += 1
+      elif line.startswith('msgstr'):
+        if status == skip:
+          status = seek
+        else:
+          status = read
+          line = line[7:]
+
+      if status == read:
+        if line.startswith('"'):
+          if line[1] != '"':   # Otherwise it's empty
+            transcount += 1
+            status = seek
+        else:
+          status = seek
+    f.close ()
+    if stringcount > 0 and (transcount / stringcount) < .8:
+      sys.stderr.write("WARNING: " + filename + " only has " + str(transcount) + " out of " + str(stringcount) + " strings translated.\n")
+      return False
+    return True
+
 if not os.path.exists (TMPDIR):
     os.makedirs (TMPDIR)
 if not os.path.exists (EXPORTDIR):
@@ -63,6 +98,12 @@ for lang in LANGUAGES:
     for pofile in pofiles:
         outfile = os.path.join (EXPORTDIR, re.sub ("po$", lang + ".po", pofile))
         infile = os.path.join (langdir, pofile)
+        goodenough = checkCompleteness(infile)
+        # Uncomment these lines to purge badly incomplete translations:
+        #if not goodenough:
+          #os.remove(infile)
+          #print ("NOT writing " + outfile)
+          #continue
 
         # copy to destination
         print ("writing " + outfile)
