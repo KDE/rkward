b9015c08be5b9474860616a410e30b7fb8e54e75
Pino Toscano <pino@kde.org>
2020-06-18 08:22:00 +0200

    i18n: use explicitly python2 for update_plugin_messages.py
    update_plugin_messages.py is not compatible with Python 3, so explicitly
    force Python 2 to run it for now.

--- Messages.sh
+++ Messages.sh
@@ -9,19 +9,19 @@ $EXTRACTRC `find rkward -name \*.rc -a \! -name rkward_windows_icon.rc -o -name 
 $XGETTEXT `find rkward -name \*.cpp -o -name \*.h -name \*.c` rc.cpp -o $podir/rkward.pot
 
 # extract messages from global .rkh pages: >> rkward__pages.pot
-python scripts/update_plugin_messages.py --extract-only --default_po=pages --outdir=$podir rkward/pages/*.rkh
+python2 scripts/update_plugin_messages.py --extract-only --default_po=pages --outdir=$podir rkward/pages/*.rkh
 # extract messages from analysis plugins: >> rkward__analysis.pot
-python scripts/update_plugin_messages.py --extract-only --outdir=$podir rkward/plugins/analysis.pluginmap
+python2 scripts/update_plugin_messages.py --extract-only --outdir=$podir rkward/plugins/analysis.pluginmap
 # extract messages from import_export plugins: >> rkward__import_export.pot
-python scripts/update_plugin_messages.py --extract-only --outdir=$podir rkward/plugins/import_export.pluginmap
+python2 scripts/update_plugin_messages.py --extract-only --outdir=$podir rkward/plugins/import_export.pluginmap
 # extract messages from data plugins: >> rkward__data.pot
-python scripts/update_plugin_messages.py --extract-only --outdir=$podir rkward/plugins/data.pluginmap
+python2 scripts/update_plugin_messages.py --extract-only --outdir=$podir rkward/plugins/data.pluginmap
 # extract messages from plot plugins: >> rkward__plots.pot
-python scripts/update_plugin_messages.py --extract-only --outdir=$podir rkward/plugins/plots.pluginmap
+python2 scripts/update_plugin_messages.py --extract-only --outdir=$podir rkward/plugins/plots.pluginmap
 # extract messages from graphics device plugins: >> rkward__graphics_device.pot
-python scripts/update_plugin_messages.py --extract-only --outdir=$podir rkward/plugins/x11device.pluginmap
+python2 scripts/update_plugin_messages.py --extract-only --outdir=$podir rkward/plugins/x11device.pluginmap
 # extract messages from IRT plugins: >> rkward__item_response_theory.pot
-python scripts/update_plugin_messages.py --extract-only --outdir=$podir rkward/plugins/irt.pluginmap
+python2 scripts/update_plugin_messages.py --extract-only --outdir=$podir rkward/plugins/irt.pluginmap
 # extract messages from distribution plugins: >> rkward__distributions.pot
-python scripts/update_plugin_messages.py --extract-only --outdir=$podir rkward/plugins/distributions.pluginmap
+python2 scripts/update_plugin_messages.py --extract-only --outdir=$podir rkward/plugins/distributions.pluginmap
 # messages of embedded plugins are extracted implicitly, as part of the above extraction calls: >> rkward__embedded.pot
