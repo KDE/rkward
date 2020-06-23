KDE/rkward

commit 00ff46157ab9e9a4c1cc99d733ff791077c39489

Split polib dependent check into separate file.
@pino ... because it really is causing extra trouble on craft.

scripts/import_translations.py
@@ -26,33 +26,13 @@ import subprocess
 import os
 import re
 import shutil
-import polib
 
 SVNROOT = "svn://anonsvn.kde.org/home/kde/trunk/l10n-kf5/"
 RKWARDSVNPATH = "messages/rkward"
 SCRIPTDIR = os.path.dirname (os.path.realpath (sys.argv[0]))
 TMPDIR = os.path.join (SCRIPTDIR, "tmp")
-EXPORTDIR = os.path.join (SCRIPTDIR, "..", "i18n", "po")
+EXPORTDIR = os.path.join (SCRIPTDIR, "..", "i18n")
 IGNOREDPONAMES = {'org.kde.rkward.appdata.po', 'rkward._desktop_.po', 'rkward_xml_mimetypes.po'}
-
-def checkCompleteness(filename):
-    po = polib.pofile(filename)
-    stringcount = 0
-    transcount = 0
-    for entry in po:
-        if entry.obsolete:
-            continue
-        stringcount += 1
-        if entry.translated():
-            transcount += 1
-    if transcount == 0:
-        sys.stderr.write("SKIP: %s has no translated messages.\n" % filename)
-        return False
-    if stringcount > 0 and (transcount / stringcount) < .8:
-        sys.stderr.write("WARNING: %s only has %d messages translated out of %d (%.2f%%).\n" % (filename, transcount, stringcount, (transcount / stringcount) * 100.0))
-        # Uncomment this line to purge badly incomplete translations:
-        #return False
-    return True
 
 if not os.path.exists (TMPDIR):
     os.makedirs (TMPDIR)
@@ -84,9 +64,6 @@ for lang in LANGUAGES:
     for pofile in pofiles:
         outfile = os.path.join (EXPORTDIR, re.sub ("po$", lang + ".po", pofile))
         infile = os.path.join (langdir, pofile)
-        goodenough = checkCompleteness(infile)
-        if not goodenough:
-            continue
 
         # copy to destination
         print ("writing " + outfile)

scripts/check_translations.py
@@ -1,0 +1,44 @@
+#! /usr/bin/env python3
+# ***************************************************************************
+#                          check_translations  -  description
+#                             -------------------
+#    begin                : Jun 2020
+#    copyright            : (C) 2020 by Thomas Friedrichsmeier
+#    email                : tfry@users.sourceforge.net
+# ***************************************************************************
+#
+# ***************************************************************************
+# *                                                                         *
+# *   This program is free software; you can redistribute it and/or modify  *
+# *   it under the terms of the GNU General Public License as published by  *
+# *   the Free Software Foundation; either version 2 of the License, or     *
+# *   (at your option) any later version.                                   *
+# *                                                                         *
+# ***************************************************************************
+#
+# Check translations for obvious problems
+
+import os
+import sys
+import glob
+import polib
+
+SCRIPTDIR = os.path.dirname (os.path.realpath (sys.argv[0]))
+EXPORTDIR = os.path.join (SCRIPTDIR, "..", "i18n", "po")
+
+def checkCompleteness(filename):
+    po = polib.pofile(filename)
+    percentage = po.percent_translated()
+    if percentage == 0:
+        sys.stderr.write("PURGE: %s has no translated messages.\n" % filename)
+        return False
+    if percentage < 80:
+        sys.stderr.write("WARNING: %s only has %d%% messages translated.\n" % (filename, percentage))
+        # Uncomment this line to purge badly incomplete translations:
+        #return False
+    return True
+
+pofiles = glob.glob(os.path.join(EXPORTDIR, '**', '*.po'), recursive=True)
+for pofile in pofiles:
+    if not checkCompleteness(pofile):
+        os.remove(pofile)

commit b0bb273025dde5d8dd4be78e54beeba99f317d79

import_translations.py: fix EXPORTDIR definition
Broken by commit 00ff46157ab9e9a4c1cc99d733ff791077c39489.

scripts/import_translations.py
@@ -31,7 +31,7 @@ SVNROOT = "svn://anonsvn.kde.org/home/kde/trunk/l10n-kf5/"
 RKWARDSVNPATH = "messages/rkward"
 SCRIPTDIR = os.path.dirname (os.path.realpath (sys.argv[0]))
 TMPDIR = os.path.join (SCRIPTDIR, "tmp")
-EXPORTDIR = os.path.join (SCRIPTDIR, "..", "i18n")
+EXPORTDIR = os.path.join (SCRIPTDIR, "..", "i18n", "po")
 IGNOREDPONAMES = {'org.kde.rkward.appdata.po', 'rkward._desktop_.po', 'rkward_xml_mimetypes.po'}
 
 if not os.path.exists (TMPDIR):