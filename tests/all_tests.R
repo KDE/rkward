ee37bd4c77b9d8d3283ffd064d0410067e90dbae
Thomas Friedrichsmeier <thomas.friedrichsmeier@kdemail.net>
2022-04-10 12:54:15 +0200

    Next round of fixing updating test standards (and fix one minor issue)

--- tests/all_tests.R
+++ tests/all_tests.R
@@ -3,9 +3,9 @@ require (rkwardtests)
 ## add your test suite files, to this vector:
 testsuites <- c (
 #	"rkward_application_tests.R",
-	"import_export_plugins.R"
+#	"import_export_plugins.R"
 #	"item_response_theory.R",
-#	"analysis_plugins.R",
+	"analysis_plugins.R",
 #	"distributions.R",
 #	"plots.R",
 #	"data_plugin_tests.R"
