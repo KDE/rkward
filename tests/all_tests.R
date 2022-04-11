f4b256a377b2261ad9071691e379fddc3ddce5ab
Thomas Friedrichsmeier <thomas.friedrichsmeier@kdemail.net>
2022-04-11 22:49:27 +0200

    Update more test standards

--- tests/all_tests.R
+++ tests/all_tests.R
@@ -2,13 +2,13 @@ require (rkwardtests)
 
 ## add your test suite files, to this vector:
 testsuites <- c (
-#	"rkward_application_tests.R",
-#	"import_export_plugins.R"
-#	"item_response_theory.R",
-	"analysis_plugins.R",
-#	"distributions.R",
-#	"plots.R",
-#	"data_plugin_tests.R"
+	"rkward_application_tests.R"
+	"import_export_plugins.R"
+	"item_response_theory.R",
+	"analysis_plugins.R"
+	"distributions.R",
+	"plots.R",
+	"data_plugin_tests.R"
 )
 
 rktest.makeplugintests (testsuites=testsuites, outfile="make_plugintests.txt")
