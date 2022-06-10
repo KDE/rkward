37f7e14f7794541c01d9bdcb2491a80902470037
Thomas Friedrichsmeier <thomas.friedrichsmeier@kdemail.net>
2022-06-10 23:50:52 +0200

    Try to get better diagnostics for test backend failing to start on CI

--- rkward/autotests/core_test.cpp
+++ rkward/autotests/core_test.cpp
@@ -1,6 +1,8 @@
 #include <QObject>
 #include <QTest>
 #include <QApplication>
+#include <QFile>
+#include <QDir>
 
 #include <KAboutData>
 
@@ -41,16 +43,28 @@ private slots:
 	void initTestCase()
 	{
 		qputenv("QTWEBENGINE_CHROMIUM_FLAGS", "--no-sandbox"); // Allow test to be run as root, which, for some reason is being done on the SuSE CI.
-		KAboutData::setApplicationData(KAboutData("rkward")); // needed for .rc files to load0
-		RK_Debug::RK_Debug_Level = DL_WARNING;
+		KAboutData::setApplicationData(KAboutData("rkward")); // needed for .rc files to load
+		RK_Debug::RK_Debug_Level = DL_DEBUG;
 		qDebug(R_EXECUTABLE);
 		RKSessionVars::r_binary = R_EXECUTABLE;
 		main_win = new RKWardMainWindow();
 		main_win->testmode_suppress_dialogs = true;
+
+		QElapsedTimer t;
+		t.start();
 		while (!(RInterface::instance()->backendIsDead() || RInterface::instance()->backendIsIdle())) {
-			qApp->processEvents();
+			if (t.elapsed() > 20000) break;
+			qApp->processEvents(QEventLoop::AllEvents);
 		}
-		qDebug("Startup completed");
+		if (RInterface::instance()->backendIsIdle()) {
+			qDebug("Startup completed");
+		} else {
+			qDebug("Startup failed. Listing contents of /tmp/rkward.rbackend");
+			QFile f(QDir::tempPath() + "/rkward.rbackend");
+			f.open(QIODevice::ReadOnly);
+			auto output = f.readAll();
+			qDebug("%s", output.data());
+		}
 	}
 
 	void getIntVector() {
