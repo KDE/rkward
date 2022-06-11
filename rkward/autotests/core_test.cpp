5694adc32b33a61debc86f0c61352c09dbb5bfce
Thomas Friedrichsmeier <thomas.friedrichsmeier@kdemail.net>
2022-06-11 19:31:23 +0200

    Add license header

--- rkward/autotests/core_test.cpp
+++ rkward/autotests/core_test.cpp
@@ -1,3 +1,10 @@
+/*
+core_test - This file is part of RKWard (https://rkward.kde.org). Created: Thu Jun 09 2022
+SPDX-FileCopyrightText: 2022 by Thomas Friedrichsmeier <thomas.friedrichsmeier@kdemail.net>
+SPDX-FileContributor: The RKWard Team <rkward-devel@kde.org>
+SPDX-License-Identifier: GPL-2.0-or-later
+*/
+
 #include <QObject>
 #include <QTest>
 #include <QApplication>
