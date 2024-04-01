023095df00dab83a458b17a274935f3217066c83
Thomas Friedrichsmeier <thomas.friedrichsmeier@kdemail.net>
2024-04-01 18:15:46 +0200

    Fix compilation

--- rkward/autotests/core_test.cpp
+++ rkward/autotests/core_test.cpp
@@ -14,6 +14,7 @@ SPDX-License-Identifier: GPL-2.0-or-later
 #include <QRegularExpression>
 
 #include <KAboutData>
+#include <KLocalizedString>
 #include <KActionCollection>
 
 #include "../debug.h"
