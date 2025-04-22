2eb2aba4b948415f8e6f4da04dcbc0319c638ecc
Thomas Friedrichsmeier <thomas.friedrichsmeier@kdemail.net>
2025-04-22 16:49:41 +0200

    Remove dependency on qt5compat

--- rkward/windows/rkhtmlwindow.cpp
+++ rkward/windows/rkhtmlwindow.cpp
@@ -28,7 +28,6 @@ SPDX-License-Identifier: GPL-2.0-or-later
 #include <QPrintDialog>
 #include <QMenu>
 #include <QFileDialog>
-#include <QTextCodec>
 #include <QFontDatabase>
 #include <QTemporaryFile>
 #include <QGuiApplication>
