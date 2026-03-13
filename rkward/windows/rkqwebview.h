184cd9d33267f02b7c24cb53e750ac3aaa8c2314
Thomas Friedrichsmeier <thomas.friedrichsmeier@kdemail.net>
2026-03-13 12:47:53 +0100

    Implement callback from qwebview page to rkward, handle selection as proof of concept

--- rkward/windows/rkqwebview.h
+++ rkward/windows/rkqwebview.h
@@ -41,11 +41,20 @@ class RKQWebView : public RKHTMLViewer {
 
   private:
 	friend class RKHTMLViewer;
+	friend class RKQWebViewCallbackServer;
 	RKQWebView(RKHTMLWindow *parent);
 	QPointer<QQuickWidget> view;
 	QUrl currentAcceptedUrl() const;
 	QMap<QString, QString> persistentScripts;
-	std::function<void(const QVariant &)> runjs_callback;
+	struct Script {
+		QString script;
+		std::function<void(const QVariant &)> callback;
+	};
+	QList<Script> runjs_queue;
+	bool running_script;
+	void tryRunNextScript();
+	void receivedCallbackMessage(const QString &message);
+	QString selected_text;
   private Q_SLOTS:
 	void onUrlChanged(const QUrl &url, const QString &error, int status);
 	void onLoadFinished(const QUrl &url);
